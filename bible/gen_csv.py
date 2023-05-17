98dd98c65f22825c513769d7b1dbe8d3f79504fa
pishoyg <pishoybg@gmail.com>
2023-05-17 09:43:04 -0400

    Parse sources, update generation of TXT and CSV files.

---
+++ bible/gen_csv.py
@@ -0,0 +1,92 @@
+import argparse
+import json
+import json5
+import os
+import pandas as pd
+import re
+
+GSPREAD_ENABLED = True
+try:
+  import gspread
+  from oauth2client.service_account import ServiceAccountCredentials
+except:
+  GSPREAD_ENABLED = False
+
+
+LANGUAGES = [
+  'Bohairic', 'Sahidic', 'English', 'Greek', 'Fayyumic', 'Akhmimic',
+  'OldBohairic', 'Mesokemic', 'DialectP', 'Lycopolitan']
+VERSE_PREFIX = re.compile('^\(([^)]+)\)')
+
+argparser = argparse.ArgumentParser(
+  description='Process the Coptic Bible data.')
+argparser.add_argument(
+  '--books',
+  type=str,
+  help='Path to a file containing the books in order.',
+  default='raw/books.txt'
+)
+argparser.add_argument(
+  '--input_dir',
+  type=str,
+  help='Path to the input directory.',
+  default='raw'
+)
+argparser.add_argument(
+  '--output_dir',
+  type=str,
+  help='Path to the input directory.',
+  default='data'
+)
+args = argparser.parse_args()
+
+
+def verse_number(verse):
+  if not isinstance(verse, dict):
+    print(verse)
+  t = verse['English'] or verse['Greek']
+  s = VERSE_PREFIX.search(t)
+  return s.groups()[0] if s else ''
+
+
+def main():
+  with open(args.books) as b:
+    books = b.read().split('\n')
+    books = filter(None, books)
+
+  bible = pd.DataFrame()
+
+  for book_name in books:
+    try:
+      t = open(os.path.join(args.input_dir, book_name + '.json')).read()
+    except:
+      print('Book not found : {}'.format(book_name))
+      continue
+    print('Processing book: {}'.format(book_name))
+    try:
+      data = json.loads(t)
+    except:
+      data = json5.loads(t)
+    del t
+
+    df = pd.DataFrame()
+
+    for chapter in data:
+      chapter_num = chapter['sectionNameEnglish'] or '1'
+      for verse in chapter['data']:
+        d = {
+          'book': book_name,
+          'chapter': chapter_num,
+          'verse': verse_number(verse),
+        }
+        for lang in LANGUAGES:
+          d[lang] = VERSE_PREFIX.sub('', verse[lang])
+        df = pd.concat([df, pd.DataFrame([d])], ignore_index=True)
+    df.to_csv(os.path.join(args.output_dir, book_name + '.csv'), sep='\t',
+              index=False)
+    bible = pd.concat([bible, df], ignore_index=True)
+  bible.to_csv(os.path.join(args.output_dir, 'bible.csv'), sep='\t', index=False)
+
+
+if __name__ == '__main__':
+  main()
---
+++ bible/gen_txt.py
@@ -0,0 +1,154 @@
+import argparse
+import json
+import json5
+import os
+import pathlib
+import re
+
+
+LANGUAGES = [
+  'Bohairic', 'Akhmimic', 'Fayyumic', 'OldBohairic', 'Mesokemic', 'DialectP',
+  'Lycopolitan', 'Greek', 'English', 'Sahidic']
+VERSE_PREFIX = re.compile('^\(([^)]+)\)')
+SPACE = re.compile('\s')
+
+argparser = argparse.ArgumentParser(
+  description='Process the Coptic Bible data.')
+argparser.add_argument(
+  '--input_dir',
+  type=str,
+  help='Path to the input directory.',
+  default='raw'
+)
+argparser.add_argument(
+  '--output_dir',
+  type=str,
+  help='Path to the input directory.',
+  default='data'
+)
+args = argparser.parse_args()
+
+
+class lang_processor:
+  errors = {lang: [] for lang in LANGUAGES}
+
+  def __init__(self, _book_name, _lang):
+    self.book_name = _book_name
+    self.lang = _lang
+    self.lines = []
+    self.done = False
+    self.title_check_done = False
+    self.verse_num = None
+    self.chapter_num = None
+
+  def _title_check(self, verse):
+    # Return true if this is the title.
+    if self.title_check_done:
+      return False
+    self.title_check_done = True
+    if VERSE_PREFIX.match(verse):
+      return False
+    return True
+  
+  def _update_verse_number(self, verse):
+    s = VERSE_PREFIX.search(verse)
+    if s:
+      g = s.groups()
+      assert len(g) == 1
+      self.verse_num = g[0]
+      return True
+
+    if not self.verse_num:
+      self.verse_num = 1
+      return True
+
+    try:
+      self.verse_num = int(self.verse_num) + 1
+      return True
+    except:
+      self.errors[self.lang].append('Suspected non-verse: {}:{} : {}'.format(
+        self.chapter_num, self.verse_num, verse))
+      return False
+
+  def _process_verse(self, verse):
+    verse = verse[self.lang]
+    if self._title_check(verse):
+      if verse:
+        self.errors[self.lang].append("Suspected title    : {} : {}".format(
+          self.chapter_num, verse))
+      return
+    if not self._update_verse_number(verse):
+      # Ignore verse when unable to infer the number.
+      # It's likely not a real verse.
+      return
+    verse = VERSE_PREFIX.sub('', verse)
+    verse = ' '.join(['{}:{}'.format(self.chapter_num, self.verse_num)] + verse.split())
+    self.lines.append(verse)
+
+  def _update_chapter_number(self, chapter):
+    if chapter['sectionNameEnglish']:
+      self.chapter_num = chapter['sectionNameEnglish']
+      return
+
+    if not self.chapter_num:
+      self.chapter_num = 1
+      return
+
+    try:
+      self.chapter_num = int(self.chapter_num) + 1
+    except:
+      raise Exception('Unable to infer chapter number:\n{}'.format(chapter))
+
+  def _process_chapter(self, chapter):
+    self.title_check_done = False
+    self._update_chapter_number(chapter)
+    for verse in chapter['data']:
+      self._process_verse(verse)
+    self.verse_num = None
+
+  def process_book(self, book):
+    self.errors[self.lang].append(self.book_name)
+    for chapter in book:
+      self._process_chapter(chapter)
+    self.done = True
+    self.chapter_num = None
+    self.errors[self.lang].append('')
+  
+  def text(self):
+    assert self.done
+    return '\n'.join(self.lines)
+  
+  def errors_text(self, lang):
+    return '\n'.join(self.errors[lang])
+
+
+def main():
+  for f in os.listdir(args.input_dir):
+    if not f.endswith('.json'):
+      continue
+    book_name = os.path.splitext(f)[0]
+    print('Processing book: {}'.format(book_name))
+    t = open(os.path.join(args.input_dir, f)).read()
+    try:
+      data = json.loads(t)
+    except:
+      data = json5.loads(t)
+    for lang in LANGUAGES:
+      out_dir = os.path.join(args.output_dir, lang)
+      pathlib.Path(out_dir).mkdir(exist_ok=True)
+      p = lang_processor(book_name, lang)
+      p.process_book(data)
+      out = open(os.path.join(out_dir, book_name + '.txt'), 'w')
+      out.write(p.text())
+  for lang in LANGUAGES:
+    err = lang_processor(None, lang).errors_text(lang)
+    if not err:
+      continue
+    out_dir = os.path.join(args.output_dir, lang, 'errors')
+    pathlib.Path(out_dir).mkdir(exist_ok=True)
+    out = open(os.path.join(out_dir, 'errors.txt'), 'w')
+    out.write(err)
+
+
+if __name__ == '__main__':
+  main()
