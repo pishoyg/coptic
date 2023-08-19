cea80892733954a44f0f89a2c4392d1eb11fa220
pishoyg <pishoybg@gmail.com>
2023-08-19 11:32:21 +0200

    Slight modifications to the format of a Python tool.

--- dictionary/marcion/utils/formatter.py
+++ dictionary/marcion/utils/formatter.py
@@ -1,13 +1,16 @@
+import clipboard
 import sys
+import re
+
+
 x = sys.argv[1]
 x = x.split('\n')
 x = [a for a in x if 'B' in a]
-prefix = '→ \([^)]+\) '
-suffix = ' (←|↓)'
-import re
-for pair in [[prefix, ''], [suffix, ''], ['–', '-'], ['=', '//'], ['\+', ' (ⲉϥ)']]:
-  x = [re.sub(pair[0], pair[1], a) for a in x]
+prefix = r'→ \([^)]+\) '
+suffix = r' (←|↓)'
+for pair in [
+        [prefix, ''], [suffix, ''], ['–', '-'], ['=', '//'], [r'\+', ' (ⲉϥ)']]:
+    x = [re.sub(pair[0], pair[1], a) for a in x]
 
 x = ', '.join(x)
-import clipboard
 clipboard.copy(x)
