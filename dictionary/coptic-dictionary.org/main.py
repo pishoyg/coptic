73c7fff3329aaa96fd2a66942cecef98e7fe6caf
Pishoy Girgis <pishoybg@gmail.com>
2024-05-30 22:01:54 +0200

    Change the property display.

--- dictionary/coptic-dictionary.org/main.py
+++ dictionary/coptic-dictionary.org/main.py
@@ -1,69 +1,67 @@
 # TODO: Read this file for inspiration:
 # https://github.com/KELLIA/dictionary/blob/master/utils/dictionary_reader.py.
 import argparse
+import pprint
 
 import bs4
 import type_enforced
 
-ENTRY_ATTRS = {"xml:id", "change", "type"}
-ENTRY_CHILDREN = {"gramGrp", "etym", "sense", "note", "xr", "form"}
-
-FORM_ATTRS = {"type", "xml:id", "change", "corresp"}
-FORM_CHILDREN = {"usg", "oRef", "note", "orth", "gramGrp"}
-
-GRAMGRP_ATTRS = set()
-GRAMGRP_CHILDREN = {"pos", "number", "subc", "gram", "gen", "note"}
-
-ETYM_ATTRS = set()
-ETYM_CHILDREN = {"note", "xr", "ref"}
-
-SENSE_ATTRS = {"xml:id"}
-SENSE_CHILDREN = {"note", "cit", "xr", "ref"}
-
-NOTE_ATTRS = {"xml:lang"}
-NOTE_CHILDREN = set()
-
-XR_ATTRS = {"type"}
-XR_CHILDREN = {"ref"}
-
-GRAM_ATTRS = {"type"}
-GRAM_CHILDREN = set()
-
-ORTH_ATTRS = set()
-ORTH_CHILDREN = set()
-
-REF_ATTRS = {"target", "type", "xml:lang"}
-REF_CHILDREN = set()
-
-OREF_ATTRS = set()
-OREF_CHILDREN = set()
-
-NUMBER_ATTRS = set()
-NUMBER_CHILDREN = set()
-
-SUBC_ATTRS = set()
-SUBC_CHILDREN = set()
-
-POS_ATTRS = set()
-POS_CHILDREN = set()
-
-USG_ATTRS = {"type"}
-USG_CHILDREN = set()
-
-GEN_ATTRS = set()
-GEN_CHILDREN = set()
-
-CIT_ATTRS = {"type"}
-CIT_CHILDREN = {"def", "quote", "note", "bibl"}
-
-QUOTE_ATTRS = {"xml:lang"}
-QUOTE_CHILDREN = set()
-
-DEF_ATTRS = {"xml:lang"}
-DEF_CHILDREN = set()
-
-BIBL_ATTRS = set()
-BIBL_CHILDREN = set()
+PROPERTIES = {
+    "superEntry": {
+        "children": {"entry"},
+    },
+    "entry": {
+        "attrs": {"xml:id", "type", "change"},
+        "children": {"etym", "note", "sense", "form", "gramGrp", "xr"},
+    },
+    "form": {
+        "attrs": {"corresp", "xml:id", "type", "change"},
+        "children": {"orth", "oRef", "usg", "note", "gramGrp"},
+    },
+    "sense": {
+        "attrs": {"xml:id"},
+        "children": {"note", "ref", "cit", "xr"},
+    },
+    "note": {
+        "attrs": {"xml:lang"},
+    },
+    "etym": {
+        "children": {"note", "xr", "ref"},
+    },
+    "gramGrp": {
+        "children": {"gram", "subc", "gen", "pos", "note", "number"},
+    },
+    "xr": {
+        "attrs": {"type"},
+        "children": {"ref"},
+    },
+    "cit": {
+        "attrs": {"type"},
+        "children": {"def", "bibl", "quote", "note"},
+    },
+    "gram": {
+        "attrs": {"type"},
+    },
+    "def": {
+        "attrs": {"xml:lang"},
+    },
+    "quote": {
+        "attrs": {"xml:lang"},
+    },
+    "ref": {
+        "attrs": {"target", "xml:lang", "type"},
+    },
+    "usg": {
+        "attrs": {"type"},
+    },
+    "subc": {},
+    "gen": {},
+    "pos": {},
+    "number": {},
+    "oRef": {},
+    "orth": {},
+    "bibl": {},
+}
 
 argparser = argparse.ArgumentParser(description="Process the Coptic Lexicon.")
 
@@ -78,14 +76,22 @@ args = argparser.parse_args()
 
 
 @type_enforced.Enforcer
-def analyze(soup: bs4.BeautifulSoup | bs4.Tag, tag_name: str) -> None:
-    attrs = set()
-    children = set()
-    for tag in soup.find_all(tag_name):
-        attrs.update(tag.attrs.keys())
-        children.update(c.name for c in tag.children if isinstance(c, bs4.Tag))
-    print(f"{tag_name.upper()}_ATTRS = {attrs}")
-    print(f"{tag_name.upper()}_CHILDREN = {children}")
+def analyze(soup: bs4.BeautifulSoup | bs4.Tag) -> None:
+    all_tag_names = {tag.name for tag in soup.descendants if isinstance(tag, bs4.Tag)}
+
+    tree = {}
+    for name in all_tag_names:
+        attrs = set()
+        children = set()
+        for tag in soup.find_all(name):
+            attrs.update(tag.attrs.keys())
+            children.update(c.name for c in tag.children if isinstance(c, bs4.Tag))
+        tree[name] = {}
+        if attrs:
+            tree[name]["attrs"] = attrs
+        if children:
+            tree[name]["children"] = children
+    pprint.pprint(tree)
 
 
 @type_enforced.Enforcer
