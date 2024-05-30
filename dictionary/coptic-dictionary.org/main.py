ebb2191a00d3cb0000fabc48e86fe07dd3af4f9c
Pishoy Girgis <pishoybg@gmail.com>
2024-05-30 23:18:34 +0200

    Update the property display.

--- dictionary/coptic-dictionary.org/main.py
+++ dictionary/coptic-dictionary.org/main.py
@@ -1,67 +1,147 @@
 # TODO: Read this file for inspiration:
 # https://github.com/KELLIA/dictionary/blob/master/utils/dictionary_reader.py.
 import argparse
-import pprint
+import json
+import re
 
 import bs4
 import type_enforced
 
+MAX_NUM_ATTR_VALUES = 10
+LIST_ELEMENT_CLOSING_QUOTE = re.compile(r'",\s+')
+
+
 PROPERTIES = {
-    "superEntry": {
-        "children": {"entry"},
+    "def": {
+        "xml:lang": ["de", "fr", "en"],
+        "STRINGS": [
+            "146 DISTINCT VALUES",
+            "roter Turmalin",
+            "iterrog. or condit.",
+            "variété inconnue de plante",
+            "...",
+        ],
     },
     "entry": {
-        "attrs": {"xml:id", "type", "change"},
-        "children": {"etym", "note", "sense", "form", "gramGrp", "xr"},
+        "type": ["compound", "foreign", "hom"],
+        "xml:id": ["11284 DISTINCT VALUES", "C10197", "C9097", "C8392", "..."],
+        "change": ["#deprecated #v1.2"],
+        "STRINGS": ["to be deleted?; no longer in DB"],
+        "CHILDREN": ["gramGrp", "form", "etym", "note", "xr", "sense"],
     },
-    "form": {
-        "attrs": {"corresp", "xml:id", "type", "change"},
-        "children": {"orth", "oRef", "usg", "note", "gramGrp"},
+    "etym": {"CHILDREN": ["xr", "note", "ref"]},
+    "gen": {"STRINGS": ["m.", "f."]},
+    "xr": {"type": ["cf", "ant", "syn"], "CHILDREN": ["ref"]},
+    "usg": {"type": ["geo"], "STRINGS": ["11 DISTINCT VALUES", "S", "Ak", "V", "..."]},
+    "gram": {
+        "type": [
+            "collocPartikel",
+            "collocConj",
+            "collocAdv",
+            "collocNoun",
+            "collocPrep",
+            "collocParticle",
+        ],
+        "STRINGS": ["107 DISTINCT VALUES", "ϩⲁϫⲛ-", "ⲛⲧⲛ-", "ⲉⲑⲏ", "..."],
     },
-    "sense": {
-        "attrs": {"xml:id"},
-        "children": {"note", "ref", "cit", "xr"},
+    "orth": {"STRINGS": ["15259 DISTINCT VALUES", "ⲥⲧⲁⲇⲓⲟⲛ", "ⲧⲱⲃⲉ", "ⲟⲩⲁ-", "..."]},
+    "cit": {
+        "type": ["example", "translation"],
+        "CHILDREN": ["def", "bibl", "note", "quote"],
     },
     "note": {
-        "attrs": {"xml:lang"},
+        "xml:lang": ["de", "fr", "en"],
+        "STRINGS": [
+            "405 DISTINCT VALUES",
+            "zumeist mit folgender Kardinalzahl",
+            "conjuguè à une négation",
+            "Briefe",
+            "...",
+        ],
     },
-    "etym": {
-        "children": {"note", "xr", "ref"},
+    "bibl": {
+        "STRINGS": [
+            "6416 DISTINCT VALUES",
+            "CD 480b",
+            "CD 374a; CED 169; KoptHWb 193, 539; DELC 194-195",
+            "CDC 9a; KoptHWb 496; ONB II 687",
+            "...",
+        ]
+    },
+    "pos": {
+        "STRINGS": [
+            "34 DISTINCT VALUES",
+            "unbestimmter Artikel",
+            "Interjektion",
+            "Nominalpräfix",
+            "...",
+        ]
+    },
+    "superEntry": {"CHILDREN": ["entry"]},
+    "number": {"STRINGS": ["sg.", "pl."]},
+    "ref": {
+        "target": ["677 DISTINCT VALUES", "#ϩⲙⲟⲟⲥ", "#ⲱϩⲉ", "#ⲕⲱⲕ ⲁϩⲏⲩ", "..."],
+        "type": [
+            "coptic_usage::cu_ID",
+            "WCNae",
+            "greek_lemma::grl_ID",
+            "Greek",
+            "greek_lemma::grl_ref",
+            "greek_lemma::grl_meaning",
+            "greek_lemma::grl_lemma",
+            "greek_lemma::grl_pos",
+            "WCNde",
+        ],
+        "xml:lang": ["grc"],
+        "STRINGS": ["17117 DISTINCT VALUES", "δευτερονόμος", "1642", "περιχρίω", "..."],
     },
     "gramGrp": {
-        "children": {"gram", "subc", "gen", "pos", "note", "number"},
+        "STRINGS": [
+            '<gram type="pos">Possessivartikel</gram>\n         <gram type="gen">f.</gram>\n         <gram type="number">sg.</gram>\n         <gram type="person[psor]">1.</gram>\n         <gram type="number[psor]">sg.</gram>',
+            '<gram type="pos">Possessivartikel</gram>\n          <gram type="gen">m.</gram>\n          <gram type="number">sg.</gram>\n          <gram type="person[psor]">2.</gram>\n          <gram type="gen[psor]">f.</gram>\n          <gram type="number[psor]">sg.</gram>',
+            '<gram type="pos">Possessivartikel</gram>\n          <gram type="number">pl.</gram>\n          <gram type="person[psor]">2.</gram>\n          <gram type="gen[psor]">f.</gram>\n          <gram type="number[psor]">sg.</gram>',
+            '<gram type="pos">Possessivartikel</gram>\n          <gram type="number">pl.</gram>\n          <gram type="person[psor]">1.</gram>\n          <gram type="number[psor]">sg.</gram>',
+            '-\n          <gram type="pos">Possessivartikel</gram>\n          <gram type="gen">f.</gram>\n          <gram type="number">sg.</gram>\n          <gram type="person[psor]">2.</gram>\n          <gram type="gen[psor]">f.</gram>\n          <gram type="number[psor]">sg.</gram>',
+            '<gram type="pos">Possessivartikel</gram>\n          <gram type="gen">m.</gram>\n          <gram type="number">sg.</gram>\n          <gram type="person[psor]">1.</gram>\n          <gram type="number[psor]">sg.</gram>',
+        ],
+        "CHILDREN": ["subc", "number", "note", "gen", "gram", "pos"],
     },
-    "xr": {
-        "attrs": {"type"},
-        "children": {"ref"},
+    "subc": {
+        "STRINGS": [
+            "55 DISTINCT VALUES",
+            "zur Bildung von Ortsangaben",
+            "Umstandsatzkonverter",
+            "Konjunktiv ⲛ(ⲧⲉ)-",
+            "...",
+        ]
     },
-    "cit": {
-        "attrs": {"type"},
-        "children": {"def", "bibl", "quote", "note"},
+    "oRef": {
+        "STRINGS": ["65 DISTINCT VALUES", "ⲣ ⲡ ⲣⲱϣⲉ", "ⲣ ⲡ ⲕⲉ-", "ϭⲙ ⲡ ⲟⲩⲱ", "..."]
     },
-    "gram": {
-        "attrs": {"type"},
+    "form": {
+        "type": ["lemma", "compound", "inflected"],
+        "xml:id": ["32230 DISTINCT VALUES", "CF23974", "CF19823", "CF4356", "..."],
+        "change": ["#added #v1.2", "#deprecated #v1.2"],
+        "corresp": ["762 DISTINCT VALUES", "CF13062", "CF15733", "CF14313", "..."],
+        "STRINGS": ["ⲓ", "ⲣⲧϭⲟⲧ", "ⲙⲉⲧϩⲟⲩⲟ"],
+        "CHILDREN": ["orth", "gramGrp", "oRef", "note", "usg"],
     },
-    "def": {
-        "attrs": {"xml:lang"},
+    "sense": {
+        "xml:id": ["15923 DISTINCT VALUES", "CS11973", "CS10964", "CS11952", "..."],
+        "CHILDREN": ["ref", "xr", "note", "cit"],
     },
     "quote": {
-        "attrs": {"xml:lang"},
+        "xml:lang": ["de", "fr", "en"],
+        "STRINGS": [
+            "28320 DISTINCT VALUES",
+            "freundlich, höflich sein, werden",
+            "coup (sans doute un coup qui rend sourd)",
+            "wachsen lassen, hervorbringen",
+            "...",
+        ],
     },
-    "ref": {
-        "attrs": {"target", "xml:lang", "type"},
-    },
-    "usg": {
-        "attrs": {"type"},
-    },
-    "subc": {},
-    "gen": {},
-    "pos": {},
-    "number": {},
-    "oRef": {},
-    "orth": {},
-    "bibl": {},
 }
+
 
 argparser = argparse.ArgumentParser(description="Process the Coptic Lexicon.")
 
@@ -75,23 +155,57 @@ argparser.add_argument(
 args = argparser.parse_args()
 
 
+def format_set(s: set) -> dict:
+    s = list(s)
+    if len(s) > MAX_NUM_ATTR_VALUES:
+        return [f"{len(s)} DISTINCT VALUES"] + s[:3] + ["..."]
+    return s
+
+
+@type_enforced.Enforcer
+def prettify(d: dict) -> str:
+    out = json.dumps(d, indent=2, ensure_ascii=False).encode("utf8").decode()
+    out = LIST_ELEMENT_CLOSING_QUOTE.sub('", ', out)
+    return out
+    # TODO: Try to return the output with the keys in the following order:
+    # [
+    #     "superEntry", "entry", "form", "sense", "note", "etym", "gramGrp", "xr",
+    #     "cit", "gram", "def", "quote", "ref", "usg", "subc", "gen", "pos",
+    #     "number", "oRef", "orth", "bibl",
+    # ]
+
+
 @type_enforced.Enforcer
 def analyze(soup: bs4.BeautifulSoup | bs4.Tag) -> None:
     all_tag_names = {tag.name for tag in soup.descendants if isinstance(tag, bs4.Tag)}
 
     tree = {}
     for name in all_tag_names:
-        attrs = set()
+        attrs = dict()
         children = set()
+        strings = set()
         for tag in soup.find_all(name):
-            attrs.update(tag.attrs.keys())
-            children.update(c.name for c in tag.children if isinstance(c, bs4.Tag))
+            for key, value in tag.attrs.items():
+                if key not in attrs:
+                    attrs[key] = set()
+                attrs[key].add(value)
+            for c in tag.children:
+                if isinstance(c, bs4.Tag):
+                    children.add(c.name)
+                elif isinstance(c, bs4.NavigableString):
+                    c = str(c).strip()
+                    if c:
+                        strings.add(c)
+                else:
+                    raise ValueError(f"Unknown child type: {c}")
         tree[name] = {}
         if attrs:
-            tree[name]["attrs"] = attrs
+            tree[name] = {k: format_set(v) for k, v in attrs.items()}
+        if strings:
+            tree[name]["STRINGS"] = format_set(strings)
         if children:
-            tree[name]["children"] = children
-    pprint.pprint(tree)
+            tree[name]["CHILDREN"] = list(children)
+    print(prettify(tree))
 
 
 @type_enforced.Enforcer
@@ -107,6 +221,8 @@ def main():
             continue
         assert isinstance(child, bs4.Tag)
         assert child.name in ["entry", "superEntry"], child
+
+    analyze(soup)
 
 
 if __name__ == "__main__":
