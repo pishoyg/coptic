ad2b73a83dfb2dadf99b29c114fa22b9620dbcea
Pishoy Girgis <pishoybg@gmail.com>
2024-06-30 11:10:27 +0200

    Marcion: Add a placeholder for the `inflect` module.

---
+++ dictionary/marcion.sourceforge.net/inflect.py
@@ -0,0 +1,8 @@
+import word as lexical
+
+
+def inflect(word: lexical.structured_word) -> list[str]:
+    """
+    Given a word, return a list of inflected forms.
+    """
+    raise NotImplementedError("Not implemented!")
