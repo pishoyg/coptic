a0b1888b253da27caa54af848497fc97c58239e1
Pishoy Girgis <pishoybg@gmail.com>
2024-07-02 10:32:20 +0200

    Use `magick` instead of the deprecated `convert`.

--- dictionary/marcion.sourceforge.net/convert_resize.sh
+++ dictionary/marcion.sourceforge.net/convert_resize.sh
@@ -3,5 +3,5 @@ WIDTH="300"
 DST="dictionary/marcion.sourceforge.net/data/img-${WIDTH}"
 
 for FILE in $(ls "${SRC}"); do
-  convert "${SRC}/${FILE}" -resize "${WIDTH}x" "${DST}/${FILE/.png/.jpg}"
+  magick "${SRC}/${FILE}" -resize "${WIDTH}x" "${DST}/${FILE/.png/.jpg}"
 done
