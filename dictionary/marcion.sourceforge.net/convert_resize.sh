832fc4fe4b112e9d38697de4eff0846c33628503
Pishoy Girgis <pishoybg@gmail.com>
2024-07-03 21:38:20 +0200

    Add an image. Slight update to the script.

--- dictionary/marcion.sourceforge.net/convert_resize.sh
+++ dictionary/marcion.sourceforge.net/convert_resize.sh
@@ -1,7 +1,14 @@
+# TODO: Add a flag to make it possible to skip the images that already exist in
+# the output.
 SRC="dictionary/marcion.sourceforge.net/data/img"
 WIDTH="300"
 DST="dictionary/marcion.sourceforge.net/data/img-${WIDTH}"
 
-for FILE in $(ls "${SRC}"); do
+process() {
+  FILE="$1"
   magick "${SRC}/${FILE}" -resize "${WIDTH}x" "${DST}/${FILE/.png/.jpg}"
+}
+
+for FILE in $(ls "${SRC}"); do
+  process "${FILE}"
 done
