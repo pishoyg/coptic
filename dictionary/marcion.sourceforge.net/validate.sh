294e60282b453b11d5af848db82658e7e1864fa0
Pishoy Girgis <pishoybg@gmail.com>
2024-07-09 11:01:58 +0200

    Update the Marcion validation script.

--- dictionary/marcion.sourceforge.net/validate.sh
+++ dictionary/marcion.sourceforge.net/validate.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
+IMG="dictionary/marcion.sourceforge.net/data/img/"
+IMG_300="dictionary/marcion.sourceforge.net/data/img-300/"
+
+img_extensions () {
+  ls "${IMG}" | grep -o '\..*' | tr '[:upper:]' '[:lower:]' | sort
+}
+
+img_basenames () {
+  ls "${IMG}" | grep -oE '^[^.]+' | sort
+}
+
+img_300_basenames () {
+  ls "${IMG-300}" | grep -oE '^[^.]+' | sort
+}
+
 # Checking for unknown image extensions:
-DIFF=$(comm -23 <( ls dictionary/marcion.sourceforge.net/data/img/ | grep -o '\..*' | tr '[:upper:]' '[:lower:]' | sort | uniq ) <( echo .avif .gif .jpeg .jpg .png .webp | tr ' ' '\n' | sort ))
+DIFF=$(comm -23 <(img_extensions | uniq) <(echo .avif .gif .jpeg .jpg .png .webp | tr ' ' '\n' | sort))
 
 if [ ! -z "${DIFF}" ]; then
   echo "Unknown extensions:"
@@ -10,7 +25,7 @@ if [ ! -z "${DIFF}" ]; then
 fi
 
 # Check that all images are converted.
-DIFF=$(comm -23 <( ls dictionary/marcion.sourceforge.net/data/img/ | grep -oE '^[^.]+' | sort) <( ls dictionary/marcion.sourceforge.net/data/img-300/ | grep -oE '^[^.]+' | sort))
+DIFF=$(comm -3 <(img_basenames) <(img_300_basenames))
 if [ ! -z "${DIFF}" ]; then
   echo "Images that are not converted:"
   echo "${DIFF}"
@@ -18,4 +33,9 @@ if [ ! -z "${DIFF}" ]; then
 fi
 
 # Check that there are no identical IDs.
-# TODO: Implement.
+DIFF=$(comm -3 <(img_basenames) <(img_basenames))
+if [ ! -z "${DIFF}" ]; then
+  echo "The following basenames are duplicate:"
+  echo "${DIFF}"
+  exit 1
+fi
