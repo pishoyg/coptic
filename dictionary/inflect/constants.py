fe16c88f0ce4becfade6e1edd096466086b317ed
Pishoy Girgis <pishoybg@gmail.com>
2024-07-11 16:46:26 +0200

    Inflect: Expand the list of constants.
    Privatize the lists that are not intended to be used outside of the
    constants package.

--- dictionary/inflect/constants.py
+++ dictionary/inflect/constants.py
@@ -1,335 +1,83 @@
-# TODO: Some of the prefixes below convert the verbs to nouns, and they could
-# therefore accept noun prefixes afterwards. Those extra prefixes should be
-# accounted for as well. Design a neat solution to handle that.
+# TODO: Handle compound prefixes.
 
-# TODO: Populate the noun prefixes.
-# TODO: Split the verb prefixes by tense.
+###############################################################################
+# LEVEL-1 CONSTANTS ###########################################################
+###############################################################################
 
-NOUN_DEFINITE = []
-NOUN_INDEFINITE = []
-NOUN_DEMONSTRATIVE = []
-NOUN_POSSESSIVE = []
+_NOUN_GENERAL = [
+    "ⲉ̀",
+    "ⲛ̀",
+    "ⲙ̀",
+]
 
-VERB_PREFIXES = [
+_NOUN_INDEFINITE_SINGULAR = [
+    "ⲟⲩ",
+]
+
+_NOUN_INDEFINITE_PLURAL = [
+    "ϩⲁⲛ",
+]
+
+_NOUN_MASCULINE = [
+    # Definite
+    "ⲡ̀",
+    "ⲫ̀",
+    # Demonstrative
+    "ⲡⲁⲓ",
+    # Possessive
+    "ⲡⲁ",
+    "ⲡⲉⲛ",
+    "ⲡⲉⲕ",
+    "ⲡⲉ",
+    "ⲡⲉⲧⲉⲛ",
+    "ⲡⲉϥ",
+    "ⲡⲉⲥ",
+    "ⲡⲟⲩ",
+]
+
+_NOUN_FEMININE = [
+    # Definite
+    "ⲧ̀",
+    "ⲑ̀",
+    # Demonstrative
+    "ⲧⲁⲓ",
+    # Possessive
+    "ⲧⲁ",
+    "ⲧⲉⲛ",
+    "ⲧⲉⲕ",
+    "ⲧⲉ",
+    "ⲧⲉⲧⲉⲛ",
+    "ⲧⲉϥ",
+    "ⲧⲉⲥ",
+    "ⲧⲟⲩ",
+]
+
+_NOUN_PLURAL = [
+    # Definite
+    "ⲛⲓ",
+    "ⲛⲉⲛ",
+    # Demonstrative
+    "ⲛⲁⲓ",
+    # Possessive
+    "ⲛⲁ",
+    "ⲛⲉⲛ",
+    "ⲛⲉⲕ",
+    "ⲛⲉ",
+    "ⲛⲉⲧⲉⲛ",
+    "ⲛⲉϥ",
+    "ⲛⲉⲥ",
+    "ⲛⲟⲩ",
+]
+
+_VERB_GENERAL = [
     # Articles.
     "ⲉ̀",
     "ⲛ̀",
     "ⲙ̀",
-    # Present
-    "ϯ",
-    "ⲭ̀",
-    "ⲕ̀",
-    "ⲧⲉ",
-    "ϥ̀",
-    "ⲥ̀",
-    "ⲧⲉⲛ",
-    "ⲧⲉⲧⲉⲛ",
-    "ⲥⲉ",
-    # Negative present
-    "ⲛ̀ϯ",
-    "ⲛ̀ⲭ̀",
-    "ⲛ̀ⲕ̀",
-    "ⲛ̀ⲧⲉ",
-    "ⲛ̀ϥ̀",
-    "ⲛ̀ⲥ̀",
-    "ⲛ̀ⲧⲉⲛ",
-    "ⲛ̀ⲧⲉⲧⲉⲛ",
-    "ⲛ̀ⲥⲉ",
-    # Circumstantial present
-    "ⲉⲓ",
-    "ⲉⲕ",
-    "ⲉⲣⲉ",
-    "ⲉϥ",
-    "ⲉⲥ",
-    "ⲉⲛ",
-    "ⲉⲣⲉⲧⲉⲛ",
-    "ⲉⲩ",
-    # Relative present
-    "ⲉϯ",
-    "ⲉⲧⲉⲕ",
-    "ⲉⲧⲉ",
-    "ⲉⲧⲉϥ",
-    "ⲉⲧⲉⲥ",
-    "ⲉⲧⲉⲛ",
-    "ⲉⲧⲉⲧⲉⲛ",
-    "ⲉⲧⲟⲩ",
-    # Focalized present (N.B. Same form as perfect)
-    "ⲁⲓ",
-    "ⲁⲕ",
-    "ⲁⲣⲉ",
-    "ⲁϥ",
-    "ⲁⲥ",
-    "ⲁⲛ",
-    "ⲁⲣⲉⲧⲉⲛ",
-    "ⲁⲧⲉⲧⲉⲛ",
-    "ⲁⲩ",
-    # Preterite present
-    "ⲛⲁⲓ",
-    "ⲛⲁⲕ",
-    "ⲛⲁⲣⲉ",
-    "ⲛⲁϥ",
-    "ⲛⲁⲥ",
-    "ⲛⲁⲛ",
-    "ⲛⲁⲣⲉⲧⲉⲛ",
-    "ⲛⲁⲩ",
-    # Circumstantial preterite present
-    "ⲉ̀ⲛⲁⲓ",
-    "ⲉ̀ⲛⲁⲕ",
-    "ⲉ̀ⲛⲁⲣⲉ",
-    "ⲉ̀ⲛⲁϥ",
-    "ⲉ̀ⲛⲁⲥ",
-    "ⲉ̀ⲛⲁⲛ",
-    "ⲉ̀ⲛⲁⲣⲉⲧⲉⲛ",
-    "ⲉ̀ⲛⲁⲩ",
-    # Future
-    "ϯⲛⲁ",
-    "ⲭ̀ⲛⲁ",
-    "ⲧⲉⲣⲁ",
-    "ϥ̀ⲛⲁ",
-    "ⲥ̀ⲛⲁ",
-    "ⲧⲉⲛⲛⲁ",
-    "ⲧⲉⲧⲉⲛⲛⲁ",
-    "ⲥⲉⲛⲁ",
-    # Negative future
-    "ⲛ̀ϯⲛⲁ",
-    "ⲛ̀ⲭ̀ⲛⲁ",
-    "ⲛ̀ⲧⲉⲣⲁ",
-    "ⲛ̀ϥ̀ⲛⲁ",
-    "ⲛ̀ⲥ̀ⲛⲁ",
-    "ⲛ̀ⲧⲉⲛⲛⲁ",
-    "ⲛ̀ⲧⲉⲧⲉⲛⲛⲁ",
-    "ⲛ̀ⲥⲉⲛⲁ",
-    # Circumstantial future
-    "ⲉⲓⲛⲁ",
-    "ⲉⲭⲛⲁ",
-    "ⲉⲣⲉⲛⲁ",
-    "ⲉϥⲛⲁ",
-    "ⲉⲥⲛⲁ",
-    "ⲉⲛⲛⲁ",
-    "ⲉⲣⲉⲧⲉⲛⲛⲁ",
-    "ⲉⲩⲛⲁ",
-    # Relative future
-    "ⲉϯⲛⲁ",
-    "ⲉⲧⲉⲕⲛⲁ",
-    "ⲉⲧⲉⲛⲁ",
-    "ⲉⲧⲉϥⲛⲁ",
-    "ⲉⲧⲉⲥⲛⲁ",
-    "ⲉⲧⲉⲛⲛⲁ",
-    "ⲉⲧⲉⲧⲉⲛⲛⲁ",
-    "ⲉⲧⲟⲩⲛⲁ",
-    # Focalized future
-    "ⲁⲓⲛⲁ",
-    "ⲁⲕⲛⲁ",
-    "ⲁⲣⲉⲛⲁ",
-    "ⲁϥⲛⲁ",
-    "ⲁⲥⲛⲁ",
-    "ⲁⲛⲛⲁ",
-    "ⲁⲣⲉⲧⲉⲛⲛⲁ",
-    "ⲁⲩⲛⲁ",
-    # Preterite future
-    "ⲛⲁⲓⲛⲁ",
-    "ⲛⲁⲕⲛⲁ",
-    "ⲛⲁⲣⲉⲛⲁ",
-    "ⲛⲁϥⲛⲁ",
-    "ⲛⲁⲥⲛⲁ",
-    "ⲛⲁⲛⲛⲁ",
-    "ⲛⲁⲣⲉⲧⲉⲛⲛⲁ",
-    "ⲛⲁⲩⲛⲁ",
-    # Circumstantial preterite future
-    "ⲉ̀ⲛⲁⲓⲛⲁ",
-    "ⲉ̀ⲛⲁⲕⲛⲁ",
-    "ⲉ̀ⲛⲁⲣⲉⲛⲁ",
-    "ⲉ̀ⲛⲁϥⲛⲁ",
-    "ⲉ̀ⲛⲁⲥⲛⲁ",
-    "ⲉ̀ⲛⲁⲛⲛⲁ",
-    "ⲉ̀ⲛⲁⲣⲉⲧⲉⲛⲛⲁ",
-    "ⲉ̀ⲛⲁⲩⲛⲁ",
-    # Perfect
-    "ⲁⲓ",
-    "ⲁⲕ",
-    "ⲁⲣⲉ",
-    "ⲁϥ",
-    "ⲁⲥ",
-    "ⲁⲛ",
-    "ⲁⲣⲉⲧⲉⲛ",
-    "ⲁⲧⲉⲧⲉⲛ",
-    "ⲁⲩ",
-    # Negative perfect
-    "ⲙ̀ⲡⲓ",
-    "ⲙ̀ⲡⲉⲕ",
-    "ⲙ̀ⲡⲉ",
-    "ⲙ̀ⲡⲉϥ",
-    "ⲙ̀ⲡⲉⲥ",
-    "ⲙ̀ⲡⲉⲛ",
-    "ⲙ̀ⲡⲉⲧⲉⲛ",
-    "ⲙ̀ⲡⲟⲩ",
-    # Circumstantial perfect
-    "ⲉ̀ⲁⲓ",
-    "ⲉ̀ⲁⲕ",
-    "ⲉ̀ⲁⲣⲉ",
-    "ⲉ̀ⲁϥ",
-    "ⲉ̀ⲁⲥ",
-    "ⲉ̀ⲁⲛ",
-    "ⲉ̀ⲁⲣⲉⲧⲉⲛ",
-    "ⲉ̀ⲁⲧⲉⲧⲉⲛ",
-    "ⲉ̀ⲁⲩ",
-    # Relative / Focalized perfect
-    "ⲉⲧⲁⲓ",
-    "ⲉⲧⲁⲕ",
-    "ⲉⲧⲁⲣⲉ",
-    "ⲉⲧⲁϥ",
-    "ⲉⲧⲁⲥ",
-    "ⲉⲧⲁⲛ",
-    "ⲉⲧⲁⲣⲉⲧⲉⲛ",
-    "ⲉⲧⲁⲩ",
-    # Habitual
-    "ϣⲁⲓ",
-    "ϣⲁⲕ",
-    "ϣⲁⲣⲉ",
-    "ϣⲁϥ",
-    "ϣⲁⲥ",
-    "ϣⲁⲛ",
-    "ϣⲁⲣⲉⲧⲉⲛ",
-    "ϣⲁⲩ",
-    # Negative habitual
-    "ⲙ̀ⲡⲁⲓ",
-    "ⲙ̀ⲡⲁⲕ",
-    "ⲙ̀ⲡⲁⲣⲉ",
-    "ⲙ̀ⲡⲁϥ",
-    "ⲙ̀ⲡⲁⲥ",
-    "ⲙ̀ⲡⲁⲛ",
-    "ⲙ̀ⲡⲁⲣⲉⲧⲉⲛ",
-    "ⲙ̀ⲡⲁⲩ",
-    # Circumstantial habitual
-    "ⲉ̀ϣⲁⲓ",
-    "ⲉ̀ϣⲁⲕ",
-    "ⲉ̀ϣⲁⲣⲉ",
-    "ⲉ̀ϣⲁϥ",
-    "ⲉ̀ϣⲁⲥ",
-    "ⲉ̀ϣⲁⲛ",
-    "ⲉ̀ϣⲁⲣⲉⲧⲉⲛ",
-    "ⲉ̀ϣⲁⲩ",
-    # Optative
-    "ⲉⲓⲉ̀",
-    "ⲉⲕⲉ̀",
-    "ⲉⲣⲉⲉ̀",
-    "ⲉϥⲉ̀",
-    "ⲉⲥⲉ̀",
-    "ⲉⲛⲉ̀",
-    "ⲉⲣⲉⲧⲉⲛⲉ̀",
-    "ⲉⲩⲉ̀",
-    # Negative optative
-    "ⲛ̀ⲛⲁ",
-    "ⲛ̀ⲛⲉⲕ",
-    "ⲛ̀ⲛⲉ",
-    "ⲛ̀ⲛⲉϥ",
-    "ⲛ̀ⲛⲉⲥ",
-    "ⲛ̀ⲛⲉⲛ",
-    "ⲛ̀ⲛⲉⲧⲉⲛ",
-    "ⲛ̀ⲛⲟⲩ",
-    # Conditional
-    "ⲁⲓϣⲁⲛ",
-    "ⲁⲕϣⲁⲛ",
-    "ⲁⲣⲉϣⲁⲛ",
-    "ⲁϥϣⲁⲛ",
-    "ⲁⲥϣⲁⲛ",
-    "ⲁⲛϣⲁⲛ",
-    "ⲁⲣⲉⲧⲉⲛϣⲁⲛ",
-    "ⲁⲩϣⲁⲛ",
-    # Negative conditional
-    "ⲁⲓϣ̀ⲧⲉⲙ",
-    "ⲁⲕϣ̀ⲧⲉⲙ",
-    "ⲁⲣⲉϣ̀ⲧⲉⲙ",
-    "ⲁϥϣ̀ⲧⲉⲙ",
-    "ⲁⲥϣ̀ⲧⲉⲙ",
-    "ⲁⲛϣ̀ⲧⲉⲙ",
-    "ⲁⲣⲉⲧⲉⲛϣ̀ⲧⲉⲙ",
-    "ⲁⲩϣ̀ⲧⲉⲙ",
-    # Until - ϣⲁⲛ̀ⲧⲉ
-    "ϣⲁⲛ̀ⲧⲁ",
-    "ϣⲁⲛ̀ⲧⲉⲕ",
-    "ϣⲁⲛ̀ⲧⲉ",
-    "ϣⲁⲛ̀ⲧⲉϥ",
-    "ϣⲁⲛ̀ⲧⲉⲥ",
-    "ϣⲁⲛ̀ⲧⲉⲛ",
-    "ϣⲁⲛ̀ⲧⲉⲧⲉⲛ",
-    "ϣⲁⲛ̀ⲧⲟⲩ",
-    # Until - ϣⲁⲧⲉ
-    "ϣⲁϯ",
-    "ϣⲁⲧⲉⲕ",
-    "ϣⲁⲧⲉ",
-    "ϣⲁⲧⲉϥ",
-    "ϣⲁⲧⲉⲥ",
-    "ϣⲁⲧⲉⲛ",
-    "ϣⲁⲧⲉⲧⲉⲛ",
-    "ϣⲁⲧⲟⲩ",
-    # Not yet - ⲙ̀ⲡⲁⲧⲉ
-    "ⲙ̀ⲡⲁϯ",
-    "ⲙ̀ⲡⲁⲧⲉⲕ",
-    "ⲙ̀ⲡⲁⲧⲉ",
-    "ⲙ̀ⲡⲁⲧⲉϥ",
-    "ⲙ̀ⲡⲁⲧⲉⲥ",
-    "ⲙ̀ⲡⲁⲧⲉⲛ",
-    "ⲙ̀ⲡⲁⲧⲉⲧⲉⲛ",
-    "ⲙ̀ⲡⲁⲧⲟⲩ",
-    # Causative
-    "ⲑ̀ⲣⲓ",
-    "ⲑ̀ⲣⲉⲕ",
-    "ⲑ̀ⲣⲉ",
-    "ⲑ̀ⲣⲉϥ",
-    "ⲑ̀ⲣⲉⲥ",
-    "ⲑ̀ⲣⲉⲛ",
-    "ⲑ̀ⲣⲉⲧⲉⲛ",
-    "ⲑ̀ⲣⲟⲩ",
-    # Causative
-    "ⲉ̀ⲑⲣⲓ",
-    "ⲉ̀ⲑⲣⲉⲕ",
-    "ⲉ̀ⲑⲣⲉ",
-    "ⲉ̀ⲑⲣⲉϥ",
-    "ⲉ̀ⲑⲣⲉⲥ",
-    "ⲉ̀ⲑⲣⲉⲛ",
-    "ⲉ̀ⲑⲣⲉⲧⲉⲛ",
-    "ⲉ̀ⲑⲣⲟⲩ",
-    # Conjunctive
-    "ⲛ̀ⲧⲁ",
-    "ⲛ̀ⲧⲉⲕ",
-    "ⲛ̀ⲧⲉ",
-    "ⲛ̀ⲧⲉϥ",
-    "ⲛ̀ⲧⲉⲥ",
-    "ⲛ̀ⲧⲉⲛ",
-    "ⲛ̀ⲧⲉⲧⲉⲛ",
-    "ⲛ̀ⲧⲟⲩ",
-    # Negative conjunctive
-    "ⲛ̀ⲧⲁϣ̀ⲧⲉⲙ",
-    "ⲛ̀ⲧⲉⲕϣ̀ⲧⲉⲙ",
-    "ⲛ̀ⲧⲉϣ̀ⲧⲉⲙ",
-    "ⲛ̀ⲧⲉϥϣ̀ⲧⲉⲙ",
-    "ⲛ̀ⲧⲉⲥϣ̀ⲧⲉⲙ",
-    "ⲛ̀ⲧⲉⲛϣ̀ⲧⲉⲙ",
-    "ⲛ̀ⲧⲉⲧⲉⲛϣ̀ⲧⲉⲙ",
-    "ⲛ̀ⲧⲟⲩϣ̀ⲧⲉⲙ",
-    # Jussive
-    "ⲙⲁⲣⲓ",
-    "ⲙⲁⲣⲉⲕ",
-    "ⲙⲁⲣⲉ",
-    "ⲙⲁⲣⲉϥ",
-    "ⲙⲁⲣⲉⲥ",
-    "ⲙⲁⲣⲉⲛ",
-    "ⲙⲁⲣⲉⲧⲉⲛ",
-    "ⲙⲁⲣⲟⲩ",
-    # Negative jussive
-    "ⲙ̀ⲡⲉⲛⲑ̀ⲣⲓ",
-    "ⲙ̀ⲡⲉⲛⲑ̀ⲣⲉⲕ",
-    "ⲙ̀ⲡⲉⲛⲑ̀ⲣⲉ",
-    "ⲙ̀ⲡⲉⲛⲑ̀ⲣⲉϥ",
-    "ⲙ̀ⲡⲉⲛⲑ̀ⲣⲉⲥ",
-    "ⲙ̀ⲡⲉⲛⲑ̀ⲣⲉⲛ",
-    "ⲙ̀ⲡⲉⲛⲑ̀ⲣⲉⲧⲉⲛ",
-    "ⲙ̀ⲡⲉⲛⲑ̀ⲣⲟⲩ",
 ]
 
-VERB_TO_NOUN_PREFIXES = [
-    # Use of the verb as a noun.
+_VERB_TO_NOUN = [
+    # Definite articles.
     "ⲡⲓ",
     "ⲡ̀",
     # ⲣⲉϥ
@@ -355,4 +103,472 @@ VERB_TO_NOUN_PREFIXES = [
     "ⲡ̀ϫⲓⲛⲑ̀ⲣⲉⲛ",
     "ⲡ̀ϫⲓⲛⲑ̀ⲣⲉⲧⲉⲛ",
     "ⲡ̀ϫⲓⲛⲑ̀ⲣⲟⲩ",
+]
+
+_TENSE_PRESENT = [
+    "ϯ",
+    "ⲭ̀",
+    "ⲕ̀",
+    "ⲧⲉ",
+    "ϥ̀",
+    "ⲥ̀",
+    "ⲧⲉⲛ",
+    "ⲧⲉⲧⲉⲛ",
+    "ⲥⲉ",
+]
+
+_TENSE_NEGATIVE_PRESENT = [
+    "ⲛ̀ϯ",
+    "ⲛ̀ⲭ̀",
+    "ⲛ̀ⲕ̀",
+    "ⲛ̀ⲧⲉ",
+    "ⲛ̀ϥ̀",
+    "ⲛ̀ⲥ̀",
+    "ⲛ̀ⲧⲉⲛ",
+    "ⲛ̀ⲧⲉⲧⲉⲛ",
+    "ⲛ̀ⲥⲉ",
+]
+
+_TENSE_CIRCUMSTANTIAL_PRESENT = [
+    "ⲉⲓ",
+    "ⲉⲕ",
+    "ⲉⲣⲉ",
+    "ⲉϥ",
+    "ⲉⲥ",
+    "ⲉⲛ",
+    "ⲉⲣⲉⲧⲉⲛ",
+    "ⲉⲩ",
+]
+
+_TENSE_RELATIVE_PRESENT = [
+    "ⲉϯ",
+    "ⲉⲧⲉⲕ",
+    "ⲉⲧⲉ",
+    "ⲉⲧⲉϥ",
+    "ⲉⲧⲉⲥ",
+    "ⲉⲧⲉⲛ",
+    "ⲉⲧⲉⲧⲉⲛ",
+    "ⲉⲧⲟⲩ",
+]
+
+_TENSE_FOCALIZED_PRESENT_ = [
+    "ⲁⲓ",
+    "ⲁⲕ",
+    "ⲁⲣⲉ",
+    "ⲁϥ",
+    "ⲁⲥ",
+    "ⲁⲛ",
+    "ⲁⲣⲉⲧⲉⲛ",
+    "ⲁⲧⲉⲧⲉⲛ",
+    "ⲁⲩ",
+]
+
+_TENSE_PRETERITE_PRESENT = [
+    "ⲛⲁⲓ",
+    "ⲛⲁⲕ",
+    "ⲛⲁⲣⲉ",
+    "ⲛⲁϥ",
+    "ⲛⲁⲥ",
+    "ⲛⲁⲛ",
+    "ⲛⲁⲣⲉⲧⲉⲛ",
+    "ⲛⲁⲩ",
+]
+
+_TENSE_CIRCUMSTANTIAL_PRETERITE_PRESENT = [
+    "ⲉ̀ⲛⲁⲓ",
+    "ⲉ̀ⲛⲁⲕ",
+    "ⲉ̀ⲛⲁⲣⲉ",
+    "ⲉ̀ⲛⲁϥ",
+    "ⲉ̀ⲛⲁⲥ",
+    "ⲉ̀ⲛⲁⲛ",
+    "ⲉ̀ⲛⲁⲣⲉⲧⲉⲛ",
+    "ⲉ̀ⲛⲁⲩ",
+]
+
+_TENSE_FUTURE = [
+    "ϯⲛⲁ",
+    "ⲭ̀ⲛⲁ",
+    "ⲧⲉⲣⲁ",
+    "ϥ̀ⲛⲁ",
+    "ⲥ̀ⲛⲁ",
+    "ⲧⲉⲛⲛⲁ",
+    "ⲧⲉⲧⲉⲛⲛⲁ",
+    "ⲥⲉⲛⲁ",
+]
+
+_TENSE_NEGATIVE_FUTURE = [
+    "ⲛ̀ϯⲛⲁ",
+    "ⲛ̀ⲭ̀ⲛⲁ",
+    "ⲛ̀ⲧⲉⲣⲁ",
+    "ⲛ̀ϥ̀ⲛⲁ",
+    "ⲛ̀ⲥ̀ⲛⲁ",
+    "ⲛ̀ⲧⲉⲛⲛⲁ",
+    "ⲛ̀ⲧⲉⲧⲉⲛⲛⲁ",
+    "ⲛ̀ⲥⲉⲛⲁ",
+]
+
+_TENSE_CIRCUMSTANTIAL_FUTURE = [
+    "ⲉⲓⲛⲁ",
+    "ⲉⲭⲛⲁ",
+    "ⲉⲣⲉⲛⲁ",
+    "ⲉϥⲛⲁ",
+    "ⲉⲥⲛⲁ",
+    "ⲉⲛⲛⲁ",
+    "ⲉⲣⲉⲧⲉⲛⲛⲁ",
+    "ⲉⲩⲛⲁ",
+]
+
+_TENSE_RELATIVE_FUTURE = [
+    "ⲉϯⲛⲁ",
+    "ⲉⲧⲉⲕⲛⲁ",
+    "ⲉⲧⲉⲛⲁ",
+    "ⲉⲧⲉϥⲛⲁ",
+    "ⲉⲧⲉⲥⲛⲁ",
+    "ⲉⲧⲉⲛⲛⲁ",
+    "ⲉⲧⲉⲧⲉⲛⲛⲁ",
+    "ⲉⲧⲟⲩⲛⲁ",
+]
+
+_TENSE_FOCALIZED_FUTURE = [
+    "ⲁⲓⲛⲁ",
+    "ⲁⲕⲛⲁ",
+    "ⲁⲣⲉⲛⲁ",
+    "ⲁϥⲛⲁ",
+    "ⲁⲥⲛⲁ",
+    "ⲁⲛⲛⲁ",
+    "ⲁⲣⲉⲧⲉⲛⲛⲁ",
+    "ⲁⲩⲛⲁ",
+]
+
+_TENSE_PRETERITE_FUTURE = [
+    "ⲛⲁⲓⲛⲁ",
+    "ⲛⲁⲕⲛⲁ",
+    "ⲛⲁⲣⲉⲛⲁ",
+    "ⲛⲁϥⲛⲁ",
+    "ⲛⲁⲥⲛⲁ",
+    "ⲛⲁⲛⲛⲁ",
+    "ⲛⲁⲣⲉⲧⲉⲛⲛⲁ",
+    "ⲛⲁⲩⲛⲁ",
+]
+
+_TENSE_CIRCUMSTANTIAL_PRETERITE_FUTURE = [
+    "ⲉ̀ⲛⲁⲓⲛⲁ",
+    "ⲉ̀ⲛⲁⲕⲛⲁ",
+    "ⲉ̀ⲛⲁⲣⲉⲛⲁ",
+    "ⲉ̀ⲛⲁϥⲛⲁ",
+    "ⲉ̀ⲛⲁⲥⲛⲁ",
+    "ⲉ̀ⲛⲁⲛⲛⲁ",
+    "ⲉ̀ⲛⲁⲣⲉⲧⲉⲛⲛⲁ",
+    "ⲉ̀ⲛⲁⲩⲛⲁ",
+]
+
+_TENSE_PERFECT = [
+    "ⲁⲓ",
+    "ⲁⲕ",
+    "ⲁⲣⲉ",
+    "ⲁϥ",
+    "ⲁⲥ",
+    "ⲁⲛ",
+    "ⲁⲣⲉⲧⲉⲛ",
+    "ⲁⲧⲉⲧⲉⲛ",
+    "ⲁⲩ",
+]
+
+_TENSE_NEGATIVE_PERFECT = [
+    "ⲙ̀ⲡⲓ",
+    "ⲙ̀ⲡⲉⲕ",
+    "ⲙ̀ⲡⲉ",
+    "ⲙ̀ⲡⲉϥ",
+    "ⲙ̀ⲡⲉⲥ",
+    "ⲙ̀ⲡⲉⲛ",
+    "ⲙ̀ⲡⲉⲧⲉⲛ",
+    "ⲙ̀ⲡⲟⲩ",
+]
+
+_TENSE_CIRCUMSTANTIAL_PERFECT = [
+    "ⲉ̀ⲁⲓ",
+    "ⲉ̀ⲁⲕ",
+    "ⲉ̀ⲁⲣⲉ",
+    "ⲉ̀ⲁϥ",
+    "ⲉ̀ⲁⲥ",
+    "ⲉ̀ⲁⲛ",
+    "ⲉ̀ⲁⲣⲉⲧⲉⲛ",
+    "ⲉ̀ⲁⲧⲉⲧⲉⲛ",
+    "ⲉ̀ⲁⲩ",
+]
+
+_TENSE_RELATIVE_OR_FOCALIZED_PERFECT = [
+    "ⲉⲧⲁⲓ",
+    "ⲉⲧⲁⲕ",
+    "ⲉⲧⲁⲣⲉ",
+    "ⲉⲧⲁϥ",
+    "ⲉⲧⲁⲥ",
+    "ⲉⲧⲁⲛ",
+    "ⲉⲧⲁⲣⲉⲧⲉⲛ",
+    "ⲉⲧⲁⲩ",
+]
+
+_TENSE_HABITUAL = [
+    "ϣⲁⲓ",
+    "ϣⲁⲕ",
+    "ϣⲁⲣⲉ",
+    "ϣⲁϥ",
+    "ϣⲁⲥ",
+    "ϣⲁⲛ",
+    "ϣⲁⲣⲉⲧⲉⲛ",
+    "ϣⲁⲩ",
+]
+
+_TENSE_NEGATIVE_HABITUAL = [
+    "ⲙ̀ⲡⲁⲓ",
+    "ⲙ̀ⲡⲁⲕ",
+    "ⲙ̀ⲡⲁⲣⲉ",
+    "ⲙ̀ⲡⲁϥ",
+    "ⲙ̀ⲡⲁⲥ",
+    "ⲙ̀ⲡⲁⲛ",
+    "ⲙ̀ⲡⲁⲣⲉⲧⲉⲛ",
+    "ⲙ̀ⲡⲁⲩ",
+]
+
+_TENSE_CIRCUMSTANTIAL_HABITUAL = [
+    "ⲉ̀ϣⲁⲓ",
+    "ⲉ̀ϣⲁⲕ",
+    "ⲉ̀ϣⲁⲣⲉ",
+    "ⲉ̀ϣⲁϥ",
+    "ⲉ̀ϣⲁⲥ",
+    "ⲉ̀ϣⲁⲛ",
+    "ⲉ̀ϣⲁⲣⲉⲧⲉⲛ",
+    "ⲉ̀ϣⲁⲩ",
+]
+
+_TENSE_OPTATIVE = [
+    "ⲉⲓⲉ̀",
+    "ⲉⲕⲉ̀",
+    "ⲉⲣⲉⲉ̀",
+    "ⲉϥⲉ̀",
+    "ⲉⲥⲉ̀",
+    "ⲉⲛⲉ̀",
+    "ⲉⲣⲉⲧⲉⲛⲉ̀",
+    "ⲉⲩⲉ̀",
+]
+
+_TENSE_NEGATIVE_OPTATIVE = [
+    "ⲛ̀ⲛⲁ",
+    "ⲛ̀ⲛⲉⲕ",
+    "ⲛ̀ⲛⲉ",
+    "ⲛ̀ⲛⲉϥ",
+    "ⲛ̀ⲛⲉⲥ",
+    "ⲛ̀ⲛⲉⲛ",
+    "ⲛ̀ⲛⲉⲧⲉⲛ",
+    "ⲛ̀ⲛⲟⲩ",
+]
+
+_TENSE_CONDITIONAL = [
+    "ⲁⲓϣⲁⲛ",
+    "ⲁⲕϣⲁⲛ",
+    "ⲁⲣⲉϣⲁⲛ",
+    "ⲁϥϣⲁⲛ",
+    "ⲁⲥϣⲁⲛ",
+    "ⲁⲛϣⲁⲛ",
+    "ⲁⲣⲉⲧⲉⲛϣⲁⲛ",
+    "ⲁⲩϣⲁⲛ",
+]
+
+_TENSE_NEGATIVE_CONDITIONAL = [
+    "ⲁⲓϣ̀ⲧⲉⲙ",
+    "ⲁⲕϣ̀ⲧⲉⲙ",
+    "ⲁⲣⲉϣ̀ⲧⲉⲙ",
+    "ⲁϥϣ̀ⲧⲉⲙ",
+    "ⲁⲥϣ̀ⲧⲉⲙ",
+    "ⲁⲛϣ̀ⲧⲉⲙ",
+    "ⲁⲣⲉⲧⲉⲛϣ̀ⲧⲉⲙ",
+    "ⲁⲩϣ̀ⲧⲉⲙ",
+]
+
+_TENSE_UNTIL = [
+    "ϣⲁϯ",
+    "ϣⲁⲧⲉⲕ",
+    "ϣⲁⲧⲉ",
+    "ϣⲁⲧⲉϥ",
+    "ϣⲁⲧⲉⲥ",
+    "ϣⲁⲧⲉⲛ",
+    "ϣⲁⲧⲉⲧⲉⲛ",
+    "ϣⲁⲧⲟⲩ",
+]
+
+_TENSE_UNTIL_N = [
+    "ϣⲁⲛ̀ⲧⲁ",
+    "ϣⲁⲛ̀ⲧⲉⲕ",
+    "ϣⲁⲛ̀ⲧⲉ",
+    "ϣⲁⲛ̀ⲧⲉϥ",
+    "ϣⲁⲛ̀ⲧⲉⲥ",
+    "ϣⲁⲛ̀ⲧⲉⲛ",
+    "ϣⲁⲛ̀ⲧⲉⲧⲉⲛ",
+    "ϣⲁⲛ̀ⲧⲟⲩ",
+]
+
+_TENSE_NOT_YET = [
+    "ⲙ̀ⲡⲁϯ",
+    "ⲙ̀ⲡⲁⲧⲉⲕ",
+    "ⲙ̀ⲡⲁⲧⲉ",
+    "ⲙ̀ⲡⲁⲧⲉϥ",
+    "ⲙ̀ⲡⲁⲧⲉⲥ",
+    "ⲙ̀ⲡⲁⲧⲉⲛ",
+    "ⲙ̀ⲡⲁⲧⲉⲧⲉⲛ",
+    "ⲙ̀ⲡⲁⲧⲟⲩ",
+]
+
+_TENSE_CAUSATIVE = [
+    "ⲑ̀ⲣⲓ",
+    "ⲑ̀ⲣⲉⲕ",
+    "ⲑ̀ⲣⲉ",
+    "ⲑ̀ⲣⲉϥ",
+    "ⲑ̀ⲣⲉⲥ",
+    "ⲑ̀ⲣⲉⲛ",
+    "ⲑ̀ⲣⲉⲧⲉⲛ",
+    "ⲑ̀ⲣⲟⲩ",
+]
+
+_TENSE_CAUSATIVE = [
+    "ⲉ̀ⲑⲣⲓ",
+    "ⲉ̀ⲑⲣⲉⲕ",
+    "ⲉ̀ⲑⲣⲉ",
+    "ⲉ̀ⲑⲣⲉϥ",
+    "ⲉ̀ⲑⲣⲉⲥ",
+    "ⲉ̀ⲑⲣⲉⲛ",
+    "ⲉ̀ⲑⲣⲉⲧⲉⲛ",
+    "ⲉ̀ⲑⲣⲟⲩ",
+]
+
+_TENSE_CONJUNCTIVE = [
+    "ⲛ̀ⲧⲁ",
+    "ⲛ̀ⲧⲉⲕ",
+    "ⲛ̀ⲧⲉ",
+    "ⲛ̀ⲧⲉϥ",
+    "ⲛ̀ⲧⲉⲥ",
+    "ⲛ̀ⲧⲉⲛ",
+    "ⲛ̀ⲧⲉⲧⲉⲛ",
+    "ⲛ̀ⲧⲟⲩ",
+]
+
+_TENSE_NEGATIVE_CONJUNCTIVE = [
+    "ⲛ̀ⲧⲁϣ̀ⲧⲉⲙ",
+    "ⲛ̀ⲧⲉⲕϣ̀ⲧⲉⲙ",
+    "ⲛ̀ⲧⲉϣ̀ⲧⲉⲙ",
+    "ⲛ̀ⲧⲉϥϣ̀ⲧⲉⲙ",
+    "ⲛ̀ⲧⲉⲥϣ̀ⲧⲉⲙ",
+    "ⲛ̀ⲧⲉⲛϣ̀ⲧⲉⲙ",
+    "ⲛ̀ⲧⲉⲧⲉⲛϣ̀ⲧⲉⲙ",
+    "ⲛ̀ⲧⲟⲩϣ̀ⲧⲉⲙ",
+]
+
+_TENSE_JUSSIVE = [
+    "ⲙⲁⲣⲓ",
+    "ⲙⲁⲣⲉⲕ",
+    "ⲙⲁⲣⲉ",
+    "ⲙⲁⲣⲉϥ",
+    "ⲙⲁⲣⲉⲥ",
+    "ⲙⲁⲣⲉⲛ",
+    "ⲙⲁⲣⲉⲧⲉⲛ",
+    "ⲙⲁⲣⲟⲩ",
+]
+
+_TENSE_NEGATIVE_JUSSIVE = [
+    "ⲙ̀ⲡⲉⲛⲑ̀ⲣⲓ",
+    "ⲙ̀ⲡⲉⲛⲑ̀ⲣⲉⲕ",
+    "ⲙ̀ⲡⲉⲛⲑ̀ⲣⲉ",
+    "ⲙ̀ⲡⲉⲛⲑ̀ⲣⲉϥ",
+    "ⲙ̀ⲡⲉⲛⲑ̀ⲣⲉⲥ",
+    "ⲙ̀ⲡⲉⲛⲑ̀ⲣⲉⲛ",
+    "ⲙ̀ⲡⲉⲛⲑ̀ⲣⲉⲧⲉⲛ",
+    "ⲙ̀ⲡⲉⲛⲑ̀ⲣⲟⲩ",
+]
+
+
+###############################################################################
+# LEVEL-2 CONSTANTS ###########################################################
+###############################################################################
+
+ALL_NOUN = [
+    _NOUN_GENERAL,
+    _NOUN_INDEFINITE_SINGULAR,
+    _NOUN_INDEFINITE_PLURAL,
+    _NOUN_MASCULINE,
+    _NOUN_FEMININE,
+    _NOUN_PLURAL,
+]
+
+ALL_NOUN_MASCULINE = [
+    _NOUN_GENERAL,
+    _NOUN_INDEFINITE_SINGULAR,
+    _NOUN_INDEFINITE_PLURAL,
+    _NOUN_MASCULINE,
+    _NOUN_PLURAL,
+]
+
+ALL_NOUN_FEMININE = [
+    _NOUN_GENERAL,
+    _NOUN_INDEFINITE_SINGULAR,
+    _NOUN_INDEFINITE_PLURAL,
+    _NOUN_FEMININE,
+    _NOUN_PLURAL,
+]
+
+ALL_NOUN_PLURAL = [
+    _NOUN_GENERAL,
+    _NOUN_INDEFINITE_PLURAL,
+    _NOUN_PLURAL,
+]
+
+ALL_NOUN_NO_ARTICLE = [_NOUN_GENERAL]
+
+ALL_VERB = [
+    _VERB_GENERAL,
+    _VERB_TO_NOUN,
+    _TENSE_PRESENT,
+    _TENSE_NEGATIVE_PRESENT,
+    _TENSE_CIRCUMSTANTIAL_PRESENT,
+    _TENSE_RELATIVE_PRESENT,
+    _TENSE_FOCALIZED_PRESENT_,
+    _TENSE_PRETERITE_PRESENT,
+    _TENSE_CIRCUMSTANTIAL_PRETERITE_PRESENT,
+    _TENSE_FUTURE,
+    _TENSE_NEGATIVE_FUTURE,
+    _TENSE_CIRCUMSTANTIAL_FUTURE,
+    _TENSE_RELATIVE_FUTURE,
+    _TENSE_FOCALIZED_FUTURE,
+    _TENSE_PRETERITE_FUTURE,
+    _TENSE_CIRCUMSTANTIAL_PRETERITE_FUTURE,
+    _TENSE_PERFECT,
+    _TENSE_NEGATIVE_PERFECT,
+    _TENSE_CIRCUMSTANTIAL_PERFECT,
+    _TENSE_RELATIVE_OR_FOCALIZED_PERFECT,
+    _TENSE_HABITUAL,
+    _TENSE_NEGATIVE_HABITUAL,
+    _TENSE_CIRCUMSTANTIAL_HABITUAL,
+    _TENSE_OPTATIVE,
+    _TENSE_NEGATIVE_OPTATIVE,
+    _TENSE_CONDITIONAL,
+    _TENSE_NEGATIVE_CONDITIONAL,
+    _TENSE_UNTIL,
+    _TENSE_UNTIL_N,
+    _TENSE_NOT_YET,
+    _TENSE_CAUSATIVE,
+    _TENSE_CAUSATIVE,
+    _TENSE_CONJUNCTIVE,
+    _TENSE_NEGATIVE_CONJUNCTIVE,
+    _TENSE_JUSSIVE,
+    _TENSE_NEGATIVE_JUSSIVE,
+]
+
+ALL_VERB_QUALITATIVE = [
+    _VERB_GENERAL,
+    _VERB_TO_NOUN,
+    _TENSE_PRESENT,
+    _TENSE_NEGATIVE_PRESENT,
+    _TENSE_CIRCUMSTANTIAL_PRESENT,
+    _TENSE_RELATIVE_PRESENT,
+    _TENSE_FOCALIZED_PRESENT_,
+    _TENSE_PRETERITE_PRESENT,
+    _TENSE_CIRCUMSTANTIAL_PRETERITE_PRESENT,
 ]
