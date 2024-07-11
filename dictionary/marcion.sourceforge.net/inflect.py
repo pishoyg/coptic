89e90f13e17c8566234b93754eece98a7d642241
Pishoy Girgis <pishoybg@gmail.com>
2024-07-11 14:09:22 +0200

    Inflections: Change the signature of the module.

--- dictionary/marcion.sourceforge.net/inflect.py
+++ dictionary/marcion.sourceforge.net/inflect.py
@@ -1,7 +1,25 @@
-import word as lexical
+import enum
+
+import type_enforced
 
 
-def inflect(word: lexical.structured_word) -> list[str]:
+class Type(enum.Enum):
+    NOUN_MASCULINE = 11
+    NOUN_FEMININE = 12
+    NOUN_PLURAL = 13
+    NOUN_MASCULINE_OR_FEMININE = 14
+    NOUN_UNKNOWN_GENDER = 15
+    NOUN_NO_ARTICLE = 16
+
+    VERB_INFINITIVE = 21
+    VERB_PRENOMINAL = 22
+    VERB_PRONOMINAL = 23
+    VERB_QUALITATIVE = 24
+    VERB_IMPERATIVE = 25
+
+
+@type_enforced.Enforcer(enabled=True)
+def inflect(morpheme: str, type: Type) -> list[str]:
     """
     Given a word, return a list of inflected forms.
     """
