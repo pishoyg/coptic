19e706ace3ed02ecc9a585afbf7e9b14deb116f4
Pishoy Girgis <pishoybg@gmail.com>
2024-07-20 13:59:25 +0200

    [Crum] Add a script (archived) used to crop images along the y-axis.

---
+++ archive/crop_y.sh
@@ -0,0 +1,30 @@
+# Crop out the bottom part of an image, keeping only the given proportion of
+# the original height.
+set -e  # Exit upon encountering a failure.
+set -u  # Consider an undefined variable to be an error.
+
+# TODO: Use named args instead of positional args.
+FILE="${1}"
+NUMER="${2}"
+DENOM="${3}"
+
+# The default proportion is 17/20 of the original.
+if [ -z ${NUMER} ]; then
+  NUMER="17"
+fi
+if [ -z ${DENOM} ]; then
+  DENOM="20"
+fi
+
+DIMENSIONS="$(magick identify "${FILE}" | awk '{ print $3 }')"
+WIDTH="$(echo "${DIMENSIONS}" | grep -oE '^[[:digit:]]+')"
+HEIGHT="$(echo "${DIMENSIONS}" | grep -oE '[[:digit:]]+$')"
+HEIGHT="$(("${HEIGHT}" / "${DENOM}" * "${NUMER}"))"
+
+DIRNAME="$(dirname "${FILE}")"
+BASENAME="$(basename "${FILE}")"
+STEM="${BASENAME##*.}"
+EXT="${BASENAME%.*}"
+DEST="${DIRNAME}/${STEM}_${WIDTH}x${HEIGHT}.${EXT}"
+magick "${FILE}" -crop "${WIDTH}x${HEIGHT}+0+0" "${DEST}"
+open "${DEST}"
