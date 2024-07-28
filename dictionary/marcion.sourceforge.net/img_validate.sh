484b852b8a56ba6546aa364c88af12266a60aa89
Pishoy Girgis <pishoybg@gmail.com>
2024-07-28 09:15:39 +0200

    [Crum] Rewrite the image-validation script.
    Besides aesthetics, perform the following additional checks:
    - Check the timestamps of the converted images during validation.
    - Check that there are no superfluous source files.

--- dictionary/marcion.sourceforge.net/img_validate.sh
+++ dictionary/marcion.sourceforge.net/img_validate.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
 
-IMG="dictionary/marcion.sourceforge.net/data/img"
-IMG_300="dictionary/marcion.sourceforge.net/data/img-300"
+IMG_DIR="dictionary/marcion.sourceforge.net/data/img"
+IMG_300_DIR="dictionary/marcion.sourceforge.net/data/img-300"
+IMG_SOURCES_DIR="dictionary/marcion.sourceforge.net/data/img-sources"
 
-img_extensions () {
-  find "${IMG}" -type f | grep -oE '\.[^.]+$' | tr '[:upper:]' '[:lower:]' | sort
+VALID_EXTENSIONS=".avif .gif .jpeg .jpg .png .webp"
+
+find_basenames () {
+  find "${1}" -type f -exec basename {} \; | sort
 }
 
-valid_img_extensions () {
-  echo .avif .gif .jpeg .jpg .png .webp | tr ' ' '\n' | sort
+find_stems () {
+  find_basenames "${1}" | grep -oE '^[^.]+' | sort
 }
 
-img_stems () {
-  find "${IMG}" -type f -exec basename {} \; | grep -oE '^[^.]+' | sort
-}
-
-valid_img_stems () {
-  img_stems | grep -oE '^[[:digit:]]+-[[:digit:]]+-[[:digit:]]+$' | grep --invert '^$'
-}
-
-img_300_stems () {
-  find "${IMG_300}" -type f -exec basename {} \; | grep -oE '^[^.]+' | sort
+find_extensions () {
+  find_basenames "${1}" | grep -oE '\.[^.]+$' | tr '[:upper:]' '[:lower:]' | sort
 }
 
 # Checking for unknown image extensions:
-DIFF=$(comm -23 <(img_extensions | uniq) <(valid_img_extensions))
-
+# shellcheck disable=SC2086
+DIFF=$(comm -23 <(find_extensions "${IMG_DIR}" | uniq) <(echo ${VALID_EXTENSIONS} | tr ' ' '\n' | sort))
 if [ -n "${DIFF}" ]; then
   echo "Unknown extensions:"
   echo "${DIFF}"
@@ -34,44 +29,64 @@ if [ -n "${DIFF}" ]; then
   exit 1
 fi
 
-# Check that there are no identical IDs.
-DIFF=$(comm -3 <(img_stems) <(img_stems | uniq))
-if [ -n "${DIFF}" ]; then
-  echo "The following basenames are duplicate:"
-  echo "${DIFF}"
-  exit 1
-fi
-
-# Check that all images are converted.
-DIFF=$(comm -3 <(img_stems) <(img_300_stems))
-if [ -n "${DIFF}" ]; then
-  echo "Delta:"
-  echo "${DIFF}"
-  echo "You might want to run img_setup.sh, or delete some images from"
-  echo " the destination that are no longer in the source directory."
-  exit 1
-fi
-
 # Check that all images have valid names.
-DIFF=$(comm -3 <(img_stems) <(valid_img_stems))
+DIFF=$(comm -3 <(find_stems "${IMG_DIR}") <(find_stems "${IMG_DIR}" | grep -oE '^[[:digit:]]+-[[:digit:]]+-[[:digit:]]+$' | grep --invert '^$'))
 if [ -n "${DIFF}" ]; then
   echo "Invalid image names:"
   echo "${DIFF}"
   exit 1
 fi
 
-# Check that all images have up-to-date sources.
-find "${IMG}" -type f | while read -r FILE; do
-  SOURCE_FILE="${FILE%.*}.txt"
-  SOURCE_FILE="${SOURCE_FILE/img/img-sources}"
-  if [ ! -f "${SOURCE_FILE}" ]; then
-    echo "${FILE} doesn't have a source!"
-    echo "${SOURCE_FILE} doesn't exist!"
+# Check that there are no repeated image IDs.
+DIFF=$(comm -3 <(find_stems "${IMG_DIR}") <(find_stems "${IMG_DIR}" | uniq))
+if [ -n "${DIFF}" ]; then
+  echo "The following basenames are duplicate:"
+  echo "${DIFF}"
+  exit 1
+fi
+
+# Check that the two sets of IDs of original and converted images are equal.
+# This guarantees that all images have been converted, and that we don't have
+# any leftover converted images that need to be cleaned up.
+DIFF=$(comm -3 <(find_stems "${IMG_DIR}") <(find_stems "${IMG_300_DIR}"))
+if [ -n "${DIFF}" ]; then
+  echo "Delta:"
+  echo "${DIFF}"
+  echo "The set of image ID's doesn't match the set of converted image ID's."
+  echo "Running img_setup.sh might help. Note: Run with '--help' first!"
+  exit 1
+fi
+
+# Check that the two sets of IDs of images and source files are equal.
+# This guarantees that all images have sources specified, and that we don't
+# have leftover sources that need to be cleaned up
+DIFF=$(comm -3 <(find_stems "${IMG_DIR}") <(find_stems "${IMG_SOURCES_DIR}"))
+if [ -n "${DIFF}" ]; then
+  echo "Delta:"
+  echo "${DIFF}"
+  echo "The set of image ID's doesn't match the set of source ID's."
+  echo "Running img_setup.sh might help."
+  exit 1
+fi
+
+# Check that the converted images are more recent than the original ones.
+find_basenames "${IMG_DIR}"| while read -r BASENAME; do
+  IMAGE="${IMG_DIR}/${BASENAME}"
+  IMAGE_300="${IMG_300_DIR}/${BASENAME/.png/.jpg}"
+  if (($(date +%s -r "${IMAGE}") > $(date +%s -r "${IMAGE_300}"))); then
+    echo "${IMAGE} has a more recent timestamp than its converted version ${IMAGE_300}!"
     exit 1
   fi
-  if (($(date +%s -r "${FILE}") > $(date +%s -r "${SOURCE_FILE}"))); then
-    echo "${FILE} has a more recent timestamp than its source!"
-    echo "Update ${SOURCE_FILE}, or merely touch if the content is already up-to-date."
+done
+
+# Check that all sources are more recent than their corresponding files.
+find_basenames "${IMG_DIR}" | while read -r BASENAME; do
+  IMAGE="${IMG_DIR}/${BASENAME}"
+  STEM="${BASENAME%.*}"
+  IMAGE_SOURCE="${IMG_SOURCES_DIR}/${STEM}.txt"
+  if (($(date +%s -r "${IMAGE}") > $(date +%s -r "${IMAGE_SOURCE}"))); then
+    echo "${IMAGE} has a more recent timestamp than its source!"
+    echo "Update ${IMAGE_SOURCE}, or merely touch if the content is already up-to-date."
     exit 1
   fi
 done
