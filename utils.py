3ef228812f906e1e6c0ffe01d8ecbdebf67c2104
Pishoy Girgis <pishoybg@gmail.com>
2024-08-07 16:02:53 +0200

    [#150][Utils] Define `read_tsv`, type enforcement, and aesthetics.

--- utils.py
+++ utils.py
@@ -16,10 +16,16 @@ def warning(*args):
 
 
 @type_enforced.Enforcer(enabled=ENFORCED)
-def to_tsv(df: pd.DataFrame, path: str):
-    df.to_csv(path, sep="\t", index=False)
+def to_tsv(df: pd.DataFrame, path: str, **kwargs):
+    df.to_csv(path, sep="\t", index=False, **kwargs)
 
 
+@type_enforced.Enforcer(enabled=ENFORCED)
+def read_tsv(path: str) -> pd.DataFrame:
+    return pd.read_csv(path, sep="\t", dtype=str, encoding="utf-8").fillna("")
+
+
+@type_enforced.Enforcer(enabled=ENFORCED)
 def clean_dir(dir: str) -> None:
     if os.path.exists(dir):
         assert os.path.isdir(dir)
@@ -27,6 +33,7 @@ def clean_dir(dir: str) -> None:
     pathlib.Path(dir).mkdir(parents=True)
 
 
+@type_enforced.Enforcer(enabled=ENFORCED)
 def write_tsvs(
     df: pd.DataFrame, tsvs: str, chunk_size: int = 100, zfill: int = 0, **kwargs
 ) -> None:
@@ -35,19 +42,15 @@ def write_tsvs(
     zfill = zfill or len(str(len(starts) + 2))
     for idx, start in enumerate(starts):
         chunk = df.iloc[start : start + chunk_size]
-        chunk.to_csv(
-            os.path.join(tsvs, str(idx).zfill(zfill) + ".tsv"),
-            sep="\t",
-            index=False,
-            **kwargs
-        )
+        to_tsv(chunk, os.path.join(tsvs, str(idx).zfill(zfill) + ".tsv"), **kwargs)
 
 
+@type_enforced.Enforcer(enabled=ENFORCED)
 def read_tsvs(tsvs: str) -> pd.DataFrame:
     files = [os.path.join(tsvs, basename) for basename in os.listdir(tsvs)]
     files = sorted(files)
     df = pd.DataFrame()
     for f in files:
-        cur = pd.read_csv(f, sep="\t", dtype=str, encoding="utf-8").fillna("")
+        cur = read_tsv(f)
         df = pd.concat([df, cur], ignore_index=True)
     return df
