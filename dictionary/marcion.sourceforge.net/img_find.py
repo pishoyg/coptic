pishoyg/coptic

commit 8ac5ce13203cfffe55e9b02245bb86426c6929be

[#162][Crum/Images] Fix bug in logic to change key in `img_find.py`.

dictionary/marcion.sourceforge.net/img_find.py
@@ -254,6 +254,7 @@ def main():
         assert k not in exclude
         exclude[k] = v
 
+    key_to_row = {row[KEY_COL]: row for _, row in df.iterrows()}
     for _, row in df.iterrows():
         key = row[KEY_COL]
         key = int(key)
@@ -271,14 +272,13 @@ def main():
             continue
 
         open_images(g)
-        q = query(row[MEANING_COL])
         subprocess.run(
             [
                 "open",
                 "-a",
                 args.browser,
                 row[LINK_COL],
-                q,
+                query(row[MEANING_COL]),
             ]
         )
 
@@ -338,6 +338,7 @@ def main():
 
             if sense.startswith("key="):
                 key = sense[4:]
+                row = key_to_row[key]
                 continue
 
             if sense.startswith("source="):

commit 4e22b06cca07dfcbb2293df258ba57346ec720a7

[#162][Crum/Images] Fix bug in `img_find.py`.
The bug was introduced in 8ac5ce13203cfffe55e9b02245bb86426c6929be.

dictionary/marcion.sourceforge.net/img_find.py
@@ -350,7 +350,7 @@ def main():
                         files,
                     )
                     continue
-                sense = sense[1:]
+                sense = sense[7:]
                 if not sense:
                     utils.error("No source given!")
                     continue