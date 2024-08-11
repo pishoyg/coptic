pishoyg/coptic

commit e2bcfeb7488b0923615821fd7daf024a266256e2

[134][Flashcards] Use JavaScript for Greek Dictionary Lookups.

flashcards/constants.py
@@ -12,6 +12,7 @@ HOME = "https://github.com/pishoyg/coptic/"
 EMAIL = "pishoybg@gmail.com"
 
 COPTIC_WORD_RE = re.compile("([Ⲁ-ⲱϢ-ϯⳈⳉ]+)")
+GREEK_WORD_RE = re.compile("([Α-Ωα-ω]+)")
 
 
 @type_enforced.Enforcer(enabled=enforcer.ENABLED)
@@ -40,13 +41,23 @@ def crum(
             force=force,
         )
 
-    @type_enforced.Enforcer(enabled=enforcer.ENABLED)
     # TODO: This replaces all Coptic words, regardless of whether they
     # represent plain text. Coptic text that occurs inside a tag (for example
     # as a tag property) would still get wrapped inside this <span> tag.
+    @type_enforced.Enforcer(enabled=enforcer.ENABLED)
     def cdo(text: str) -> str:
         return COPTIC_WORD_RE.sub(
             r'<span class="coptic">\1</span>',
+            text,
+        )
+
+    # TODO: This replaces all Greek words, regardless of whether they
+    # represent plain text. Greek text that occurs inside a tag (for example
+    # as a tag property) would still acquire this tag.
+    @type_enforced.Enforcer(enabled=enforcer.ENABLED)
+    def greek(text: str) -> str:
+        return GREEK_WORD_RE.sub(
+            r'<span class="greek">\1</span>',
             text,
         )
 
@@ -119,6 +130,16 @@ def crum(
                     + btn.innerHTML, '_blank').focus();
             }
         });
+        var els = document.getElementsByClassName('greek');
+        Array.prototype.forEach.call(els, function(btn) {
+            btn.classList.add('link');
+            btn.classList.add('light');
+            btn.onclick = () => {
+                window.open(
+                    'https://logeion.uchicago.edu/'
+                    + btn.innerHTML, '_blank').focus();
+            }
+        });
         """,
         # N.B. The key is a protected field. Do not change unless you know what
         # you're doing.
@@ -143,7 +164,7 @@ def crum(
                 ),
                 # Meaning.
                 field.aon(
-                    roots_col("en-parsed-link-light-greek", line_br=True, force=False),
+                    field.apl(greek, roots_col("en-parsed", line_br=True, force=False)),
                     "<br/>",
                 ),
                 # Image.

dictionary/marcion.sourceforge.net/tree.py
@@ -111,7 +111,7 @@ class node:
         - depth
         - word-parsed-prettify || dialect-*
         - type-parsed
-        - en-parsed-light-greek
+        - en-parsed
         - crum
         - key
         They are expected to be pre-sorted, and to belong to a single word.
@@ -157,7 +157,7 @@ class node:
                 else d.cell("dialect-" + dialect)
             )
             type = d.cell("type-parsed")
-            meaning = d.cell("en-parsed-link-light-greek")
+            meaning = d.cell("en-parsed")
             key = d.cell("key")
             word_width = int((NUM_COLS - depth) / 2) if word else 0
             # We keep the meaning column regardless of whether a meaning is

commit 7a3e0d80161f4b6192c83988550c626d85ca265c

[#134][Flashcards] Greek Lookups Work in the Derivations Table.
Bug introduced in e2bcfeb7488b0923615821fd7daf024a266256e2.

flashcards/constants.py
@@ -269,7 +269,9 @@ def crum(
                 # Full entry.
                 roots_col("word-parsed-no-ref", line_br=True, force=True),
                 # Derivations.
-                roots_col("derivations-table", line_br=True, force=False),
+                field.apl(
+                    greek, roots_col("derivations-table", line_br=True, force=False)
+                ),
                 # Crum's pages.
                 field.cat(
                     "<hr/>",