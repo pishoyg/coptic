b2a00f4ac1485601691585b248a0a6989e516e16
Pishoy Girgis <pishoybg@gmail.com>
2024-08-12 08:32:42 +0200

    [#111][env] Export Colors.

--- env.sh
+++ env.sh
@@ -57,3 +57,14 @@ export THENOUNPROJECT_SECRET=""
 bohairic () {
   open "flashcards/data/output/html/a_coptic_dictionary__bohairic/${1}.html"
 }
+
+# Colors are used to prettify outputs.
+export RESET='\033[0m'
+export BLACK='\033[0;30m'
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export WHITE='\033[0;37m'
