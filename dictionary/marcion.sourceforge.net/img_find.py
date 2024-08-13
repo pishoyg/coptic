e637626ede08b4857e3d80c7597b9a80fe5474d1
Pishoy Girgis <pishoybg@gmail.com>
2024-08-13 17:54:45 +0200

    [#183,#5] Image Validate is Implemented in `img_find`.
    The two have a lot in common. It is desirable at this point to
    centralize image processing in a single script.

--- dictionary/marcion.sourceforge.net/img_find.py
+++ dictionary/marcion.sourceforge.net/img_find.py
@@ -30,6 +30,10 @@ LINK_COL: str = "key-link"
 SOURCES_DIR: str = "dictionary/marcion.sourceforge.net/data/img-sources/"
 
 
+VALID_EXTENSIONS = {".avif", ".gif", ".jpeg", ".jpg", ".JPG", ".png", ".webp", ".svg"}
+VALID_EXTENSIONS_300 = {".avif", ".gif", ".jpeg", ".jpg", ".JPG", ".webp"}
+
+
 @type_enforced.Enforcer(enabled=enforcer.ENABLED)
 def params_str(params: dict) -> str:
     return "?" + "&".join(f"{k}={v}" for k, v in params.items())
@@ -132,6 +136,14 @@ argparser.add_argument(
     help="Secret to the API of thenounproject.",
 )
 
+argparser.add_argument(
+    "--validate",
+    default=False,
+    action="store_true",
+    help="If true, just validate the directories and exit.",
+)
+
+
 global args
 
 
@@ -218,6 +230,10 @@ def target_ext(ext: str) -> str:
 def main():
     global args
     args = argparser.parse_args()
+    if args.validate:
+        validate()
+        exit()
+
     df = utils.read_tsvs(INPUT_TSVS)
     df.sort_values(by=KEY_COL, inplace=True)
 
@@ -501,6 +517,66 @@ def main():
                         ),
                     ]
                 )
+
+
+@type_enforced.Enforcer(enabled=enforcer.ENABLED)
+def listdir_sorted(dir: str) -> list[str]:
+    return utils.sort_semver(utils.paths(dir))
+
+
+@type_enforced.Enforcer(enabled=enforcer.ENABLED)
+def validate():
+    images = listdir_sorted(IMG_DIR)
+    converted_images = listdir_sorted(IMG_300_DIR)
+    sources = listdir_sorted(SOURCES_DIR)
+
+    utils.verify_unique(utils.stems(images), "images:")
+    utils.verify_unique(utils.stems(converted_images), "converted images:")
+    utils.verify_unique(utils.stems(sources), "sources:")
+
+    # Checking that extensions are valid.
+    utils.verify_all_belong_to_set(
+        utils.exts(images), VALID_EXTENSIONS, "Images: Unknown extension:"
+    )
+    utils.verify_all_belong_to_set(
+        utils.exts(converted_images),
+        VALID_EXTENSIONS_300,
+        "Converted Images: Unknown extension:",
+    )
+    utils.verify_all_belong_to_set(
+        utils.exts(sources), {".txt"}, "Sources: Unknown extension:"
+    )
+
+    # Verify that all three directories have the same set of IDs.
+    utils.verify_equal_sets(
+        utils.stems(images),
+        utils.stems(converted_images),
+        "Images and converted images:",
+    )
+    utils.verify_equal_sets(
+        utils.stems(images), utils.stems(sources), "Images and sources:"
+    )
+
+    # Check that all images have valid IDs.
+    for stem in utils.stems(images):
+        match = STEM_RE.fullmatch(stem)
+        if match:
+            continue
+        utils.fatal("Invalid stem:", stem)
+
+    # Check that the sources and converted images are more recent than the
+    # original ones.
+    offending: list[str] = []
+    for image, converted, source in zip(images, converted_images, sources):
+        # Sanity check!
+        assert utils.stem(image) == utils.stem(converted) == utils.stem(source)
+        mtime = os.stat(image).st_mtime
+        if mtime > os.stat(converted).st_mtime:
+            offending.append(converted)
+        if mtime > os.stat(source).st_mtime:
+            offending.append(source)
+    if offending:
+        utils.fatal("Artifacts may be obsolete:", offending)
 
 
 if __name__ == "__main__":
