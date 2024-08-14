75269f37a9917b8f1118c7d023e7eaa0572d2b3f
Pishoy Girgis <pishoybg@gmail.com>
2024-08-14 14:14:30 +0200

    [#182][Site] Modify the Pipeline to Write the Google Analytics Tag.

--- site/publish.sh
+++ site/publish.sh
@@ -3,6 +3,17 @@
 set -o errexit  # Exit upon encountering a failure.
 set -o nounset  # Consider an undefined variable to be an error.
 
+GOOGLE_TAG='
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-VCVZFDFZR3"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag("js", new Date());
+    gtag("config", "G-VCVZFDFZR3");
+  </script>
+'
+
 AUTO=false
 while [ $# -gt 0 ]; do
   case $1 in
@@ -38,6 +49,18 @@ cp -r \
   bible/stshenouda.org/data/output/html/bohairic \
   bible/stshenouda.org/data/output/html/sahidic \
   "${BIBLE_DIR}"
+
+# TODO: Speed up the pipeline a little bit by merging the copying and
+# tag insertions steps. Write the files only once.
+find "${SITE_DIR}" -type f -name "*.html" | while read -r FILE; do
+  LINE_NUM="$(grep "^<head>$" "${FILE}" --line-number --max-count=1 | cut -f1 -d:)"
+  if [ -z "${LINE_NUM}" ]; then
+    echo -e "${PURPLE}Can't find <head> in ${RED}${FILE}"
+    exit 1
+  fi
+  NEW="$(head -n "${LINE_NUM}" "${FILE}")${GOOGLE_TAG}$(tail -n "+$((LINE_NUM + 1))" "${FILE}")"
+  echo "${NEW}" > "${FILE}"
+done
 
 git -C "${SITE_DIR}" add --all
 git -C "${SITE_DIR}" commit --fixup HEAD
