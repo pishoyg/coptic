4af55ceaf5ada79c2816d710654faeaefe6a9f5d
Pishoy Girgis <pishoybg@gmail.com>
2024-08-20 19:29:01 +0200

    [#201][TypeScript] Add Stats.

--- stats.sh
+++ stats.sh
@@ -5,7 +5,7 @@
 set -o errexit  # Exit upon encountering a failure.
 set -o nounset  # Consider an undefined variable to be an error.
 
-KNOWN_EXTENSIONS="Makefile css csslintrc env_INFO gitignore js keylayout md plist py sh strings txt yaml"
+KNOWN_EXTENSIONS="Makefile css csslintrc env_INFO gitignore json js mjs keylayout md plist py sh strings txt yaml ts"
 
 SAVE=false
 while [ $# -gt 0 ]; do
@@ -25,6 +25,11 @@ while [ $# -gt 0 ]; do
   shift
 done
 
+# NOTE: It's important to notice that the arguments get appended to a long list
+# of exclusion arguments below. Thus, be careful when you use an OR clause, as
+# it could render some of the exclusion clauses idempotent. Most of the time,
+# you will need to wrap your OR clauses inside parentheses, such that the
+# exclusion clauses below still take effect. See examples throughout the file.
 foc () {
   # Files of code.
   DIR="${1}"
@@ -46,7 +51,10 @@ foc () {
     -not -name ".DS_Store" \
     -not -path "*/__pycache__/*" \
     -not -path "./coptic.egg-info/*" \
+    -not -path "./node_modules/*" \
     -not -name ".env" \
+    -not -name "package-lock.json" \
+    -not -name "package.json" \
     -not -path "*/data/*" \
     -not -path "./archive/*" "${@:3}" -exec "${EXEC}" {} \;
 }
@@ -156,12 +164,14 @@ LOC_PYTHON=$(loc . -name "*.py")
 LOC_MAKE=$(loc . -name "Makefile")
 LOC_CSS=$(loc . -name "*.css")
 LOC_SH=$(loc . -a \( -name "*.sh" -o -name ".env" -o -name ".env_INFO" \))
-LOC_JS=$(loc . -name "*.js")
+LOC_JS=$(loc . -a \( -name "*.js" -o -name "*.mjs" \))
 LOC_MD=$(loc . -name "*.md")
 LOC_YAML=$(loc . -name "*.yaml")
 LOC_DOT=$(loc . -a \( -name ".csslintrc" -o -name ".gitignore" \) )
 LOC_KEYBOARD_LAYOUT=$(loc . -a \( -name "*.keylayout" -o -name "*.plist" -o -name "*.strings" \) )
 LOC_TXT=$(loc . -name "*.txt")
+LOC_TS=$(loc . -name "*.ts")
+LOC_JSON=$(loc . -name "*.json")
 
 TOTAL_BY_LANG="$((
   LOC_PYTHON
@@ -173,7 +183,9 @@ TOTAL_BY_LANG="$((
   + LOC_YAML
   + LOC_DOT
   + LOC_KEYBOARD_LAYOUT
-  + LOC_TXT))"
+  + LOC_TXT
+  + LOC_TS
+  + LOC_JSON))"
 
 EXTENSIONS="$(extensions .)"
 DIFF=$(comm -23 <(echo "${EXTENSIONS}") <(echo "${KNOWN_EXTENSIONS}" | tr ' ' '\n' | sort) | tr '\n' ' ')
@@ -258,6 +270,8 @@ echo -e "${BLUE}Number of live lines of code per language: "\
 "\n  ${BLUE}dot: ${GREEN}${LOC_DOT}"\
 "\n  ${BLUE}keyboard: ${GREEN}${LOC_KEYBOARD_LAYOUT}"\
 "\n  ${BLUE}txt: ${GREEN}${LOC_TXT}"\
+"\n  ${BLUE}TypeScript: ${GREEN}${LOC_TS}"\
+"\n  ${BLUE}JSON: ${GREEN}${LOC_JSON}"\
 "\n  ${BLUE}TOTAL: ${GREEN}${TOTAL_BY_LANG}"
 
 echo -e "${BLUE}Number of words possessing at least one image: "\
@@ -320,7 +334,7 @@ if ${SAVE}; then
   # to tab characters.
   # We do this by first converting all spaces to tabs, then converting five
   # tabs to spaces.
-  echo -e "$(date) $(date +%s) ${LOC} ${CRUM_IMG} ${CRUM_DAWOUD} ${LOC_CRUM} ${LOC_COPTICSITE} ${LOC_KELLIA} ${LOC_BIBLE} ${LOC_FLASHCARDS} ${LOC_GRAMMAR} ${LOC_KEYBOARD} ${LOC_MORPHOLOGY} ${LOC_SITE} ${LOC_SHARED} ${LOC_ARCHIVE} ${CRUM_TYPOS} ${CRUM_IMG_SUM} ${CRUM_DAWOUD_SUM} ${NUM_COMMITS} ${NUM_CONTRIBUTORS} ${CRUM_NOTES} ${LOC_PYTHON} ${LOC_MAKE} ${LOC_CSS} ${LOC_SH} ${LOC_JS} ${LOC_MD} ${LOC_YAML} ${LOC_DOT} ${LOC_KEYBOARD_LAYOUT} ${LOC_TXT} ${CRUM_WRD_TYPOS} ${CRUM_DRV_TYPOS} ${CRUM_PAGES_CHANGED} ${CRUM_ROOT_SENSES} ${CRUM_ROOT_SENSES_SUM}" \
+  echo -e "$(date) $(date +%s) ${LOC} ${CRUM_IMG} ${CRUM_DAWOUD} ${LOC_CRUM} ${LOC_COPTICSITE} ${LOC_KELLIA} ${LOC_BIBLE} ${LOC_FLASHCARDS} ${LOC_GRAMMAR} ${LOC_KEYBOARD} ${LOC_MORPHOLOGY} ${LOC_SITE} ${LOC_SHARED} ${LOC_ARCHIVE} ${CRUM_TYPOS} ${CRUM_IMG_SUM} ${CRUM_DAWOUD_SUM} ${NUM_COMMITS} ${NUM_CONTRIBUTORS} ${CRUM_NOTES} ${LOC_PYTHON} ${LOC_MAKE} ${LOC_CSS} ${LOC_SH} ${LOC_JS} ${LOC_MD} ${LOC_YAML} ${LOC_DOT} ${LOC_KEYBOARD_LAYOUT} ${LOC_TXT} ${CRUM_WRD_TYPOS} ${CRUM_DRV_TYPOS} ${CRUM_PAGES_CHANGED} ${CRUM_ROOT_SENSES} ${CRUM_ROOT_SENSES_SUM} ${LOC_TS} ${LOC_JSON}" \
     | sed 's/ /\t/g' \
     | sed 's/\t/ /' \
     | sed 's/\t/ /' \
