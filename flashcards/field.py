pishoyg/coptic

commit c4b05b77621562f2981d4ac21a37e50c28d9dbc2

[#188][Flashcards/Crum] Set a Fixed Width for the Crum and Dawoud Scans.

flashcards/field.py
@@ -306,6 +306,7 @@ class media(_content_field):
                 new_path = _add_to_work_dir(path)
                 args = {
                     "basename": os.path.basename(new_path),
+                    "width": "auto",
                     "alt": utils.stem(path),
                 }
                 if fmt_args:
@@ -339,7 +340,9 @@ def img(
     """
     html_fmt = '<img src="{basename}" alt="{alt}"><br/>'
     if id:
-        html_fmt = '<img src="{basename}" alt="{alt}" id="{id}"><br/>'
+        html_fmt = (
+            '<img src="{basename}" alt="{alt}" id="{id}" style="width: {width}"><br/>'
+        )
     if caption:
         html_fmt = (
             "<figure>"

flashcards/constants.py
@@ -19,6 +19,8 @@ DICTIONARY_PAGE_RE = re.compile("([0-9]+(a|b))")
 COPTIC_WORD_RE = re.compile("([Ⲁ-ⲱϢ-ϯⳈⳉ]+)")
 GREEK_WORD_RE = re.compile("([Α-Ωα-ω]+)")
 
+DICT_WIDTH = "1000px"
+
 
 @type_enforced.Enforcer(enabled=enforcer.ENABLED)
 def crum(
@@ -268,6 +270,7 @@ def crum(
                             ),
                             "id": f"crum{int(utils.stem(path)) - 20}",
                             "alt": int(utils.stem(path)) - 20,
+                            "width": DICT_WIDTH,
                         },
                         caption=True,
                         id=True,
@@ -298,6 +301,7 @@ def crum(
                             "caption": int(utils.stem(path)) - 16,
                             "id": f"dawoud{int(utils.stem(path)) - 16}",
                             "alt": int(utils.stem(path)) - 16,
+                            "width": DICT_WIDTH,
                         },
                         caption=True,
                         id=True,

commit aa17071d92d2dd41082e248dcea9e490fb08755d

[#188][Crum/HTML] Fix Bug. Width CSS Attribute is Always Present.
The previous code only set the attribute if the `id` parameter was set.
This didn't cause problems because `id` was always there. We add the
width attribute so it will be present even if `id` is set to false.

The bug was introduced in c4b05b77621562f2981d4ac21a37e50c28d9dbc2.

Running `make flashcards test` doesn't introduce any changes, as
expected, because this code path is currently unused as explained.

flashcards/field.py
@@ -338,7 +338,7 @@ def img(
         The `alt` key is optional, and can be used to override the alternative
         text (which defaults to the stem).
     """
-    html_fmt = '<img src="{basename}" alt="{alt}"><br/>'
+    html_fmt = '<img src="{basename}" alt="{alt}" style="width: {width}"><br/>'
     if id:
         html_fmt = (
             '<img src="{basename}" alt="{alt}" id="{id}" style="width: {width}"><br/>'