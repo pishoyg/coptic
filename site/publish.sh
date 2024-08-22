pishoyg/coptic

commit f1233f8fdb173af38019260ced5fcc743b0a0e05

[#141][Site] Restructure and Simplify Site Publishing.

site/publish.sh
@@ -3,7 +3,7 @@
 set -o errexit  # Exit upon encountering a failure.
 set -o nounset  # Consider an undefined variable to be an error.
 
-GOOGLE_TAG='
+readonly GOOGLE_TAG='
   <!-- Google tag (gtag.js) -->
 
   <script async src=
@@ -15,14 +15,23 @@ GOOGLE_TAG='
     gtag("config", "G-VCVZFDFZR3");
   </script>
 '
-ICON_TAG='  <link rel="icon" type="image/x-icon" href="/icon-circle.png">
+readonly ICON_TAG='  <link rel="icon" type="image/x-icon" href="/icon-circle.png">
 '
 
-AUTO=false
+PRE=false
+POST=false
 while [ $# -gt 0 ]; do
   case $1 in
-  --auto)
-    AUTO=true
+  --pre)
+    PRE=true
+    ;;
+  --post)
+    POST=true
+    ;;
+  --help)
+    echo -e "${GREEN}--pre${BLUE} regenerates the site in the site directory.${RESET}"
+    echo -e "${GREEN}--post${YELLOW} (1) ${BLUE}obfuscates the files, then ${YELLOW}(2) ${BLUE}creates, rebases, and force-pushes a fixup commit.${RESET}"
+    exit
     ;;
   *)
     echo "Unknown flag: ${1}"
@@ -32,55 +41,64 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-if [ -n "$(git -C "${SITE_DIR}" status --short)" ]; then
-  echo -e "${PURPLE}${SITE_DIR}${RED} is dirty. This should be done in a standalone commit.${RESET}"
-  exit 1
-fi
-
-find "${SITE_DIR}" -not -path "${SITE_DIR}/.git/*" -not -name ".git" -delete
-
-CRUM_DIR="${SITE_DIR}/crum"
-BIBLE_DIR="${SITE_DIR}/bible"
-
-cp \
-  "site/data/CNAME" \
-  "site/data/icon-circle.png" \
-  "${SITE_DIR}/"
-
-mkdir "${CRUM_DIR}"
-cp -r \
-  flashcards/data/output/html/a_coptic_dictionary__all_dialects/* \
-  "${CRUM_DIR}"
-
-mkdir "${BIBLE_DIR}"
-cp -r \
-  bible/stshenouda.org/data/output/html/bohairic_english \
-  bible/stshenouda.org/data/output/html/bohairic \
-  bible/stshenouda.org/data/output/html/sahidic \
-  "${BIBLE_DIR}"
-
-find "${SITE_DIR}" -type f -name "*.html" | while read -r FILE; do
-  LINE_NUM="$(grep "^<head>$" "${FILE}" --line-number --max-count=1 | cut -f1 -d:)"
-  if [ -z "${LINE_NUM}" ]; then
-    echo -e "${PURPLE}Can't find <head> in ${RED}${FILE}"
+pre () {
+  if [ -n "$(git -C "${SITE_DIR}" status --short)" ]; then
+    echo -e "${PURPLE}${SITE_DIR}${RED} is dirty. This should be done in a standalone commit.${RESET}"
     exit 1
   fi
-  NEW="$(head -n "${LINE_NUM}" "${FILE}")${GOOGLE_TAG}${ICON_TAG}$(tail -n "+$((LINE_NUM + 1))" "${FILE}")"
-  echo "${NEW}" > "${FILE}"
-  tidy -indent -modify -quiet --tidy-mark no -wrap 80 "${FILE}"
-done
 
-find "${SITE_DIR}" -type f -name "*.js" | while read -r FILE; do
-  npx javascript-obfuscator \
-    "${FILE}" \
-    --output "${FILE}"
-done
+  find "${SITE_DIR}" -not -path "${SITE_DIR}/.git/*" -not -name ".git" -delete
 
-git -C "${SITE_DIR}" add --all
-git -C "${SITE_DIR}" commit --fixup HEAD
+  readonly CRUM_DIR="${SITE_DIR}/crum"
+  readonly BIBLE_DIR="${SITE_DIR}/bible"
 
-if ${AUTO}; then
-  make site_push
-else
-  echo -e "${BLUE}Run ${GREEN}\`make site_push\`${BLUE}.${RESET}"
+  cp \
+    "site/data/CNAME" \
+    "site/data/icon-circle.png" \
+    "${SITE_DIR}/"
+
+  mkdir "${CRUM_DIR}"
+  cp -r \
+    flashcards/data/output/html/a_coptic_dictionary__all_dialects/* \
+    "${CRUM_DIR}"
+
+  mkdir "${BIBLE_DIR}"
+  cp -r \
+    bible/stshenouda.org/data/output/html/bohairic_english \
+    bible/stshenouda.org/data/output/html/bohairic \
+    bible/stshenouda.org/data/output/html/sahidic \
+    "${BIBLE_DIR}"
+
+  find "${SITE_DIR}" -type f -name "*.html" | while read -r FILE; do
+    LINE_NUM="$(grep "^<head>$" "${FILE}" --line-number --max-count=1 | cut -f1 -d:)"
+    if [ -z "${LINE_NUM}" ]; then
+      echo -e "${PURPLE}Can't find <head> in ${RED}${FILE}"
+      exit 1
+    fi
+    NEW="$(head -n "${LINE_NUM}" "${FILE}")${GOOGLE_TAG}${ICON_TAG}$(tail -n "+$((LINE_NUM + 1))" "${FILE}")"
+    echo "${NEW}" > "${FILE}"
+    tidy -indent -modify -quiet --tidy-mark no -wrap 80 "${FILE}"
+  done
+}
+
+post() {
+  find "${SITE_DIR}" -type f -name "*.js" | while read -r FILE; do
+    npx javascript-obfuscator "${FILE}" --output "${FILE}"
+  done
+
+  git -C "${SITE_DIR}" add --all
+  git -C "${SITE_DIR}" commit --fixup HEAD
+
+  git -C "${SITE_DIR}" rebase --root --autosquash
+  git -C "${SITE_DIR}" push --force
+
+  git -C "${SITE_DIR}" add --all
+  git -C "${SITE_DIR}" commit --fixup HEAD
+}
+
+if ${PRE}; then
+  pre
+fi
+if ${POST}; then
+  post
 fi

Makefile
@@ -58,12 +58,6 @@ flashcards_kellia: _flashcards_kellia
 .PHONY: bible_no_epub
 bible_no_epub: bible_no_epub_aux
 
-.PHONY: site_commit
-site_commit: _site_commit
-
-.PHONY: site_push
-site_push: _site_push
-
 .PHONY: stats
 stats: stats_save
 
@@ -234,18 +228,8 @@ endif
 
 # SITE RULES
 
-# N.B. `make site_publish` (currently accomplished by
-# `bash site/publish.sh --auto`) should be equivalent to
-# `make site_commit site_push`.
-site_publish: FORCE
-	bash site/publish.sh --auto
-
-_site_commit: FORCE
-	bash site/publish.sh
-
-_site_push: FORCE
-	git -C "$${SITE_DIR}" rebase --root --autosquash
-	git -C "$${SITE_DIR}" push --force
+site_publish:
+	bash site/publish.sh --pre --post
 
 # INFRASTRUCTURE RULES
 bin_install:

commit 98d1c4b54c5ad139a3d345e7f6b4d5a08b5fb030

[#141][Site] Fix Bug. Delete a Stray `git commit` Command.
Bug was introduced in f1233f8fdb173af38019260ced5fcc743b0a0e05.

site/publish.sh
@@ -128,9 +128,6 @@ post() {
 
   git -C "${SITE_DIR}" rebase --root --autosquash
   git -C "${SITE_DIR}" push --force
-
-  git -C "${SITE_DIR}" add --all
-  git -C "${SITE_DIR}" commit --fixup HEAD
 }
 
 if ${CLEAN}; then