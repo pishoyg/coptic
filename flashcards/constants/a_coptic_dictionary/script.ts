pishoyg/coptic

commit f2ac6d11385dd809bef5b3bd040dd9616c57613a

[#141][Crum/TypeScript] Retrieve Dialect Class from Class List.
Stop using the Inner HTML, as classes in the future will be obfuscated.

Run `make ts_transpile flashcards test` to pick up the changes.

flashcards/constants/a_coptic_dictionary/script.ts
@@ -1,3 +1,4 @@
+// TODO: Reduce the dependency on `innerHTML`. Use attributes when possible.
 function get_url_or_local(
   param: string,
   default_value: string | null = null): string | null {
@@ -135,6 +136,9 @@ Array.prototype.forEach.call(
 
 // Handle the 'dialect' class.
 type Dialect = 'S' | 'Sa' | 'Sf' | 'A' | 'sA' | 'B' | 'F' | 'Fb' | 'O' | 'NH';
+const dialects: readonly Dialect[] = [
+  'S', 'Sa', 'Sf', 'A', 'sA', 'B', 'F', 'Fb', 'O', 'NH'];
+
 function activeDialects(): Set<Dialect> | null {
   const d = get_url_or_local('d');
   if (d == null) {
@@ -146,8 +150,6 @@ function activeDialects(): Set<Dialect> | null {
 /* Update the display based on the value of the `d` parameter.
  */
 function dialect(): void {
-  const dialects: readonly Dialect[] = [
-    'S', 'Sa', 'Sf', 'A', 'sA', 'B', 'F', 'Fb', 'O', 'NH'];
   const active: Set<Dialect> | null = activeDialects();
   function dialected(el: Element): boolean {
     return dialects.some((d: Dialect) => el.classList.contains(d));
@@ -185,7 +187,16 @@ Array.prototype.forEach.call(
   (btn) => {
     btn.classList.add('hover-link');
     btn.onclick = () => {
-      const d: Dialect = btn.innerHTML;
+      const dClasses: readonly Dialect[] = dialects.filter(
+        (d) => btn.classList.contains(d));
+      if (dClasses.length != 1) {
+        console.log('Unable to determine dialect, classList: ', btn.classList);
+        return;
+      }
+      const d: Dialect | undefined = dClasses[0];
+      if (!d) {
+        return;
+      }
       let active = activeDialects();
       if (active == null) {
         active = new Set<Dialect>();

flashcards/data/build/a_coptic_dictionary/script.js
@@ -93,6 +93,9 @@ Array.prototype.forEach.call(document.getElementsByClassName('dawoud-page'), (bt
 Array.prototype.forEach.call(document.getElementsByClassName('drv-key'), (btn) => {
   btn.classList.add('small', 'light', 'italic');
 });
+const dialects = [
+  'S', 'Sa', 'Sf', 'A', 'sA', 'B', 'F', 'Fb', 'O', 'NH'
+];
 function activeDialects() {
   const d = get_url_or_local('d');
   if (d == null) {
@@ -101,9 +104,6 @@ function activeDialects() {
   return new Set(d.split(',').map((d) => d));
 }
 function dialect() {
-  const dialects = [
-    'S', 'Sa', 'Sf', 'A', 'sA', 'B', 'F', 'Fb', 'O', 'NH'
-  ];
   const active = activeDialects();
   function dialected(el) {
     return dialects.some((d) => el.classList.contains(d));
@@ -138,7 +138,15 @@ function dialect() {
 Array.prototype.forEach.call(document.getElementsByClassName('dialect'), (btn) => {
   btn.classList.add('hover-link');
   btn.onclick = () => {
-    const d = btn.innerHTML;
+    const dClasses = dialects.filter((d) => btn.classList.contains(d));
+    if (dClasses.length != 1) {
+      console.log('Unable to determine dialect, classList: ', btn.classList);
+      return;
+    }
+    const d = dClasses[0];
+    if (!d) {
+      return;
+    }
     let active = activeDialects();
     if (active == null) {
       active = new Set();

flashcards/data/output/html/a_coptic_dictionary__all_dialects/script.js
@@ -94,6 +94,9 @@ window.addEventListener('load', function() {'use strict';
   Array.prototype.forEach.call(document.getElementsByClassName('drv-key'), (btn) => {
     btn.classList.add('small', 'light', 'italic');
   });
+  const dialects = [
+    'S', 'Sa', 'Sf', 'A', 'sA', 'B', 'F', 'Fb', 'O', 'NH'
+  ];
   function activeDialects() {
     const d = get_url_or_local('d');
     if (d == null) {
@@ -102,9 +105,6 @@ window.addEventListener('load', function() {'use strict';
     return new Set(d.split(',').map((d) => d));
   }
   function dialect() {
-    const dialects = [
-      'S', 'Sa', 'Sf', 'A', 'sA', 'B', 'F', 'Fb', 'O', 'NH'
-    ];
     const active = activeDialects();
     function dialected(el) {
       return dialects.some((d) => el.classList.contains(d));
@@ -139,7 +139,15 @@ window.addEventListener('load', function() {'use strict';
   Array.prototype.forEach.call(document.getElementsByClassName('dialect'), (btn) => {
     btn.classList.add('hover-link');
     btn.onclick = () => {
-      const d = btn.innerHTML;
+      const dClasses = dialects.filter((d) => btn.classList.contains(d));
+      if (dClasses.length != 1) {
+        console.log('Unable to determine dialect, classList: ', btn.classList);
+        return;
+      }
+      const d = dClasses[0];
+      if (!d) {
+        return;
+      }
       let active = activeDialects();
       if (active == null) {
         active = new Set();

commit cddbeab92e71077ef36ddcddd2ec70263e250d01

[#141][Crum/TypeScript] Avoid use of Global Variables.
They don't work on Anki!
f2ac6d11385dd809bef5b3bd040dd9616c57613a broke JavaScript on Anki.
We duplicate one variable to avoid the problem.

P.S. Also use all caps for the variable since it's a `readonly`.

flashcards/constants/a_coptic_dictionary/script.ts
@@ -1,3 +1,4 @@
+// NOTE: Do NOT use global variables. They don't work on Anki!
 // TODO: Reduce the dependency on `innerHTML`. Use attributes when possible.
 function get_url_or_local(
   param: string,
@@ -146,8 +147,6 @@ Array.prototype.forEach.call(
 
 // Handle the 'dialect' class.
 type Dialect = 'S' | 'Sa' | 'Sf' | 'A' | 'sA' | 'B' | 'F' | 'Fb' | 'O' | 'NH';
-const dialects: readonly Dialect[] = [
-  'S', 'Sa', 'Sf', 'A', 'sA', 'B', 'F', 'Fb', 'O', 'NH'];
 
 function activeDialects(): Set<Dialect> | null {
   const d = get_url_or_local('d');
@@ -160,9 +159,11 @@ function activeDialects(): Set<Dialect> | null {
 /* Update the display based on the value of the `d` parameter.
  */
 function dialect(): void {
+  const DIALECTS: readonly Dialect[] = [
+    'S', 'Sa', 'Sf', 'A', 'sA', 'B', 'F', 'Fb', 'O', 'NH'];
   const active: Set<Dialect> | null = activeDialects();
   function dialected(el: Element): boolean {
-    return dialects.some((d: Dialect) => el.classList.contains(d));
+    return DIALECTS.some((d: Dialect) => el.classList.contains(d));
   }
   document.querySelectorAll(
     '.dialect-parenthesis,.dialect-comma,.spelling-comma,.type').forEach(
@@ -195,9 +196,11 @@ function dialect(): void {
 Array.prototype.forEach.call(
   document.getElementsByClassName('dialect'),
   (el: HTMLElement) => {
+    const DIALECTS: readonly Dialect[] = [
+      'S', 'Sa', 'Sf', 'A', 'sA', 'B', 'F', 'Fb', 'O', 'NH'];
     el.classList.add('hover-link');
     el.onclick = () => {
-      const dClasses: readonly Dialect[] = dialects.filter(
+      const dClasses: readonly Dialect[] = DIALECTS.filter(
         (d) => el.classList.contains(d));
       if (dClasses.length != 1) {
         // This is unexpected!

flashcards/data/build/a_coptic_dictionary/script.js
@@ -96,9 +96,6 @@ Array.prototype.forEach.call(document.getElementsByClassName('drv-key'), (el) =>
   el.classList.add('small', 'light', 'italic', 'hover-link');
   moveElement(el, 'a', { 'href': `#drv${el.innerHTML}` });
 });
-const dialects = [
-  'S', 'Sa', 'Sf', 'A', 'sA', 'B', 'F', 'Fb', 'O', 'NH'
-];
 function activeDialects() {
   const d = get_url_or_local('d');
   if (d == null) {
@@ -107,9 +104,12 @@ function activeDialects() {
   return new Set(d.split(',').map((d) => d));
 }
 function dialect() {
+  const DIALECTS = [
+    'S', 'Sa', 'Sf', 'A', 'sA', 'B', 'F', 'Fb', 'O', 'NH'
+  ];
   const active = activeDialects();
   function dialected(el) {
-    return dialects.some((d) => el.classList.contains(d));
+    return DIALECTS.some((d) => el.classList.contains(d));
   }
   document.querySelectorAll('.dialect-parenthesis,.dialect-comma,.spelling-comma,.type').forEach((el) => {
     if (active == null) {
@@ -139,9 +139,12 @@ function dialect() {
   });
 }
 Array.prototype.forEach.call(document.getElementsByClassName('dialect'), (el) => {
+  const DIALECTS = [
+    'S', 'Sa', 'Sf', 'A', 'sA', 'B', 'F', 'Fb', 'O', 'NH'
+  ];
   el.classList.add('hover-link');
   el.onclick = () => {
-    const dClasses = dialects.filter((d) => el.classList.contains(d));
+    const dClasses = DIALECTS.filter((d) => el.classList.contains(d));
     if (dClasses.length != 1) {
       return;
     }

flashcards/data/output/html/a_coptic_dictionary__all_dialects/script.js
@@ -97,9 +97,6 @@ window.addEventListener('load', function() {'use strict';
     el.classList.add('small', 'light', 'italic', 'hover-link');
     moveElement(el, 'a', { 'href': `#drv${el.innerHTML}` });
   });
-  const dialects = [
-    'S', 'Sa', 'Sf', 'A', 'sA', 'B', 'F', 'Fb', 'O', 'NH'
-  ];
   function activeDialects() {
     const d = get_url_or_local('d');
     if (d == null) {
@@ -108,9 +105,12 @@ window.addEventListener('load', function() {'use strict';
     return new Set(d.split(',').map((d) => d));
   }
   function dialect() {
+    const DIALECTS = [
+      'S', 'Sa', 'Sf', 'A', 'sA', 'B', 'F', 'Fb', 'O', 'NH'
+    ];
     const active = activeDialects();
     function dialected(el) {
-      return dialects.some((d) => el.classList.contains(d));
+      return DIALECTS.some((d) => el.classList.contains(d));
     }
     document.querySelectorAll('.dialect-parenthesis,.dialect-comma,.spelling-comma,.type').forEach((el) => {
       if (active == null) {
@@ -140,9 +140,12 @@ window.addEventListener('load', function() {'use strict';
     });
   }
   Array.prototype.forEach.call(document.getElementsByClassName('dialect'), (el) => {
+    const DIALECTS = [
+      'S', 'Sa', 'Sf', 'A', 'sA', 'B', 'F', 'Fb', 'O', 'NH'
+    ];
     el.classList.add('hover-link');
     el.onclick = () => {
-      const dClasses = dialects.filter((d) => el.classList.contains(d));
+      const dClasses = DIALECTS.filter((d) => el.classList.contains(d));
       if (dClasses.length != 1) {
         return;
       }