bb5c29b8debf86e91293375283e022fc41668b7b
Pishoy Girgis <pishoybg@gmail.com>
2024-08-23 09:48:49 +0200

    [#141][JavaScript/Pre-commit] Enforce Single Quotes.
    You might be wondering how this is relevant to #141. It's because
    consistency will help simplify our obfuscation algorithm.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import globals from "globals";
+import globals from 'globals';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -9,16 +10,20 @@ export default tseslint.config(
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
   {
+    plugins: {
+      '@stylistic': stylistic,
+    },
     rules: {
-      semi: "error",
-      "prefer-const": "error",
-      "indent": ["error", 2],
-      "max-len": ["warn", {
-        "code": 80,
-        "ignoreComments": false,
-        "ignoreTrailingComments": false,
+      semi: 'error',
+      'prefer-const': 'error',
+      'indent': ['error', 2],
+      'max-len': ['warn', {
+        'code': 80,
+        'ignoreComments': false,
+        'ignoreTrailingComments': false,
       }],
-      "@typescript-eslint/no-unused-expressions": "warn",
+      '@typescript-eslint/no-unused-expressions': 'warn',
+      '@stylistic/quotes': ['error', 'single'],
     },
     languageOptions: {
       globals: {
