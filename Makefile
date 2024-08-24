pishoyg/coptic

commit 7f3d9494af6db5b3bc0e67f84e24e7670b5b7da0

[#66][TODO/Makefile] Update the recipes for finding TODO's.

Makefile
@@ -74,7 +74,10 @@ diff: git_diff
 toil: crum_img_helper
 
 .PHONY: todo
-todo: todo_grep
+todo: _todo
+
+.PHONY: todo_no_issue
+todo_no_issue: _todo_no_issue
 
 .PHONY: update
 update: precommit_update pip_update
@@ -278,8 +281,13 @@ env_cp_from_home: FORCE
 precommit_update: FORCE
 	pre-commit autoupdate
 
-todo_grep: FORCE
-	. ./.helpers && grepexx . TODO
+# NOTE: We "mangle" the regex using extra parentheses in order to prevent it
+# from matching itself. Same below.
+_todo: FORCE
+	. ./.helpers && grepexx . 'TODO(:) '
+
+_todo_no_issue: FORCE
+	. ./.helpers && grepexx . 'TODO(:) (?!\(#[0-9]+\))' -P
 
 git_clean: FORCE
 	git clean \

commit d6dbec18c2fee68d59ffca9d4c2a863c18baf760

[#66][TODO/Makefile] Fix bug in the `todo` recipe.
The bug was introduced in 7f3d9494af6db5b3bc0e67f84e24e7670b5b7da0.
We forgot to add the `--extended-regexp` flag.
We also use `--perl-regexp` in the `todo_no_issue` recipe, instead of
the shorthand `-P`, for consistency, and visibility.

Makefile
@@ -282,10 +282,10 @@ precommit_update: FORCE
 # NOTE: We "mangle" the regex using extra parentheses in order to prevent it
 # from matching itself. Same below.
 _todo: FORCE
-	. ./.helpers && grepexx . 'TODO(:) '
+	. ./.helpers && grepexx . --extended-regexp 'TODO(:) '
 
 _todo_no_issue: FORCE
-	. ./.helpers && grepexx . 'TODO(:) (?!\(#[0-9]+\))' -P
+	. ./.helpers && grepexx . --perl-regexp 'TODO(:) (?!\(#[0-9]+\))'
 
 git_clean: FORCE
 	git clean \