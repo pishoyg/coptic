pishoyg/coptic

commit 187946699a477e97fbea3ed6cedca7c343869bf2

[#141][Site] Restructure Site Publishing to Allow more Granularity.
Define more flags to decide what the script should do.
Also tidy after obfuscation, as it might catch something.

Side change:
- Remove any spaces between function declarations and the parentheses,
  for uniformity.

Makefile
@@ -228,7 +228,12 @@ endif
 # SITE RULES
 
 site_publish:
-	bash site/publish.sh --pre --post
+	bash site/publish.sh \
+		--build \
+		--obf \
+		--tidy \
+		--comit \
+		--push
 
 # INFRASTRUCTURE RULES
 bin_install:

site/publish.sh
@@ -20,22 +20,39 @@ readonly ICON_TAG='  <link rel="icon" type="image/x-icon" href="/img/icon/icon-c
 '
 
 CLEAN=false
-PRE=false
-POST=false
+BUILD=false
+OBF=false
+TIDY=false
+COMMIT=false
+PUSH=false
 while [ $# -gt 0 ]; do
   case $1 in
   --clean)
     CLEAN=true
     ;;
-  --pre)
-    PRE=true
+  --build)
+    BUILD=true
     ;;
-  --post)
-    POST=true
+  --obf)
+    OBF=true
+    ;;
+  --tidy)
+    TIDY=true
+    ;;
+  --commit)
+    COMMIT=true
+    ;;
+  --push)
+    PUSH=true
     ;;
   --help)
-    echo -e "${GREEN}--pre${BLUE} regenerates the site in the site directory.${RESET}"
-    echo -e "${GREEN}--post${YELLOW} (1) ${BLUE}obfuscates the files, then ${YELLOW}(2) ${BLUE}creates, rebases, and force-pushes a fixup commit.${RESET}"
+    echo -e "${GREEN}--clean ${BLUE}CLEANES uncommitted changes from the site repo.${RESET}"
+    echo -e "${GREEN}--build ${BLUE}regenerates the site in the site repo.${RESET}"
+    echo -e "${GREEN}--obf ${BLUE}obfuscates the files.${RESET}"
+    echo -e "${GREEN}--tidy ${BLUE}tidies the HTML files.${RESET}"
+    echo -e "${GREEN}--commit ${BLUE}creates a FIXUP commit, and rebases it.${RESET}"
+    echo -e "${GREEN}--push ${BLUE}FORCE-pushes the commit to the repo.${RESET}"
+    echo -e "${BLUE}You can use any combination of flags that you want.${RESET}"
     exit
     ;;
   *)
@@ -46,15 +63,17 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-tidy () {
+_tidy() {
   command tidy -indent -modify -quiet --tidy-mark no -wrap 80 "${1}"
 }
 
-clean () {
+clean() {
+  echo -e "${GREEN}Cleaning.${RESET}"
   git -C "${SITE_DIR}" clean -d --force && git -C "${SITE_DIR}" reset --hard
 }
 
-pre () {
+build() {
+  echo -e "${GREEN}Building.${RESET}"
   if [ -n "$(git -C "${SITE_DIR}" status --short)" ]; then
     echo -e "${RED}The site repo is dirty. This should be done in a standalone commit.${RESET}"
     echo -e "${RED}You can (irreversibly) clean it by running the following:${RESET}"
@@ -76,7 +95,6 @@ pre () {
   BODY="$(python -m markdown \
     "site/home.md" \
     --output_format="html")" envsubst < "site/data/home.html" > "${SITE_DIR}/index.html"
-  tidy "${SITE_DIR}/index.html"
 
   mkdir "${IMG_DIR}"
   cp -r \
@@ -95,7 +113,7 @@ pre () {
     bible/stshenouda.org/data/output/html/sahidic \
     "${BIBLE_DIR}"
 
-  _html () {
+  _html() {
     FILE="${1}"
     LINE_NUM="$(grep "^<head>$" "${FILE}" --line-number --max-count=1 | cut -f1 -d:)"
     if [ -z "${LINE_NUM}" ]; then
@@ -104,7 +122,6 @@ pre () {
     fi
     NEW="$(head -n "${LINE_NUM}" "${FILE}")${GOOGLE_TAG}${ICON_TAG}$(tail -n "+$((LINE_NUM + 1))" "${FILE}")"
     echo "${NEW}" > "${FILE}"
-    tidy "${FILE}"
   }
 
   COUNTER=0
@@ -118,7 +135,8 @@ pre () {
   wait
 }
 
-post() {
+obf() {
+  echo -e "${GREEN}Obfuscating.${RESET}"
   python site/obfuscate_paths.py \
     --dir="${CRUM_DIR}"
 
@@ -137,10 +155,23 @@ post() {
       --rename-properties-mode "unsafe" \
       --unicode-escape-sequence "true"
   done
+}
 
+tidy() {
+  echo -e "${GREEN}Tidying.${RESET}"
+  find "${SITE_DIR}" -type f -name "*.html" | while read -r FILE; do
+    _tidy "${FILE}"
+  done
+}
+
+commit() {
+  echo -e "${GREEN}Committing.${RESET}"
   git -C "${SITE_DIR}" add --all
   git -C "${SITE_DIR}" commit --fixup HEAD
+}
 
+push() {
+  echo -e "${GREEN}Pushing.${RESET}"
   git -C "${SITE_DIR}" rebase --root --autosquash
   git -C "${SITE_DIR}" push --force
 }
@@ -148,9 +179,23 @@ post() {
 if ${CLEAN}; then
   clean
 fi
-if ${PRE}; then
-  pre
+
+if ${BUILD}; then
+  build
 fi
-if ${POST}; then
-  post
+
+if ${OBF}; then
+  obf
+fi
+
+if ${TIDY}; then
+  tidy
+fi
+
+if ${COMMIT}; then
+  commit
+fi
+
+if ${PUSH}; then
+  push
 fi

commit f2cbc3aa625d1bb555aa2c0c10624b715046f5cc

[#141][Site/Makefile] Fix typo in Flag Name.
The bug was introduced in 187946699a477e97fbea3ed6cedca7c343869bf2.

Makefile
@@ -232,7 +232,7 @@ site_publish:
 		--build \
 		--obf \
 		--tidy \
-		--comit \
+		--commit \
 		--push
 
 # INFRASTRUCTURE RULES