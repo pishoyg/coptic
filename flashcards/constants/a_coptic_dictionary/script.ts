pishoyg/coptic

commit da55052f412bcc3f9e9d56aa929b466be8af4098

[#141][Crum/TypeScript] Dialect Classes and Codes are Independent.
Rewrite the dialect handling logic to treat dialect classes and dialect
codes as separate lists of values that are not equal.

Also run `make ts_transpile flashcards test`.

Side changes:
- Rewrite `moveElement` to avoid the use of the non-null assertion
  operator.

flashcards/constants/a_coptic_dictionary/script.ts
@@ -36,6 +36,7 @@ const CLS_TYPE = 'type';
 const CLS_SPELLING = 'spelling';
 
 // The following classes represent the dialects.
+type Dialect = 'S' | 'Sa' | 'Sf' | 'A' | 'sA' | 'B' | 'F' | 'Fb' | 'O' | 'NH';
 const CLS_S: Dialect = 'S';
 const CLS_Sa: Dialect = 'Sa';
 const CLS_Sf: Dialect = 'Sf';
@@ -47,9 +48,55 @@ const CLS_Fb: Dialect = 'Fb';
 const CLS_O: Dialect = 'O';
 const CLS_NH: Dialect = 'NH';
 
-function querySelectorAll(...classes: string[]): NodeListOf<Element> {
-  return document.querySelectorAll(classes.map((c) => '.' + c).join(','));
-}
+const DIALECTS: readonly Dialect[] = [
+  CLS_S, CLS_Sa, CLS_Sf, CLS_A, CLS_sA, CLS_B, CLS_F, CLS_Fb, CLS_O, CLS_NH,
+];
+
+/*
+ * Dialect classes are to be found in:
+ * 1. HTML classes
+ * 2. JavaScript classes
+ *
+ * Dialect codes are to be found in:
+ * 1. Dialect Elements' inner HTML
+ * 2. The `d` parameter (which will be set using #1)
+ *
+ * For example, consider the following HTML:
+ * ```
+ * <span class="dialect cls_B">B</span>
+ * ```
+ *
+ * The class is "cls_B". The code is "B". The code will be used to set the
+ * parameter `d`. The class will be used in JavaScript.
+ * Thus, expect the following line to live in your JavaScript:
+ * ```
+ * const CLS_B: Dialect = "cls_B";
+ * ```
+ *
+ * We also use this HTML span to construct a mapping between dialect codes
+ * and classes.
+ * The mapping is restricted to the dialects that are present in a given
+ * page, and that's OK. On a given page, we have no need for the dialects
+ * that are absent from the page! Makes sense, eh?
+ *
+ * It is the classes that are used for internal processing, and it is the
+ * codes that are presented to the user. This way, the `d` parameter has a
+ * "pretty" value, and is persistent.
+ *
+ * As for the classes, we can obfuscate them as we like!
+ */
+
+const DIALECT_CODE_TO_CLASS: Map<string, Dialect> = (function(): Map<string, Dialect> {
+  const codeToClass: Map<string, Dialect> = new Map<string, Dialect>();
+  DIALECTS.forEach((cls) => {
+    const code: string | undefined = document.querySelector(`.${CLS_DIALECT}.${cls}`)?.innerHTML;
+    if (code === undefined) {
+      return;
+    }
+    codeToClass.set(code, cls);
+  });
+  return codeToClass;
+}());
 
 function get_url_or_local(
   param: string,
@@ -82,12 +129,12 @@ function set_url_and_local(param: string, value: string | null): void {
 function moveElement(el: HTMLElement, tag: string, attrs: Record<string, string>): void {
   const copy = document.createElement(tag);
   copy.innerHTML = el.innerHTML;
-  el.getAttributeNames().forEach((attr) => {
-    copy.setAttribute(attr, el.getAttribute(attr)!);
+  Array.from(el.attributes).forEach((att: Attr): void => {
+    copy.setAttribute(att.name, att.value);
   });
-  for (const [key, value] of Object.entries(attrs)) {
+  Object.entries(attrs).forEach(([key, value]: [string, string]): void => {
     copy.setAttribute(key, value);
-  }
+  });
   el.parentNode?.replaceChild(copy, el);
 }
 
@@ -201,12 +248,7 @@ Array.prototype.forEach.call(
   });
 
 // Handle CLS_DIALECT class.
-type Dialect = 'S' | 'Sa' | 'Sf' | 'A' | 'sA' | 'B' | 'F' | 'Fb' | 'O' | 'NH';
-const DIALECTS: readonly Dialect[] = [
-  CLS_S, CLS_Sa, CLS_Sf, CLS_A, CLS_sA, CLS_B, CLS_F, CLS_Fb, CLS_O, CLS_NH,
-];
-
-function activeDialects(): Set<Dialect> | null {
+function getActiveDialectClassesInCurrentPage(): Set<Dialect> | null {
   const d = get_url_or_local('d');
   if (d === null) {
     return null;
@@ -214,17 +256,34 @@ function activeDialects(): Set<Dialect> | null {
   if (d === '') {
     return new Set();
   }
-  return new Set(d.split(',').map((d) => d as Dialect));
+  return new Set<Dialect>(
+    d.split(',').filter((d: string): boolean => DIALECT_CODE_TO_CLASS.has(d)).map(
+      (d: string): Dialect => DIALECT_CODE_TO_CLASS.get(d)!));
+}
+
+function toggleDialect(code: string): void {
+  let d: string | null = get_url_or_local('d');
+  if (d === null) {
+    d = '';
+  }
+  const dd = new Set<string>(d.split(','));
+  if (dd.has(code)) {
+    dd.delete(code);
+  } else {
+    dd.add(code);
+  }
+  d = Array.from(dd).join(',');
+  set_url_and_local('d', d);
 }
 
 /* Update the display based on the value of the `d` parameter.
  */
 function dialect(): void {
-  const active: Set<Dialect> | null = activeDialects();
+  const active: Set<Dialect> | null = getActiveDialectClassesInCurrentPage();
   function dialected(el: Element): boolean {
     return DIALECTS.some((d: Dialect) => el.classList.contains(d));
   }
-  querySelectorAll(CLS_DIALECT_PARENTHESIS, CLS_DIALECT_COMMA, CLS_SPELLING_COMMA, CLS_TYPE).forEach(
+  document.querySelectorAll(`.${CLS_DIALECT_PARENTHESIS},.${CLS_DIALECT_COMMA},.${CLS_SPELLING_COMMA},.${CLS_TYPE}`).forEach(
     (el: Element) => {
       if (active === null) {
         el.classList.remove(CLS_VERY_LIGHT);
@@ -232,7 +291,7 @@ function dialect(): void {
         el.classList.add(CLS_VERY_LIGHT);
       }
     });
-  querySelectorAll(CLS_DIALECT, CLS_SPELLING).forEach((el: Element) => {
+  document.querySelectorAll(`.${CLS_DIALECT},.${CLS_SPELLING}`).forEach((el: Element) => {
     if (!dialected(el)) {
       return;
     }
@@ -256,26 +315,7 @@ Array.prototype.forEach.call(
   (el: HTMLElement) => {
     el.classList.add(CLS_HOVER_LINK);
     el.onclick = () => {
-      const dClasses: readonly Dialect[] = DIALECTS.filter(
-        (d) => el.classList.contains(d));
-      if (dClasses.length !== 1) {
-        // This is unexpected!
-        return;
-      }
-      const d: Dialect | undefined = dClasses[0];
-      if (!d) {
-        return;
-      }
-      let active = activeDialects();
-      if (active === null) {
-        active = new Set<Dialect>();
-      }
-      if (active.has(d)) {
-        active.delete(d);
-      } else {
-        active.add(d);
-      }
-      set_url_and_local('d', Array.from(active).join(','));
+      toggleDialect(el.innerHTML);
       dialect();
     };
   });

flashcards/data/build/a_coptic_dictionary/script.js
@@ -35,9 +35,21 @@ const CLS_F = 'F';
 const CLS_Fb = 'Fb';
 const CLS_O = 'O';
 const CLS_NH = 'NH';
-function querySelectorAll(...classes) {
-  return document.querySelectorAll(classes.map((c) => '.' + c).join(','));
-}
+const DIALECTS = [
+  CLS_S, CLS_Sa, CLS_Sf, CLS_A, CLS_sA, CLS_B, CLS_F, CLS_Fb, CLS_O, CLS_NH,
+];
+const DIALECT_CODE_TO_CLASS = (function () {
+  const codeToClass = new Map();
+  DIALECTS.forEach((cls) => {
+    var _a;
+    const code = (_a = document.querySelector(`.${CLS_DIALECT}.${cls}`)) === null || _a === void 0 ? void 0 : _a.innerHTML;
+    if (code === undefined) {
+      return;
+    }
+    codeToClass.set(code, cls);
+  });
+  return codeToClass;
+}());
 function get_url_or_local(param, default_value = null) {
   var _a, _b;
   return (_b = (_a = (new URLSearchParams(window.location.search)).get(param)) !== null && _a !== void 0 ? _a : localStorage.getItem(param)) !== null && _b !== void 0 ? _b : default_value;
@@ -64,12 +76,12 @@ function moveElement(el, tag, attrs) {
   var _a;
   const copy = document.createElement(tag);
   copy.innerHTML = el.innerHTML;
-  el.getAttributeNames().forEach((attr) => {
-    copy.setAttribute(attr, el.getAttribute(attr));
+  Array.from(el.attributes).forEach((att) => {
+    copy.setAttribute(att.name, att.value);
   });
-  for (const [key, value] of Object.entries(attrs)) {
+  Object.entries(attrs).forEach(([key, value]) => {
     copy.setAttribute(key, value);
-  }
+  });
   (_a = el.parentNode) === null || _a === void 0 ? void 0 : _a.replaceChild(copy, el);
 }
 Array.prototype.forEach.call(document.getElementsByClassName(CLS_CRUM_PAGE), (el) => {
@@ -136,10 +148,7 @@ Array.prototype.forEach.call(document.getElementsByClassName(CLS_EXPLANATORY_KEY
   el.classList.add(CLS_HOVER_LINK);
   moveElement(el, 'a', { 'href': `#explanatory${el.innerHTML}` });
 });
-const DIALECTS = [
-  CLS_S, CLS_Sa, CLS_Sf, CLS_A, CLS_sA, CLS_B, CLS_F, CLS_Fb, CLS_O, CLS_NH,
-];
-function activeDialects() {
+function getActiveDialectClassesInCurrentPage() {
   const d = get_url_or_local('d');
   if (d === null) {
     return null;
@@ -147,14 +156,29 @@ function activeDialects() {
   if (d === '') {
     return new Set();
   }
-  return new Set(d.split(',').map((d) => d));
+  return new Set(d.split(',').filter((d) => DIALECT_CODE_TO_CLASS.has(d)).map((d) => DIALECT_CODE_TO_CLASS.get(d)));
+}
+function toggleDialect(code) {
+  let d = get_url_or_local('d');
+  if (d === null) {
+    d = '';
+  }
+  const dd = new Set(d.split(','));
+  if (dd.has(code)) {
+    dd.delete(code);
+  }
+  else {
+    dd.add(code);
+  }
+  d = Array.from(dd).join(',');
+  set_url_and_local('d', d);
 }
 function dialect() {
-  const active = activeDialects();
+  const active = getActiveDialectClassesInCurrentPage();
   function dialected(el) {
     return DIALECTS.some((d) => el.classList.contains(d));
   }
-  querySelectorAll(CLS_DIALECT_PARENTHESIS, CLS_DIALECT_COMMA, CLS_SPELLING_COMMA, CLS_TYPE).forEach((el) => {
+  document.querySelectorAll(`.${CLS_DIALECT_PARENTHESIS},.${CLS_DIALECT_COMMA},.${CLS_SPELLING_COMMA},.${CLS_TYPE}`).forEach((el) => {
     if (active === null) {
       el.classList.remove(CLS_VERY_LIGHT);
     }
@@ -162,7 +186,7 @@ function dialect() {
       el.classList.add(CLS_VERY_LIGHT);
     }
   });
-  querySelectorAll(CLS_DIALECT, CLS_SPELLING).forEach((el) => {
+  document.querySelectorAll(`.${CLS_DIALECT},.${CLS_SPELLING}`).forEach((el) => {
     if (!dialected(el)) {
       return;
     }
@@ -184,25 +208,7 @@ function dialect() {
 Array.prototype.forEach.call(document.getElementsByClassName(CLS_DIALECT), (el) => {
   el.classList.add(CLS_HOVER_LINK);
   el.onclick = () => {
-    const dClasses = DIALECTS.filter((d) => el.classList.contains(d));
-    if (dClasses.length !== 1) {
-      return;
-    }
-    const d = dClasses[0];
-    if (!d) {
-      return;
-    }
-    let active = activeDialects();
-    if (active === null) {
-      active = new Set();
-    }
-    if (active.has(d)) {
-      active.delete(d);
-    }
-    else {
-      active.add(d);
-    }
-    set_url_and_local('d', Array.from(active).join(','));
+    toggleDialect(el.innerHTML);
     dialect();
   };
 });

flashcards/data/output/web/a_coptic_dictionary__all_dialects/script.js
@@ -36,9 +36,21 @@ window.addEventListener('load', () => {
   const CLS_Fb = 'Fb';
   const CLS_O = 'O';
   const CLS_NH = 'NH';
-  function querySelectorAll(...classes) {
-    return document.querySelectorAll(classes.map((c) => '.' + c).join(','));
-  }
+  const DIALECTS = [
+    CLS_S, CLS_Sa, CLS_Sf, CLS_A, CLS_sA, CLS_B, CLS_F, CLS_Fb, CLS_O, CLS_NH,
+  ];
+  const DIALECT_CODE_TO_CLASS = (function () {
+    const codeToClass = new Map();
+    DIALECTS.forEach((cls) => {
+      var _a;
+      const code = (_a = document.querySelector(`.${CLS_DIALECT}.${cls}`)) === null || _a === void 0 ? void 0 : _a.innerHTML;
+      if (code === undefined) {
+        return;
+      }
+      codeToClass.set(code, cls);
+    });
+    return codeToClass;
+  }());
   function get_url_or_local(param, default_value = null) {
     var _a, _b;
     return (_b = (_a = (new URLSearchParams(window.location.search)).get(param)) !== null && _a !== void 0 ? _a : localStorage.getItem(param)) !== null && _b !== void 0 ? _b : default_value;
@@ -65,12 +77,12 @@ window.addEventListener('load', () => {
     var _a;
     const copy = document.createElement(tag);
     copy.innerHTML = el.innerHTML;
-    el.getAttributeNames().forEach((attr) => {
-      copy.setAttribute(attr, el.getAttribute(attr));
+    Array.from(el.attributes).forEach((att) => {
+      copy.setAttribute(att.name, att.value);
     });
-    for (const [key, value] of Object.entries(attrs)) {
+    Object.entries(attrs).forEach(([key, value]) => {
       copy.setAttribute(key, value);
-    }
+    });
     (_a = el.parentNode) === null || _a === void 0 ? void 0 : _a.replaceChild(copy, el);
   }
   Array.prototype.forEach.call(document.getElementsByClassName(CLS_CRUM_PAGE), (el) => {
@@ -137,10 +149,7 @@ window.addEventListener('load', () => {
     el.classList.add(CLS_HOVER_LINK);
     moveElement(el, 'a', { 'href': `#explanatory${el.innerHTML}` });
   });
-  const DIALECTS = [
-    CLS_S, CLS_Sa, CLS_Sf, CLS_A, CLS_sA, CLS_B, CLS_F, CLS_Fb, CLS_O, CLS_NH,
-  ];
-  function activeDialects() {
+  function getActiveDialectClassesInCurrentPage() {
     const d = get_url_or_local('d');
     if (d === null) {
       return null;
@@ -148,14 +157,29 @@ window.addEventListener('load', () => {
     if (d === '') {
       return new Set();
     }
-    return new Set(d.split(',').map((d) => d));
+    return new Set(d.split(',').filter((d) => DIALECT_CODE_TO_CLASS.has(d)).map((d) => DIALECT_CODE_TO_CLASS.get(d)));
+  }
+  function toggleDialect(code) {
+    let d = get_url_or_local('d');
+    if (d === null) {
+      d = '';
+    }
+    const dd = new Set(d.split(','));
+    if (dd.has(code)) {
+      dd.delete(code);
+    }
+    else {
+      dd.add(code);
+    }
+    d = Array.from(dd).join(',');
+    set_url_and_local('d', d);
   }
   function dialect() {
-    const active = activeDialects();
+    const active = getActiveDialectClassesInCurrentPage();
     function dialected(el) {
       return DIALECTS.some((d) => el.classList.contains(d));
     }
-    querySelectorAll(CLS_DIALECT_PARENTHESIS, CLS_DIALECT_COMMA, CLS_SPELLING_COMMA, CLS_TYPE).forEach((el) => {
+    document.querySelectorAll(`.${CLS_DIALECT_PARENTHESIS},.${CLS_DIALECT_COMMA},.${CLS_SPELLING_COMMA},.${CLS_TYPE}`).forEach((el) => {
       if (active === null) {
         el.classList.remove(CLS_VERY_LIGHT);
       }
@@ -163,7 +187,7 @@ window.addEventListener('load', () => {
         el.classList.add(CLS_VERY_LIGHT);
       }
     });
-    querySelectorAll(CLS_DIALECT, CLS_SPELLING).forEach((el) => {
+    document.querySelectorAll(`.${CLS_DIALECT},.${CLS_SPELLING}`).forEach((el) => {
       if (!dialected(el)) {
         return;
       }
@@ -185,25 +209,7 @@ window.addEventListener('load', () => {
   Array.prototype.forEach.call(document.getElementsByClassName(CLS_DIALECT), (el) => {
     el.classList.add(CLS_HOVER_LINK);
     el.onclick = () => {
-      const dClasses = DIALECTS.filter((d) => el.classList.contains(d));
-      if (dClasses.length !== 1) {
-        return;
-      }
-      const d = dClasses[0];
-      if (!d) {
-        return;
-      }
-      let active = activeDialects();
-      if (active === null) {
-        active = new Set();
-      }
-      if (active.has(d)) {
-        active.delete(d);
-      }
-      else {
-        active.add(d);
-      }
-      set_url_and_local('d', Array.from(active).join(','));
+      toggleDialect(el.innerHTML);
       dialect();
     };
   });

commit 92ed6929f25524255acb37562929a0041cd76834

[#141][Crum/TypeScript] Fix Bug. Don't Split an Empty String.
The bug was introduced in da55052f412bcc3f9e9d56aa929b466be8af4098.

flashcards/constants/a_coptic_dictionary/script.ts
@@ -262,18 +262,14 @@ function getActiveDialectClassesInCurrentPage(): Set<Dialect> | null {
 }
 
 function toggleDialect(code: string): void {
-  let d: string | null = get_url_or_local('d');
-  if (d === null) {
-    d = '';
-  }
-  const dd = new Set<string>(d.split(','));
+  const cur: string | null = get_url_or_local('d');
+  const dd = new Set<string>(cur ? cur.split(',') : []);
   if (dd.has(code)) {
     dd.delete(code);
   } else {
     dd.add(code);
   }
-  d = Array.from(dd).join(',');
-  set_url_and_local('d', d);
+  set_url_and_local('d', Array.from(dd).join(','));
 }
 
 /* Update the display based on the value of the `d` parameter.

flashcards/data/build/a_coptic_dictionary/script.js
@@ -159,19 +159,15 @@ function getActiveDialectClassesInCurrentPage() {
   return new Set(d.split(',').filter((d) => DIALECT_CODE_TO_CLASS.has(d)).map((d) => DIALECT_CODE_TO_CLASS.get(d)));
 }
 function toggleDialect(code) {
-  let d = get_url_or_local('d');
-  if (d === null) {
-    d = '';
-  }
-  const dd = new Set(d.split(','));
+  const cur = get_url_or_local('d');
+  const dd = new Set(cur ? cur.split(',') : []);
   if (dd.has(code)) {
     dd.delete(code);
   }
   else {
     dd.add(code);
   }
-  d = Array.from(dd).join(',');
-  set_url_and_local('d', d);
+  set_url_and_local('d', Array.from(dd).join(','));
 }
 function dialect() {
   const active = getActiveDialectClassesInCurrentPage();

flashcards/data/output/web/a_coptic_dictionary__all_dialects/script.js
@@ -160,19 +160,15 @@ window.addEventListener('load', () => {
     return new Set(d.split(',').filter((d) => DIALECT_CODE_TO_CLASS.has(d)).map((d) => DIALECT_CODE_TO_CLASS.get(d)));
   }
   function toggleDialect(code) {
-    let d = get_url_or_local('d');
-    if (d === null) {
-      d = '';
-    }
-    const dd = new Set(d.split(','));
+    const cur = get_url_or_local('d');
+    const dd = new Set(cur ? cur.split(',') : []);
     if (dd.has(code)) {
       dd.delete(code);
     }
     else {
       dd.add(code);
     }
-    d = Array.from(dd).join(',');
-    set_url_and_local('d', d);
+    set_url_and_local('d', Array.from(dd).join(','));
   }
   function dialect() {
     const active = getActiveDialectClassesInCurrentPage();