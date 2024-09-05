45655738c1336e9a7f9ccb19fa6b30dfec4bb651
Pishoy Girgis <pishoybg@gmail.com>
2024-09-05 07:29:48 +0200

    [Site/Makefile] Update the Build Error Message to Point to `--clean`.

--- site/publish.sh
+++ site/publish.sh
@@ -63,6 +63,8 @@ build() {
   if [ -n "$(git -C "${SITE_DIR}" status --short)" ]; then
     echo -e "${RED}The site repo is dirty. This should be done in a standalone commit.${RESET}"
     echo -e "${RED}You can (irreversibly) clean it by running the following:${RESET}"
+    echo -e "${PURPLE}bash site/publish.sh --clean${RESET}"
+    echo -e "${RED}You can also do it manually:${RESET}"
     echo -e "${PURPLE}git -C '${SITE_DIR}' clean -d --force${RESET}"
     echo -e "${PURPLE}git -C '${SITE_DIR}' reset --hard${RESET}"
     exit 1
