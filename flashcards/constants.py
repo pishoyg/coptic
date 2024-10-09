pishoyg/coptic

commit b488103ce4a5a90c0962106c4d661b7a7f7004ea

[#264][copticsite/Flashcards] Add HTML Classes for Highlighting.

flashcards/constants.py
@@ -461,9 +461,14 @@ def copticsite_com(deck_name: str, deck_id: int) -> deck.deck:
         # NOTE: The key is a protected field. Do not change unless you know what
         # you're doing.
         key=field.seq(),
-        front=tsv_col("prettify", force=False),
+        front=field.cat(
+            '<span class="dialect B">',
+            tsv_col("prettify", force=False),
+            "</span>",
+        ),
         back=field.cat(
             field.aon(
+                '<span class="type B">',
                 "(",
                 "<b>",
                 field.jne(
@@ -474,6 +479,7 @@ def copticsite_com(deck_name: str, deck_id: int) -> deck.deck:
                 ),
                 "</b>",
                 ")",
+                "</span>",
                 "<br/>",
             ),
             tsv_col("Meaning", line_br=True, force=False),

commit 235c55a52f92935badb8cbcbad17e9d94f79bb59

[#264][copticsite/Flashcards] Use the Correct HTML Class.
(Ineffectual) bug was introduced in
b488103ce4a5a90c0962106c4d661b7a7f7004ea.

It has been discovered during the work on #230.

flashcards/constants.py
@@ -462,7 +462,7 @@ def copticsite_com(deck_name: str, deck_id: int) -> deck.deck:
         # you're doing.
         key=field.seq(),
         front=field.cat(
-            '<span class="dialect B">',
+            '<span class="spelling B">',
             tsv_col("prettify", force=False),
             "</span>",
         ),