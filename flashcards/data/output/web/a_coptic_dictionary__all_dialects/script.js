48669c92e662e1fa97bc4353cb29d1b3e921ae1e
Pishoy Girgis <pishoybg@gmail.com>
2024-10-15 18:09:16 +0200

    [#270,#241][Crum/Sisters] Run `make flashcards test`.

--- flashcards/data/output/web/a_coptic_dictionary__all_dialects/script.js
+++ flashcards/data/output/web/a_coptic_dictionary__all_dialects/script.js
@@ -34,7 +34,6 @@ window.addEventListener('load', () => {
   const CLS_SPELLING_COMMA = 'spelling-comma';
   const CLS_TYPE = 'type';
   const CLS_SPELLING = 'spelling';
-  const CLS_SISTERS = 'sisters';
   const CLS_SISTER_KEY = 'sister-key';
   const LOOKUP_URL_PREFIX = 'https://remnqymi.com/crum/?query=';
   const DAWOUD_OFFSET = 16;
@@ -126,11 +125,6 @@ window.addEventListener('load', () => {
   });
   // Handle CLS_COPTIC class.
   Array.prototype.forEach.call(document.getElementsByClassName(CLS_COPTIC), (el) => {
-    if (el.closest(`.${CLS_SISTERS}`)) {
-    // Exclude any element with a "sisters" parent. This is because sisters
-    // have their own links.
-      return;
-    }
     el.classList.add(CLS_HOVER_LINK);
     el.onclick = () => {
       window_open(LOOKUP_URL_PREFIX + el.innerHTML);
