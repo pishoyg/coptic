pishoyg/coptic

commit cf50310441c2701a948735cee3af028d07d9757d

[#278,#229][Crum/Flashcards,Xooxle] Miscellaneous Updates.
Summary of changes:
- Vastly simplify the dialect highlighting logic in crum.ts. Besides the
  simplification, this also renders more beautifully, because the
  previous logic used to leave some spaces bright that should be dimmed,
  or dim spaces that should be brightened; such as the spaces between
  dialect codes or between spellings. Dialect commas and spellings are
  separated by the string ", ". The first character (the comma) got
  highlighted, but the second (the space) didn't, because HTML Tidy left
  it outside of the `<span>` tags.
- Retain the class "word" in the Xooxle index because it's now relevant
  for highlighting. Retain "type" for completion, it might turn out to
  be useful later.
- Fix bug in Xooxle index construction. Tags that need to be retained
  get retained, but the retention doesn't propagate to all of their
  children. Only retain the child tags if their retention is required by
  the parameters.
- Add some doc comments.

site/crum.ts
@@ -26,29 +26,6 @@ const EMAIL_LINK = `mailto:${EMAIL}`;
 
 const DAWOUD_OFFSET = 16;
 
-const DIALECTS: readonly string[] = [
-  // Crum dialects.
-  'S',
-  'Sa',
-  'Sf',
-  'A',
-  'sA',
-  'B',
-  'F',
-  'Fb',
-  'O',
-  'NH',
-  // KELLIA-only dialects.
-  'Ak',
-  'M',
-  'L',
-  'P',
-  'V',
-  'W',
-  'U',
-  'K',
-];
-
 // DIALECT_SINGLE_CHAR is a mapping for the dialects that have shortcuts other
 // than their codes. If the shortcut to toggle a dialect is not the same as its
 // code, it should be included in this record.
@@ -66,29 +43,28 @@ class Highlighter {
   // individually instead!
   private readonly anki: boolean;
   private readonly sheet: CSSStyleSheet | null;
-  private readonly spellingRuleIndex: number;
-  private readonly undialectedRuleIndex: number;
-  private readonly punctuationRuleIndex: number;
+  private readonly dialectRuleIndex: number;
   private readonly devRuleIndex: number;
 
   private static readonly BRIGHT = '1.0';
   private static readonly DIM = '0.3';
 
   constructor() {
-    // Reading CSS rules often fails locally due to CORS.
+    // NOTE: Reading CSS rules often fails locally due to CORS. This is why we
+    // use the `try` block here. In case it fails, we fall back to Anki mode,
+    // which doesn't need to read the CSS.
+    // This failure, however, is not expected to be encountered if you're
+    // reading locally through a server. It only fails when you open the HTML
+    // file in the browser directly.
     try {
       this.anki = anki();
       this.sheet = this.anki ? null : window.document.styleSheets[0]!;
-      this.spellingRuleIndex = this.sheet?.cssRules.length ?? 0;
-      this.undialectedRuleIndex = (this.sheet?.cssRules.length ?? 0) + 1;
-      this.punctuationRuleIndex = (this.sheet?.cssRules.length ?? 0) + 2;
-      this.devRuleIndex = (this.sheet?.cssRules.length ?? 0) + 3;
+      this.dialectRuleIndex = this.sheet?.cssRules.length ?? 0;
+      this.devRuleIndex = this.dialectRuleIndex + 1;
     } catch {
       this.anki = true;
       this.sheet = null;
-      this.spellingRuleIndex = 0;
-      this.undialectedRuleIndex = 0;
-      this.punctuationRuleIndex = 0;
+      this.dialectRuleIndex = 0;
       this.devRuleIndex = 0;
     }
   }
@@ -105,18 +81,13 @@ class Highlighter {
       return;
     }
 
-    const query: string | null = active?.map((d) => `.${d}`).join(',') ?? '';
-    this.addOrReplaceRule(
-      this.spellingRuleIndex,
-      query
-        ? `.spelling:not(${query}), .dialect:not(${query}) {opacity: ${Highlighter.DIM};}`
-        : `.spelling, .dialect {opacity: ${String(active === null ? Highlighter.BRIGHT : Highlighter.DIM)};}`);
-    this.addOrReplaceRule(
-      this.undialectedRuleIndex,
-      `.spelling:not(${DIALECTS.map((d) => `.${d}`).join(',')}) { opacity: ${String(active === null || query ? Highlighter.BRIGHT : Highlighter.DIM)}; }`);
-    this.addOrReplaceRule(
-      this.punctuationRuleIndex,
-      `.dialect-parenthesis, .dialect-comma, .spelling-comma, .type { opacity: ${String(active === null ? Highlighter.BRIGHT : Highlighter.DIM)}; }`);
+    const style = active === null
+      ? '.word {}' // No dialect highlighting whatsoever!
+      : active.length === 0
+        ? `.word { opacity: ${Highlighter.DIM}; }`  // All dialects are off.
+        : `.word > :not(${active.map((d) => `.${d}`).join(',')}) {opacity: ${Highlighter.DIM};}`;  // Some dialects are off.
+
+    this.addOrReplaceRule(this.dialectRuleIndex, style);
   }
 
   updateDev(): void {
@@ -129,39 +100,32 @@ class Highlighter {
   }
 
   private updateDialectsNoSheet(active: string[] | null): void {
-    if (active === null) {
-      // Highlighting is off. Show everything.
-      document.querySelectorAll<HTMLElement>('.spelling, .dialect, .dialect-parenthesis, .dialect-comma, .spelling-comma, .type').forEach(
-        (el: HTMLElement) => {
-          el.style.opacity = Highlighter.BRIGHT;
-        }
-      );
+    // If active.length === 0 (all dialects off), we dim all elements and
+    // return.
+    // If active === null (no highlighting requested), we brighten all elements
+    // and return.
+    // Otherwise, if we need to highlight a subset of the elements, we initially
+    // brighten all of them, then we selectively dim the ones that are not
+    // selected.
+    const init = (active !== null && active.length === 0)
+      ? Highlighter.DIM : Highlighter.BRIGHT;
+    document.querySelectorAll<HTMLElement>('.word, .word *').forEach(
+      (el: HTMLElement) => {
+        el.style.opacity = init;
+      }
+    );
+
+    if (active === null || active.length === 0) {
+      // Our job is already done.
       return;
     }
 
-    // Update spelling highlighting.
-    document.querySelectorAll<HTMLElement>('.spelling, .dialect').forEach(
-      (el: HTMLElement) => {
-        let opacity: string = Highlighter.DIM;
-        if (active.some((d) => el.classList.contains(d))) {
-          opacity = Highlighter.BRIGHT;
-        } else if (
-          active.length > 1 &&
-          !DIALECTS.some((d: string) => el.classList.contains(d))) {
-          // If the element has no dialects, it should be shown provided that at
-          // least one dialect is active.
-          opacity = Highlighter.BRIGHT;
-        }
-
-        el.style.opacity = opacity;
-      });
-
-    // Update punctuation highlighting.
-    document.querySelectorAll<HTMLElement>('.dialect-parenthesis, .dialect-comma, .spelling-comma, .type').forEach(
+    document.querySelectorAll<HTMLElement>(`.word > :not(${active.map((d) => `.${d}`).join(',')})`).forEach(
       (el: HTMLElement) => {
         el.style.opacity = Highlighter.DIM;
       });
   }
+
   private updateDevNoSheet(display: string): void {
     document.querySelectorAll<HTMLElement>('.dev, .nag-hammadi').forEach(
       (el: HTMLElement) => {

site/constants.py
@@ -1,8 +1,14 @@
 import xooxle
 
+# Xooxle search will work fine even if we don't retain any HTML tags, because it
+# relies entirely on searching the text payloads of the HTML. However, we retain
+# the subset of the classes that are needed for highlighting, in order to make
+# the Xooxle search results pretty.
 _CRUM_RETAIN_CLASSES = {
+    "word",
     "dialect",
     "spelling",
+    "type",
     "dialect-comma",
     "spelling-comma",
     "dialect-parenthesis",
@@ -11,7 +17,7 @@ _CRUM_RETAIN_CLASSES = {
 _KELLIA_RETAIN_CLASSES = {
     "spelling",
     "dialect",
-    "gram_grp",
+    "type",
     "lang",
     "bibl",
     "ref",

site/xooxle.py
@@ -94,14 +94,23 @@ def _get_text(
     def __get_text(tag: bs4.Tag) -> typing.Generator:
         for child in tag.children:
             if isinstance(child, bs4.Tag):
-                if child.name in retain_tags:
-                    yield _clean_html(child)
-                    continue
-                if child.has_attr("class") and retain_classes.intersection(
-                    set(child["class"]),
+                if child.name in retain_tags or retain_classes.intersection(
+                    child.get_attribute_list("class"),
                 ):
-                    child.name = "span"
-                    yield _clean_html(child)
+                    # We need to retain this tag.
+                    name = child.name if child.name in retain_tags else "span"
+                    if child.has_attr("class"):
+                        yield f'<{name} class="{
+                                " ".join(child.get_attribute_list("class"))
+                        }">'
+                    else:
+                        yield f"<{name}>"
+                    # Mangle the child to make sure it doesn't get picked up
+                    # again, but its children should.
+                    child.name = "RANDOM"
+                    child.attrs = {}
+                    yield from __get_text(child)
+                    yield f"</{name}>"
                     continue
                 # If the tag is a block type tag, then yield new lines before
                 # and after.

commit 77df5042faa46d7637cfa02a133727883a379f9c

[#278][Crum/Flashcards] Fix Undialected Highlighting.
It broke in cf50310441c2701a948735cee3af028d07d9757d.

site/crum.ts
@@ -26,6 +26,30 @@ const EMAIL_LINK = `mailto:${EMAIL}`;
 
 const DAWOUD_OFFSET = 16;
 
+const DIALECTS = [
+  // The following dialects are found in Crum.
+  'S',
+  'Sa',
+  'Sf',
+  'A',
+  'sA',
+  'B',
+  'F',
+  'Fb',
+  'O',
+  // The following dialects are only found in Marcion.
+  'NH',
+  // The following dialects are only found in TLA / KELLIA.
+  'Ak',
+  'M',
+  'L',
+  'P',
+  'V',
+  'W',
+  'U',
+  'K',
+];
+
 // DIALECT_SINGLE_CHAR is a mapping for the dialects that have shortcuts other
 // than their codes. If the shortcut to toggle a dialect is not the same as its
 // code, it should be included in this record.
@@ -37,6 +61,10 @@ const DIALECT_SINGLE_CHAR: Record<string, string> = {
   'b': 'Fb',
   'k': 'Ak',
 };
+
+function classQuery(classes: string[]): string {
+  return classes.map(c => `.${c}`).join(', ');
+}
 
 class Highlighter {
   // Sheets are problematic on Anki, for some reason! We update the elements
@@ -74,6 +102,14 @@ class Highlighter {
     this.updateDev();
   }
 
+  nonEmptyActiveQuery(active: string[]): string {
+    // Some dialects are off.
+    // Dim all children of `word` elements, with the exception of:
+    // - Active dialects.
+    // - Undialected spellings.
+    return `.word > :not(${classQuery(active)},.spelling:not(${classQuery(DIALECTS)}))`;
+  }
+
   updateDialects(): void {
     const active = activeDialects();
     if (this.anki) {
@@ -81,11 +117,17 @@ class Highlighter {
       return;
     }
 
-    const style = active === null
-      ? '.word {}' // No dialect highlighting whatsoever!
-      : active.length === 0
-        ? `.word { opacity: ${Highlighter.DIM}; }`  // All dialects are off.
-        : `.word > :not(${active.map((d) => `.${d}`).join(',')}) {opacity: ${Highlighter.DIM};}`;  // Some dialects are off.
+    let style: string;
+
+    if (active === null) {
+      // No dialect highlighting whatsoever!
+      style = '.word {}';
+    } else if (active.length === 0) {
+      // All dialects are off.
+      style = `.word { opacity: ${Highlighter.DIM}; }`;
+    } else {
+      style = `${this.nonEmptyActiveQuery(active)} { opacity: ${Highlighter.DIM}; }`;
+    }
 
     this.addOrReplaceRule(this.dialectRuleIndex, style);
   }
@@ -109,7 +151,7 @@ class Highlighter {
     // selected.
     const init = (active !== null && active.length === 0)
       ? Highlighter.DIM : Highlighter.BRIGHT;
-    document.querySelectorAll<HTMLElement>('.word, .word *').forEach(
+    document.querySelectorAll<HTMLElement>('.word *').forEach(
       (el: HTMLElement) => {
         el.style.opacity = init;
       }
@@ -120,7 +162,8 @@ class Highlighter {
       return;
     }
 
-    document.querySelectorAll<HTMLElement>(`.word > :not(${active.map((d) => `.${d}`).join(',')})`).forEach(
+    document.querySelectorAll<HTMLElement>(
+      this.nonEmptyActiveQuery(active)).forEach(
       (el: HTMLElement) => {
         el.style.opacity = Highlighter.DIM;
       });