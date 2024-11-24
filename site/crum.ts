95791c05763b8c2769961c07f8e4e1c32005df36
Pishoy Girgis <pishoybg@gmail.com>
2024-11-24 21:12:07 +0100

    [#278][Crum/TypeScript] Use R for Email. Err to Console, not Alert.
    A shortcut for the senses was introduced in
    660a967b9a0506a3c4531c6ab2af2bb2be887bbe, which conflicts with the email
    shortcut.
    
    Also, alerting for this error is obtrusive. It made it to production
    today!

--- site/crum.ts
+++ site/crum.ts
@@ -541,9 +541,7 @@ class HelpPanel {
       if (canConsume.length <= 1) {
         return;
       }
-      // Alerting is a good option if developers always test locally, otherwise
-      // this might make it to production!
-      alert(`${key} is consumable by multiple shortcuts: ${canConsume.map((s) => s.textDescription()).join(', ')}`);
+      console.error(`${key} is consumable by multiple shortcuts: ${canConsume.map((s) => s.textDescription()).join(', ')}`);
     });
   }
 
@@ -700,7 +698,7 @@ function makeHelpPanel(): HelpPanel {
     d: [new Shortcut('Developer mode', Where.XOOXLE_AND_NOTE, () => {
       toggleDev(); highlighter.updateDev();
     })],
-    e: [new Shortcut(`Email <a class="contact" href="${EMAIL_LINK}">${EMAIL}</a>`, Where.XOOXLE_AND_NOTE, () => {
+    R: [new Shortcut(`Reports / Contact <a class="contact" href="${EMAIL_LINK}">${EMAIL}</a>`, Where.XOOXLE_AND_NOTE, () => {
       window_open(EMAIL_LINK);
     })],
     h: [new Shortcut('Open homepage', Where.XOOXLE_AND_NOTE, () => {
