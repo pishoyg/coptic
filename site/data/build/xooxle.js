381a50e4e038dc0d2ba7123b9363b8cd52a20f13
Pishoy Girgis <pishoybg@gmail.com>
2024-11-25 22:12:14 +0100

    [#300][Xooxle] Run `make transpile test`.

--- site/data/build/xooxle.js
+++ site/data/build/xooxle.js
@@ -399,8 +399,17 @@ async function searchOneDictionary(regex, xooxle, abortController) {
     ++count;
     // Create a new row for the table
     const row = document.createElement('tr');
+    // Add the view cell.
     const viewCell = document.createElement('td');
-    viewCell.classList.add('view');
+    const viewTable = document.createElement('table');
+    const viewRow = document.createElement('tr');
+    const viewIndexCell = document.createElement('td');
+    viewTable.classList.add('view-table');
+    viewIndexCell.classList.add('view-index');
+    viewIndexCell.innerHTML = '&nbsp;';
+    viewRow.appendChild(viewIndexCell);
+    viewTable.appendChild(viewRow);
+    viewCell.appendChild(viewTable);
     if (xooxle.params.href_fmt) {
       // Get the word of the first field that has a match.
       const word = search_results.find((sr) => sr.match).word;
@@ -408,14 +417,18 @@ async function searchOneDictionary(regex, xooxle, abortController) {
       a.href = xooxle.params.href_fmt.replace(`{${KEY}}`, res.key) + `#:~:text=${encodeURIComponent(word)}`;
       a.target = '_blank';
       a.textContent = localStorage.getItem('dev') === 'true' ? res.key : 'view';
-      viewCell.appendChild(a);
+      const viewLinkCell = document.createElement('td');
+      viewLinkCell.appendChild(a);
+      viewRow.appendChild(viewLinkCell);
     }
     row.appendChild(viewCell);
+    // Add the content cells.
     search_results.forEach((sr) => {
       const cell = document.createElement('td');
       cell.innerHTML = sr.html;
       row.appendChild(cell);
     });
+    // Insert the row in the correct position.
     column_sentinels[search_results.findIndex((sr) => sr.match)].insertAdjacentElement('beforebegin', row);
     // TODO: Remove the dependency on the HTML structure.
     const collapsible = resultTable.parentElement.parentElement;
@@ -426,6 +439,7 @@ async function searchOneDictionary(regex, xooxle, abortController) {
       await new Promise((resolve) => setTimeout(resolve, 0));
     }
   }
+  // Add the indices to the view cell.
   let counter = 0;
   resultTable.childNodes.forEach((node) => {
     const tr = node;
@@ -436,8 +450,12 @@ async function searchOneDictionary(regex, xooxle, abortController) {
     const small = document.createElement('small');
     small.classList.add('very-light');
     small.innerHTML = `${(++counter).toString()} / ${count.toString()}`;
-    const td = tr.firstElementChild;
-    td.prepend(' ');
+    const td = tr
+      .firstElementChild // <td>
+      ?.firstElementChild // <table class="view-table">
+      ?.firstElementChild // <tr>
+      ?.firstElementChild;
+    td.innerHTML = ''; // Clear the previous placeholder.
     td.prepend(small);
   });
 }
