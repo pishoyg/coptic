bee84d7d28bb15601efe5ba780dbd1ff28161de4
Pishoy Girgis <pishoybg@gmail.com>
2025-02-24 20:42:30 +0100

    [#125][Dawoud] Run `make transpile test`.

--- site/data/build/crum.js
+++ site/data/build/crum.js
@@ -1160,7 +1160,7 @@ function handleNonXooxleOnlyElements() {
     el.classList.add('link');
     el.onclick = () => {
       window_open(
-        `${dawoud()}${(+el.innerHTML + DAWOUD_OFFSET).toString()}.jpg`
+        `${dawoud()}?page=${(+el.innerHTML + DAWOUD_OFFSET).toString()}`
       );
     };
   });
@@ -1171,7 +1171,7 @@ function handleNonXooxleOnlyElements() {
     el.classList.add('link');
     el.onclick = () => {
       window_open(
-        `${dawoud()}${(+el.getAttribute('alt') + DAWOUD_OFFSET).toString()}.jpg`
+        `${dawoud()}?page=${(+el.getAttribute('alt') + DAWOUD_OFFSET).toString()}`
       );
     };
   });
--- site/data/build/dawoud.js
+++ site/data/build/dawoud.js
@@ -3,64 +3,144 @@ const _HOME = '/';
 const _CRUM = '/crum/';
 const MIN_PAGE_NUM = 0;
 const MAX_PAGE_NUM = 1060;
-const next = document.getElementById('next');
-const prev = document.getElementById('prev');
-const imageElement = document.getElementById('scroll-dawoud-image');
-function getPageParam() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const page = urlParams.get('page');
-  return page ? parseInt(page) : MIN_PAGE_NUM;
-}
-function update(page) {
-  if (page < MIN_PAGE_NUM) {
-    page = MIN_PAGE_NUM;
+const image = document.getElementById('scroll-dawoud-image');
+const nextButton = document.getElementById('next');
+const prevButton = document.getElementById('prev');
+const resetButton = document.getElementById('reset');
+class Scroller {
+  constructor() {
+    this.initEventListeners();
+    this.update(this.getPageParam());
   }
-  if (page > MAX_PAGE_NUM) {
-    page = MAX_PAGE_NUM;
+  getPageParam() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const page = urlParams.get('page');
+    return page ? parseInt(page) : MIN_PAGE_NUM;
   }
-  updatePageParam(page);
-  updateDisplay(page);
-}
-function updatePageParam(newPage) {
-  const url = new URL(window.location.href);
-  url.searchParams.set('page', newPage.toString());
-  window.history.pushState({}, '', url.toString());
-}
-function updateDisplay(page) {
-  imageElement.src = `${page.toString()}.jpg`;
-  imageElement.alt = page.toString();
-  if (page === MIN_PAGE_NUM) {
-    prev?.classList.add('disabled');
-  } else {
-    prev?.classList.remove('disabled');
+  update(page) {
+    if (page < MIN_PAGE_NUM) {
+      page = MIN_PAGE_NUM;
+    }
+    if (page > MAX_PAGE_NUM) {
+      page = MAX_PAGE_NUM;
+    }
+    this.updatePageParam(page);
+    this.updateDisplay(page);
   }
-  if (page === MAX_PAGE_NUM) {
-    next?.classList.add('disabled');
-  } else {
-    next?.classList.remove('disabled');
+  updatePageParam(newPage) {
+    const url = new URL(window.location.href);
+    url.searchParams.set('page', newPage.toString());
+    window.history.pushState({}, '', url.toString());
+  }
+  updateDisplay(page) {
+    image.src = `${page.toString()}.jpg`;
+    image.alt = page.toString();
+    if (page === MIN_PAGE_NUM) {
+      prevButton?.classList.add('disabled');
+    } else {
+      prevButton?.classList.remove('disabled');
+    }
+    if (page === MAX_PAGE_NUM) {
+      nextButton?.classList.add('disabled');
+    } else {
+      nextButton?.classList.remove('disabled');
+    }
+  }
+  incrementPage() {
+    this.update(this.getPageParam() + 1);
+  }
+  decrementPage() {
+    this.update(this.getPageParam() - 1);
+  }
+  handleKeyDown(event) {
+    if (event.code === 'KeyN') {
+      this.incrementPage();
+    } else if (event.code === 'KeyP') {
+      this.decrementPage();
+    } else if (event.code === 'KeyH' && event.shiftKey) {
+      window.open(_HOME, '_blank', 'noopener,noreferrer')?.focus();
+    } else if (event.code === 'KeyX' && event.shiftKey) {
+      window.open(_CRUM, '_blank', 'noopener,noreferrer')?.focus();
+    }
+  }
+  initEventListeners() {
+    document.addEventListener('keydown', this.handleKeyDown.bind(this));
+    nextButton?.addEventListener('click', this.incrementPage.bind(this));
+    prevButton?.addEventListener('click', this.decrementPage.bind(this));
   }
 }
-function incrementPage() {
-  update(getPageParam() + 1);
-}
-function decrementPage() {
-  update(getPageParam() - 1);
-}
-function handleKeyDown(event) {
-  if (event.code === 'KeyN') {
-    incrementPage();
-  } else if (event.code === 'KeyP') {
-    decrementPage();
-  } else if (event.code === 'KeyH' && event.shiftKey) {
-    window.open(_HOME, '_blank', 'noopener,noreferrer')?.focus();
-  } else if (event.code === 'KeyX' && event.shiftKey) {
-    window.open(_CRUM, '_blank', 'noopener,noreferrer')?.focus();
+class ZoomableImage {
+  constructor() {
+    this.scale = 1;
+    this.startX = 0;
+    this.startY = 0;
+    this.originX = 0;
+    this.originY = 0;
+    this.isDragging = false;
+    this.initEventListeners();
+  }
+  initEventListeners() {
+    document.addEventListener('wheel', this.handleZoom.bind(this), {
+      passive: false,
+    });
+    image.addEventListener('mousedown', this.startDragging.bind(this));
+    document.addEventListener('mousemove', this.dragImage.bind(this));
+    document.addEventListener('mouseup', this.stopDragging.bind(this));
+    resetButton?.addEventListener('click', this.reset.bind(this));
+    document.addEventListener('keydown', this.handleKeyDown.bind(this));
+  }
+  handleZoom(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    const zoomFactor = 0.1;
+    if (e.deltaY < 0) {
+      this.scale += zoomFactor;
+    } else if (e.deltaY > 0 && this.scale > 0.2) {
+      this.scale -= zoomFactor;
+    }
+    this.updateTransform();
+  }
+  startDragging(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.isDragging = true;
+    this.startX = e.clientX - this.originX;
+    this.startY = e.clientY - this.originY;
+    image.style.cursor = 'grabbing';
+  }
+  dragImage(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (!this.isDragging) {
+      return;
+    }
+    this.originX = e.clientX - this.startX;
+    this.originY = e.clientY - this.startY;
+    this.updateTransform();
+  }
+  stopDragging(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.isDragging = false;
+    image.style.cursor = 'grab';
+  }
+  reset() {
+    this.scale = 1;
+    this.originX = 0;
+    this.originY = 0;
+    this.updateTransform();
+  }
+  updateTransform() {
+    image.style.transform = `scale(${this.scale.toString()}) translate(${this.originX.toString()}px, ${this.originY.toString()}px)`;
+  }
+  handleKeyDown(e) {
+    if (e.code === 'KeyR') {
+      this.reset();
+    }
   }
 }
 function dawoudMain() {
-  document.addEventListener('keydown', handleKeyDown);
-  next?.addEventListener('click', incrementPage);
-  prev?.addEventListener('click', decrementPage);
-  update(getPageParam());
+  new Scroller();
+  new ZoomableImage();
 }
 dawoudMain();
