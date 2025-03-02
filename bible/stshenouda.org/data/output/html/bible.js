83c1fdd58653277c05db21c0cb14ba57ca19d622
Pishoy Girgis <pishoybg@gmail.com>
2025-03-02 17:36:38 +0100

    [#349][Bible] Run `make bible test`.

--- bible/stshenouda.org/data/output/html/bible.js
+++ bible/stshenouda.org/data/output/html/bible.js
@@ -1,10 +1,30 @@
 'use strict';
-document.querySelectorAll('.collapse').forEach((collapse) => {
-  collapse.addEventListener('click', function () {
-    // TODO: Remove the dependency on the HTML structure.
-    const collapsible = collapse.nextElementSibling;
-    collapsible.style.maxHeight = collapsible.style.maxHeight
-      ? ''
-      : collapsible.scrollHeight.toString() + 'px';
+function getRelHref(rel) {
+  const linkElement = document.querySelector(`link[rel="${rel}"]`);
+  return linkElement instanceof HTMLLinkElement ? linkElement.href : null;
+}
+function openRelHref(rel) {
+  const href = getRelHref(rel);
+  if (href) {
+    window.open(href, '_self');
+  }
+}
+function BibleMain() {
+  document.querySelectorAll('.collapse').forEach((collapse) => {
+    collapse.addEventListener('click', function () {
+      // TODO: Remove the dependency on the HTML structure.
+      const collapsible = collapse.nextElementSibling;
+      collapsible.style.maxHeight = collapsible.style.maxHeight
+        ? ''
+        : collapsible.scrollHeight.toString() + 'px';
+    });
   });
-});
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'n') {
+      openRelHref('next');
+    } else if (event.key === 'p') {
+      openRelHref('prev');
+    }
+  });
+}
+BibleMain();
