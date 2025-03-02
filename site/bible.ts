0d7a36def201f96f23e9c1e395aed390cdeb71df
Pishoy Girgis <pishoybg@gmail.com>
2025-03-02 17:36:38 +0100

    [#349][Bible] Add Basic Navigation Shortcuts.

--- site/bible.ts
+++ site/bible.ts
@@ -1,11 +1,35 @@
-document
-  .querySelectorAll<HTMLElement>('.collapse')
-  .forEach((collapse: HTMLElement): void => {
-    collapse.addEventListener('click', function () {
-      // TODO: Remove the dependency on the HTML structure.
-      const collapsible = collapse.nextElementSibling! as HTMLElement;
-      collapsible.style.maxHeight = collapsible.style.maxHeight
-        ? ''
-        : collapsible.scrollHeight.toString() + 'px';
+function getRelHref(rel: string): string | null {
+  const linkElement = document.querySelector(`link[rel="${rel}"]`);
+  return linkElement instanceof HTMLLinkElement ? linkElement.href : null;
+}
+
+function openRelHref(rel: string): void {
+  const href = getRelHref(rel);
+  if (href) {
+    window.open(href, '_self');
+  }
+}
+
+function BibleMain() {
+  document
+    .querySelectorAll<HTMLElement>('.collapse')
+    .forEach((collapse: HTMLElement): void => {
+      collapse.addEventListener('click', function () {
+        // TODO: Remove the dependency on the HTML structure.
+        const collapsible = collapse.nextElementSibling! as HTMLElement;
+        collapsible.style.maxHeight = collapsible.style.maxHeight
+          ? ''
+          : collapsible.scrollHeight.toString() + 'px';
+      });
     });
+
+  document.addEventListener('keydown', (event: KeyboardEvent) => {
+    if (event.key === 'n') {
+      openRelHref('next');
+    } else if (event.key === 'p') {
+      openRelHref('prev');
+    }
   });
+}
+
+BibleMain();
