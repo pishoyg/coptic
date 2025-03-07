551a47ccb70ecee64806af1bd3b3d2774cd1af68
Pishoy Girgis <pishoybg@gmail.com>
2025-03-07 12:36:08 +0100

    [#356,#354] Define the docs_structure pre-commit hook.

---
+++ pre-commit/docs_structure.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# In case the structure changes in in an unexpected way, this script will act as
+# a reminder for the developers to update `.env` accordingly, particularly the
+# `findex` / `findexx` helpers.
+
+import fnmatch
+import pathlib
+
+import utils
+
+
+class Pattern:
+    def __init__(self, pattern: str, required: bool = True):
+        self.pattern: str = pattern
+        self.required: bool = required
+
+    def match(self, paths: list[str]) -> tuple[list[str], list[str]]:
+        results: dict[bool, list[str]] = {True: [], False: []}
+        for path in paths:
+            match = fnmatch.fnmatch(path, self.pattern)
+            results[bool(match)].append(path)
+        assert len(results[True]) + len(results[False]) == len(paths)
+        return results[True], results[False]
+
+
+PATTERNS: list[Pattern] = [
+    # Manually-written code files:
+    Pattern("index.html"),
+    Pattern("style.css"),
+    Pattern("crum/index.html"),
+    Pattern("dawoud/index.html"),
+    # Data files:
+    Pattern("img/**"),
+    Pattern("fonts/**"),
+    Pattern("CNAME"),
+    # Auto-generated (lexicon):
+    Pattern("crum/*.html"),
+    Pattern("crum/xooxle.json"),
+    Pattern("crum/crum.js"),
+    Pattern("crum/xooxle.js"),
+    Pattern("crum/crum/*.png"),  # Crum scan.
+    Pattern("crum/explanatory/*-*-*.*"),  # Explanatory images.
+    Pattern("crum/anki/*", required=False),  # Anki is not tracked in Git.
+    # Auto-generated (bible):
+    Pattern("bible/index.html"),
+    Pattern("bible/*.html"),
+    Pattern("bible/bible.js"),
+    Pattern(
+        "bible/epub/*",
+        required=False,
+    ),  # Epub files are not tracked in Git.
+    # Auto-generated (dawoud):
+    Pattern("dawoud/dawoud.js"),
+    Pattern("dawoud/*.jpg"),  # Dawoud scan is a JPG.
+]
+
+
+def main():
+    directory: pathlib.Path = pathlib.Path(utils.SITE_DIR).resolve()
+
+    files: list[str] = [
+        str(f.relative_to(directory))
+        for f in directory.rglob("*")
+        if f.is_file()
+    ]
+
+    for pattern in PATTERNS:
+        matched, files = pattern.match(files)
+        if not pattern.required:
+            continue
+        utils.assass(matched, pattern.pattern, "did not match any files!")
+
+    utils.assass(
+        not files,
+        "The following files were not matched by any pattern:",
+        files,
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ pre-commit/test/test_docs_structure.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
