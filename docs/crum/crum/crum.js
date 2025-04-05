5e6d09a195db3f0bbf71c5145f3aa09b29233d1c
Pishoy Girgis <pishoybg@gmail.com>
2025-04-05 17:49:18 +0200

    [fix #397][Crum/Scan] Publish a Scrollable Version of Crum.

---
+++ docs/crum/crum/crum.js
@@ -0,0 +1,17 @@
+import * as scan from '../../scan.js';
+const MIN_PAGE_NUM = 1;
+const MAX_PAGE_NUM = 973;
+const PNG_RANGES = [
+  [1, 20],
+  [865, 973],
+];
+function ext(page) {
+  return PNG_RANGES.some((range) => page >= range[0] && page <= range[1])
+    ? 'png'
+    : 'jpeg';
+}
+function main() {
+  new scan.Scroller(MIN_PAGE_NUM, MAX_PAGE_NUM, ext);
+  new scan.ZoomerDragger();
+}
+main();
