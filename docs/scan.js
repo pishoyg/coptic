8bb7ff50f373c31983aad871a3dcc893a2690916
Pishoy Girgis <pishoybg@gmail.com>
2025-04-05 17:43:09 +0200

    [#397][Crum/Scan] Take out Dawoud Scan Logic to a Common Package.

---
+++ docs/scan.js
@@ -0,0 +1,146 @@
+// NOTE: We assume the existence of the following elements:
+const image = document.getElementById('scan');
+const nextButton = document.getElementById('next');
+const prevButton = document.getElementById('prev');
+const resetButton = document.getElementById('reset');
+export class Scroller {
+  start;
+  end;
+  ext;
+  constructor(
+    start,
+    end,
+    // TODO: Clean this mess. There is point of making the extension a function
+    // of the page number!
+    ext
+  ) {
+    this.start = start;
+    this.end = end;
+    this.ext = ext;
+    this.initEventListeners();
+    this.update(this.getPageParam());
+  }
+  getPageParam() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const page = urlParams.get('page');
+    return page ? parseInt(page) : this.start;
+  }
+  update(page) {
+    if (page < this.start) {
+      page = this.start;
+    }
+    if (page > this.end) {
+      page = this.end;
+    }
+    this.updatePageParam(page);
+    this.updateDisplay(page);
+    resetButton?.click();
+  }
+  updatePageParam(newPage) {
+    const url = new URL(window.location.href);
+    url.searchParams.set('page', newPage.toString());
+    window.history.replaceState({}, '', url.toString());
+  }
+  updateDisplay(page) {
+    image.src = `${page.toString()}.${typeof this.ext === 'function' ? this.ext(page) : this.ext}`;
+    image.alt = page.toString();
+    if (page === this.start) {
+      prevButton?.classList.add('disabled');
+    } else {
+      prevButton?.classList.remove('disabled');
+    }
+    if (page === this.end) {
+      nextButton?.classList.add('disabled');
+    } else {
+      nextButton?.classList.remove('disabled');
+    }
+  }
+  incrementPage() {
+    this.update(this.getPageParam() + 1);
+  }
+  decrementPage() {
+    this.update(this.getPageParam() - 1);
+  }
+  handleKeyDown(event) {
+    if (event.code === 'KeyN') {
+      this.incrementPage();
+    } else if (event.code === 'KeyP') {
+      this.decrementPage();
+    }
+  }
+  initEventListeners() {
+    document.addEventListener('keydown', this.handleKeyDown.bind(this));
+    nextButton?.addEventListener('click', this.incrementPage.bind(this));
+    prevButton?.addEventListener('click', this.decrementPage.bind(this));
+  }
+}
+export class ZoomerDragger {
+  scale = 1;
+  startX = 0;
+  startY = 0;
+  originX = 0;
+  originY = 0;
+  isDragging = false;
+  constructor() {
+    this.initEventListeners();
+  }
+  initEventListeners() {
+    document.addEventListener('wheel', this.handleZoom.bind(this), {
+      passive: false,
+    });
+    image.addEventListener('mousedown', this.startDragging.bind(this));
+    document.addEventListener('mousemove', this.dragImage.bind(this));
+    document.addEventListener('mouseup', this.stopDragging.bind(this));
+    resetButton?.addEventListener('click', this.reset.bind(this));
+    document.addEventListener('keydown', this.handleKeyDown.bind(this));
+  }
+  handleZoom(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    const zoomFactor = 0.1;
+    if (e.deltaY < 0) {
+      this.scale += zoomFactor;
+    } else if (e.deltaY > 0 && this.scale > 0.2) {
+      this.scale -= zoomFactor;
+    }
+    this.updateTransform();
+  }
+  startDragging(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.isDragging = true;
+    this.startX = e.clientX - this.originX;
+    this.startY = e.clientY - this.originY;
+    image.style.cursor = 'grabbing';
+  }
+  dragImage(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (!this.isDragging) {
+      return;
+    }
+    this.originX = e.clientX - this.startX;
+    this.originY = e.clientY - this.startY;
+    this.updateTransform();
+  }
+  stopDragging(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.isDragging = false;
+    image.style.cursor = 'grab';
+  }
+  reset() {
+    this.scale = 1;
+    this.originX = 0;
+    this.originY = 0;
+    this.updateTransform();
+  }
+  updateTransform() {
+    image.style.transform = `scale(${this.scale.toString()}) translate(${this.originX.toString()}px, ${this.originY.toString()}px)`;
+  }
+  handleKeyDown(e) {
+    if (e.code === 'KeyR') {
+      this.reset();
+    }
+  }
+}
---
+++ docs/scan.ts
@@ -0,0 +1,163 @@
+// NOTE: We assume the existence of the following elements:
+const image = document.getElementById('scan') as HTMLImageElement;
+const nextButton = document.getElementById('next');
+const prevButton = document.getElementById('prev');
+const resetButton = document.getElementById('reset');
+
+export class Scroller {
+  constructor(
+    private readonly start: number,
+    private readonly end: number,
+    // TODO: Clean this mess. There is point of making the extension a function
+    // of the page number!
+    private readonly ext: string | ((page: number) => string)
+  ) {
+    this.initEventListeners();
+    this.update(this.getPageParam());
+  }
+
+  private getPageParam(): number {
+    const urlParams = new URLSearchParams(window.location.search);
+    const page = urlParams.get('page');
+    return page ? parseInt(page) : this.start;
+  }
+
+  private update(page: number): void {
+    if (page < this.start) {
+      page = this.start;
+    }
+    if (page > this.end) {
+      page = this.end;
+    }
+    this.updatePageParam(page);
+    this.updateDisplay(page);
+    resetButton?.click();
+  }
+
+  private updatePageParam(newPage: number): void {
+    const url = new URL(window.location.href);
+    url.searchParams.set('page', newPage.toString());
+    window.history.replaceState({}, '', url.toString());
+  }
+
+  private updateDisplay(page: number): void {
+    image.src = `${page.toString()}.${typeof this.ext === 'function' ? this.ext(page) : this.ext}`;
+    image.alt = page.toString();
+    if (page === this.start) {
+      prevButton?.classList.add('disabled');
+    } else {
+      prevButton?.classList.remove('disabled');
+    }
+    if (page === this.end) {
+      nextButton?.classList.add('disabled');
+    } else {
+      nextButton?.classList.remove('disabled');
+    }
+  }
+
+  private incrementPage(): void {
+    this.update(this.getPageParam() + 1);
+  }
+
+  private decrementPage(): void {
+    this.update(this.getPageParam() - 1);
+  }
+
+  private handleKeyDown(event: KeyboardEvent): void {
+    if (event.code === 'KeyN') {
+      this.incrementPage();
+    } else if (event.code === 'KeyP') {
+      this.decrementPage();
+    }
+  }
+  private initEventListeners(): void {
+    document.addEventListener('keydown', this.handleKeyDown.bind(this));
+    nextButton?.addEventListener('click', this.incrementPage.bind(this));
+    prevButton?.addEventListener('click', this.decrementPage.bind(this));
+  }
+}
+
+export class ZoomerDragger {
+  private scale = 1;
+  private startX = 0;
+  private startY = 0;
+  private originX = 0;
+  private originY = 0;
+  private isDragging = false;
+
+  constructor() {
+    this.initEventListeners();
+  }
+
+  private initEventListeners(): void {
+    document.addEventListener('wheel', this.handleZoom.bind(this), {
+      passive: false,
+    });
+
+    image.addEventListener('mousedown', this.startDragging.bind(this));
+    document.addEventListener('mousemove', this.dragImage.bind(this));
+    document.addEventListener('mouseup', this.stopDragging.bind(this));
+    resetButton?.addEventListener('click', this.reset.bind(this));
+    document.addEventListener('keydown', this.handleKeyDown.bind(this));
+  }
+
+  private handleZoom(e: WheelEvent): void {
+    e.preventDefault();
+    e.stopPropagation();
+    const zoomFactor = 0.1;
+
+    if (e.deltaY < 0) {
+      this.scale += zoomFactor;
+    } else if (e.deltaY > 0 && this.scale > 0.2) {
+      this.scale -= zoomFactor;
+    }
+
+    this.updateTransform();
+  }
+
+  private startDragging(e: MouseEvent): void {
+    e.preventDefault();
+    e.stopPropagation();
+    this.isDragging = true;
+    this.startX = e.clientX - this.originX;
+    this.startY = e.clientY - this.originY;
+    image.style.cursor = 'grabbing';
+  }
+
+  private dragImage(e: MouseEvent): void {
+    e.preventDefault();
+    e.stopPropagation();
+    if (!this.isDragging) {
+      return;
+    }
+
+    this.originX = e.clientX - this.startX;
+    this.originY = e.clientY - this.startY;
+
+    this.updateTransform();
+  }
+
+  private stopDragging(e: MouseEvent): void {
+    e.preventDefault();
+    e.stopPropagation();
+    this.isDragging = false;
+    image.style.cursor = 'grab';
+  }
+
+  private reset(): void {
+    this.scale = 1;
+    this.originX = 0;
+    this.originY = 0;
+    this.updateTransform();
+  }
+
+  private updateTransform(): void {
+    image.style.transform = `scale(${this.scale.toString()}) translate(${this.originX.toString()}px, ${this.originY.toString()}px)`;
+  }
+
+  private handleKeyDown(e: KeyboardEvent): void {
+    if (e.code === 'KeyR') {
+      this.reset();
+    }
+  }
+}
