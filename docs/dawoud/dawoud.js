aa40f12a4576b2cbdd0ad88188689086c77173fa
Pishoy Girgis <pishoybg@gmail.com>
2025-04-06 08:28:01 +0200

    [#405][Dawoud/Search] Robust Search.
    Summary of changes:
    - Fix bug in search. Keep the page structure, instead of flattening the
      columns, which results in the loss of information necessary for the
      search to work correctly. Implement a dictionary-correct binary search
      algorithm.
    - Add comments.
    - Move word sorting logic to a Word class.
    - Implement DawoudWord as a subclass that follows Dawoud's sorting
      conventions.
    - Rename some types and variables.

--- docs/dawoud/dawoud.js
+++ docs/dawoud/dawoud.js
@@ -2,22 +2,107 @@ import * as scan from '../scan.js';
 const MIN_PAGE_NUM = 0;
 const MAX_PAGE_NUM = 1054;
 const OFFSET = 16;
-// TODO: (#405): Use the local TSV once the index is populated.
+// TODO: If this were to be adapted to Crum, keep in mind that he sorts the
+// words in a peculiar way!
+// TODO: (#405): Use the local TSV once the index is fully populated.
 const COPTIC =
   'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-qCcmKVqniHVF6vtmzRoedIqgH96sDWMetp4HMSApUKNCZSqUDi3FnU_tW87yWBH2HPMbjJei9KIL/pub?gid=0&single=true&output=tsv';
-// NOTE: Capital letters have a higher unicode value, and they immediately
-// precede their small counterpart.
+// Coptic exists in the Unicode in two blocks:
+//   https://en.wikipedia.org/wiki/Coptic_(Unicode_block)
+//   https://en.wikipedia.org/wiki/Greek_and_Coptic
+// Thus, to list the letters, we need to iterate over the two ranges separately.
 const COPTIC_LETTERS = [
+  // Capital letters have a higher unicode value, and they immediately
+  // precede their small counterpart.
+  // Thus, to cover the full range of the alphabet, pair[0] should be a
+  // capital letter, and pair[1] should be a small letter.
   ['Ⲁ', 'ⲱ'],
   ['Ϣ', 'ϯ'],
 ];
+// SEARCH_BOX is the ID of the search box element in the page.
 const SEARCH_BOX = 'searchBox';
-class Index {
+// Word represents a Coptic word that is lexicographically comparable to
+// another.
+// The two unicode blocks for the language are swapped (the lexicographically
+// smaller range have higher Unicode values!) We hack around it using this
+// wrapper, to allow you to conveniently compare words lexicographically.
+class Word {
+  static mapping = Word.buildMapping();
+  mapped;
+  word;
+  constructor(word) {
+    // I hope the word consists entirely of Coptic words.
+    this.word = word.toLowerCase();
+    this.mapped = Word.map(this.word);
+  }
+  static isCopticWord(word) {
+    return [...word].every((c) => c in Word.mapping);
+  }
+  leq(other) {
+    return this.mapped <= other.mapped;
+  }
+  static map(word) {
+    return Array.from(word)
+      .map((a) => Word.mapping[a] ?? a)
+      .join();
+  }
+  static buildMapping() {
+    return COPTIC_LETTERS.map((range) => Word.between(range[0], range[1]))
+      .flat()
+      .reduce((acc, letter, index) => {
+        acc[letter] = String.fromCharCode('a'.charCodeAt(0) + index);
+        return acc;
+      }, {});
+  }
+  static between(a, b) {
+    const arr = [];
+    for (
+      let char = a;
+      char <= b;
+      char = String.fromCharCode(char.charCodeAt(0) + 1)
+    ) {
+      arr.push(char);
+    }
+    return arr;
+  }
+}
+// Dawoud gives ⲟⲩ special handling!
+// All words starting with ⲟⲩ are grouped together, under a section in the
+// dictionary between ⲟ and ⲡ.
+// We reimplement sorting for Dawoud!
+class DawoudWord extends Word {
+  leq(other) {
+    if (DawoudWord.ou(this) === DawoudWord.ou(other)) {
+      // Either neither is an ⲟⲩ words, or both are.
+      // Lexicographic comparison should work either way.
+      return super.leq(other);
+    }
+    if (!DawoudWord.o(this) || !DawoudWord.o(other)) {
+      // One of them doesn't start with ⲟ. Again, lexicographic comparison
+      // should work.
+      return super.leq(other);
+    }
+    // Both words start with ⲟ, and only one of them starts with ⲟⲩ.
+    // The ⲟⲩ word is lexicographically larger.
+    return !DawoudWord.ou(this);
+  }
+  static o(w) {
+    return w.word.startsWith('ⲟ');
+  }
+  static ou(w) {
+    return w.word.startsWith('ⲟⲩ');
+  }
+}
+class Dictionary {
   scroller;
-  static mapping = Index.buildMapping();
   index;
   searchBox = document.getElementById(SEARCH_BOX);
-  constructor(index, scroller) {
+  constructor(
+    // index represents the guide words in TSV format.
+    index,
+    // scroller will be used to update the scan image for each query.
+    scroller
+  ) {
     this.scroller = scroller;
     this.index = this.buildIndex(index);
     this.addListeners();
@@ -29,24 +114,27 @@ class Index {
       .split('\n')
       .map((row) => {
         const [page, start, end] = row.split('\t').map((s) => s.trim());
-        return [
-          { word: start, page: parseInt(page) },
-          { word: end, page: parseInt(page) },
-        ];
-      })
-      .flat();
+        return {
+          page: parseInt(page),
+          start: new DawoudWord(start),
+          end: new DawoudWord(end),
+        };
+      });
     // TODO: Remove this block once the index is complete!
     arr.forEach((entry, idx) => {
       if (!idx) {
         return;
       }
-      if (entry.word) {
-        return;
+      if (!entry.start.word) {
+        entry.start = arr[idx - 1].end;
       }
-      arr[idx].word = arr[idx - 1].word;
+      if (!entry.end.word) {
+        entry.end = entry.start;
+      }
     });
     return arr;
   }
+  // Parse the query from the search box, and return a target page.
   getPage() {
     const query = this.searchBox.value.trim().toLowerCase();
     if (!query) {
@@ -58,24 +146,22 @@ class Index {
       return num;
     }
     // Check if this is a Coptic word.
-    if (!Index.isCopticWord(query)) {
+    if (!Word.isCopticWord(query)) {
       return undefined;
     }
+    const target = new DawoudWord(query);
     let left = 0;
     let right = this.index.length - 1;
     while (left < right) {
-      const mid = Math.floor((left + right) / 2) + 1;
+      const mid = Math.floor((left + right) / 2);
       const cur = this.index[mid];
-      if (cur.word === query) {
-        return cur.page;
-      }
-      if (Index.less(query, cur.word)) {
-        right = mid - 1;
+      if (target.leq(cur.end)) {
+        right = mid;
       } else {
-        left = mid;
+        left = mid + 1;
       }
     }
-    return this.index[right].page;
+    return left === right ? this.index[right].page : undefined;
   }
   search() {
     const page = this.getPage();
@@ -107,46 +193,11 @@ class Index {
       event.stopPropagation();
     });
   }
-  static isCopticWord(word) {
-    return [...word].every((c) => c in Index.mapping);
-  }
-  // Compare two Coptic words.
-  static less(a, b) {
-    return Index.map(a) < Index.map(b);
-  }
-  static map(a) {
-    return Array.from(a)
-      .map((a) => Index.mapping[a] ?? a)
-      .join();
-  }
-  static buildMapping() {
-    return COPTIC_LETTERS.map((range) => Index.between(range[0], range[1]))
-      .flat()
-      .reduce((acc, letter, index) => {
-        acc[letter] = String.fromCharCode('a'.charCodeAt(0) + index);
-        return acc;
-      }, {});
-  }
-  static between(a, b) {
-    const arr = [];
-    for (
-      let char = a;
-      char <= b;
-      char = String.fromCharCode(char.charCodeAt(0) + 1)
-    ) {
-      arr.push(char);
-    }
-    return arr;
-  }
-  static *letters() {
-    yield* Index.between('ⲁ', 'ⲱ');
-    yield* Index.between('ϣ', 'ϯ');
-  }
 }
 async function main() {
   const scroller = new scan.Scroller(MIN_PAGE_NUM, MAX_PAGE_NUM, OFFSET, 'jpg');
   const index = await fetch(COPTIC).then((res) => res.text());
   new scan.ZoomerDragger();
-  new Index(index, scroller);
+  new Dictionary(index, scroller);
 }
 await main();
--- docs/dawoud/dawoud.ts
+++ docs/dawoud/dawoud.ts
@@ -2,31 +2,127 @@ import * as scan from '../scan.js';
 const MIN_PAGE_NUM = 0;
 const MAX_PAGE_NUM = 1054;
 const OFFSET = 16;
-// TODO: (#405): Use the local TSV once the index is populated.
+// TODO: If this were to be adapted to Crum, keep in mind that he sorts the
+// words in a peculiar way!
+
+// TODO: (#405): Use the local TSV once the index is fully populated.
 const COPTIC =
   'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-qCcmKVqniHVF6vtmzRoedIqgH96sDWMetp4HMSApUKNCZSqUDi3FnU_tW87yWBH2HPMbjJei9KIL/pub?gid=0&single=true&output=tsv';
-// NOTE: Capital letters have a higher unicode value, and they immediately
-// precede their small counterpart.
+
+// Coptic exists in the Unicode in two blocks:
+//   https://en.wikipedia.org/wiki/Coptic_(Unicode_block)
+//   https://en.wikipedia.org/wiki/Greek_and_Coptic
+// Thus, to list the letters, we need to iterate over the two ranges separately.
 const COPTIC_LETTERS: [string, string][] = [
+  // Capital letters have a higher unicode value, and they immediately
+  // precede their small counterpart.
+  // Thus, to cover the full range of the alphabet, pair[0] should be a
+  // capital letter, and pair[1] should be a small letter.
   ['Ⲁ', 'ⲱ'],
   ['Ϣ', 'ϯ'],
 ];
+
+// SEARCH_BOX is the ID of the search box element in the page.
 const SEARCH_BOX = 'searchBox';
 
-interface Entry {
-  word: string;
+// Word represents a Coptic word that is lexicographically comparable to
+// another.
+// The two unicode blocks for the language are swapped (the lexicographically
+// smaller range have higher Unicode values!) We hack around it using this
+// wrapper, to allow you to conveniently compare words lexicographically.
+class Word {
+  private static readonly mapping: Record<string, string> = Word.buildMapping();
+  private readonly mapped: string;
+  readonly word: string;
+  constructor(word: string) {
+    // I hope the word consists entirely of Coptic words.
+    this.word = word.toLowerCase();
+    this.mapped = Word.map(this.word);
+  }
+
+  static isCopticWord(word: string) {
+    return [...word].every((c) => c in Word.mapping);
+  }
+
+  leq(other: Word): boolean {
+    return this.mapped <= other.mapped;
+  }
+
+  private static map(word: string): string {
+    return Array.from(word)
+      .map((a) => Word.mapping[a] ?? a)
+      .join();
+  }
+
+  private static buildMapping(): Record<string, string> {
+    return COPTIC_LETTERS.map((range) => Word.between(range[0], range[1]))
+      .flat()
+      .reduce<Record<string, string>>((acc, letter, index) => {
+        acc[letter] = String.fromCharCode('a'.charCodeAt(0) + index);
+        return acc;
+      }, {});
+  }
+
+  private static between(a: string, b: string): string[] {
+    const arr: string[] = [];
+    for (
+      let char = a;
+      char <= b;
+      char = String.fromCharCode(char.charCodeAt(0) + 1)
+    ) {
+      arr.push(char);
+    }
+    return arr;
+  }
+}
+
+// Dawoud gives ⲟⲩ special handling!
+// All words starting with ⲟⲩ are grouped together, under a section in the
+// dictionary between ⲟ and ⲡ.
+// We reimplement sorting for Dawoud!
+class DawoudWord extends Word {
+  override leq(other: Word): boolean {
+    if (DawoudWord.ou(this) === DawoudWord.ou(other)) {
+      // Either neither is an ⲟⲩ words, or both are.
+      // Lexicographic comparison should work either way.
+      return super.leq(other);
+    }
+    if (!DawoudWord.o(this) || !DawoudWord.o(other)) {
+      // One of them doesn't start with ⲟ. Again, lexicographic comparison
+      // should work.
+      return super.leq(other);
+    }
+    // Both words start with ⲟ, and only one of them starts with ⲟⲩ.
+    // The ⲟⲩ word is lexicographically larger.
+    return !DawoudWord.ou(this);
+  }
+
+  private static o(w: Word): boolean {
+    return w.word.startsWith('ⲟ');
+  }
+
+  private static ou(w: Word): boolean {
+    return w.word.startsWith('ⲟⲩ');
+  }
+}
+
+// Entry represents a dictionary page, where each page has a defined range,
+// specified by the so-called *guide words*.
+interface Page {
+  start: Word;
+  end: Word;
   page: number;
 }
 
-class Index {
-  private static readonly mapping: Record<string, string> =
-    Index.buildMapping();
-  private readonly index: Entry[];
+class Dictionary {
+  private readonly index: Page[];
   private readonly searchBox = document.getElementById(
     SEARCH_BOX
   ) as HTMLInputElement;
   constructor(
+    // index represents the guide words in TSV format.
     index: string,
+    // scroller will be used to update the scan image for each query.
     private readonly scroller: scan.Scroller
   ) {
     this.index = this.buildIndex(index);
@@ -34,31 +130,35 @@ class Index {
     this.searchBox.focus();
   }
 
-  private buildIndex(index: string): Entry[] {
-    const arr: Entry[] = index
+  private buildIndex(index: string): Page[] {
+    const arr: Page[] = index
       .trim()
       .split('\n')
       .map((row) => {
         const [page, start, end] = row.split('\t').map((s) => s.trim());
-        return [
-          { word: start!, page: parseInt(page!) },
-          { word: end!, page: parseInt(page!) },
-        ];
-      })
-      .flat();
+        return {
+          page: parseInt(page!),
+          start: new DawoudWord(start!),
+          end: new DawoudWord(end!),
+        };
+      });
+
     // TODO: Remove this block once the index is complete!
     arr.forEach((entry, idx) => {
       if (!idx) {
         return;
       }
-      if (entry.word) {
-        return;
+      if (!entry.start.word) {
+        entry.start = arr[idx - 1]!.end;
       }
-      arr[idx]!.word = arr[idx - 1]!.word;
+      if (!entry.end.word) {
+        entry.end = entry.start;
+      }
     });
     return arr;
   }
 
+  // Parse the query from the search box, and return a target page.
   private getPage(): number | undefined {
     const query = this.searchBox.value.trim().toLowerCase();
     if (!query) {
@@ -72,28 +172,24 @@ class Index {
     }
 
     // Check if this is a Coptic word.
-    if (!Index.isCopticWord(query)) {
+    if (!Word.isCopticWord(query)) {
       return undefined;
     }
 
+    const target = new DawoudWord(query);
     let left = 0;
     let right = this.index.length - 1;
-
     while (left < right) {
-      const mid = Math.floor((left + right) / 2) + 1;
+      const mid = Math.floor((left + right) / 2);
       const cur = this.index[mid]!;
-      if (cur.word === query) {
-        return cur.page;
-      }
-
-      if (Index.less(query, cur.word)) {
-        right = mid - 1;
+      if (target.leq(cur.end)) {
+        right = mid;
       } else {
-        left = mid;
+        left = mid + 1;
       }
     }
 
-    return this.index[right]!.page;
+    return left === right ? this.index[right]!.page : undefined;
   }
 
   private search() {
@@ -129,54 +225,13 @@ class Index {
       event.stopPropagation();
     });
   }
-
-  static isCopticWord(word: string) {
-    return [...word].every((c) => c in Index.mapping);
-  }
-
-  // Compare two Coptic words.
-  static less(a: string, b: string): boolean {
-    return Index.map(a) < Index.map(b);
-  }
-
-  static map(a: string): string {
-    return Array.from(a)
-      .map((a) => Index.mapping[a] ?? a)
-      .join();
-  }
-
-  static buildMapping(): Record<string, string> {
-    return COPTIC_LETTERS.map((range) => Index.between(range[0], range[1]))
-      .flat()
-      .reduce<Record<string, string>>((acc, letter, index) => {
-        acc[letter] = String.fromCharCode('a'.charCodeAt(0) + index);
-        return acc;
-      }, {});
-  }
-
-  static between(a: string, b: string): string[] {
-    const arr: string[] = [];
-    for (
-      let char = a;
-      char <= b;
-      char = String.fromCharCode(char.charCodeAt(0) + 1)
-    ) {
-      arr.push(char);
-    }
-    return arr;
-  }
-
-  static *letters(): Generator<string> {
-    yield* Index.between('ⲁ', 'ⲱ');
-    yield* Index.between('ϣ', 'ϯ');
-  }
 }
 
 async function main() {
   const scroller = new scan.Scroller(MIN_PAGE_NUM, MAX_PAGE_NUM, OFFSET, 'jpg');
   const index = await fetch(COPTIC).then((res) => res.text());
   new scan.ZoomerDragger();
-  new Index(index, scroller);
+  new Dictionary(index, scroller);
 }
 
 await main();
