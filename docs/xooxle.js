146df21fa67058cd2fbef7ca7942e03fe046f7d2
Pishoy Girgis <pishoybg@gmail.com>
2025-05-28 09:46:15 +0300

    [#229][Xooxle] Run `make transpile test`.

--- docs/xooxle.js
+++ docs/xooxle.js
@@ -773,20 +773,25 @@ export class Xooxle {
    *
    */
   search() {
-    // Use void to ignore the returned promise.
-    void this.searchAux();
-    this.form.populateParams();
-  }
-  /**
-   *
-   */
-  async searchAux() {
-    if (this.currentAbortController) {
-      this.currentAbortController.abort();
-    }
+    // Abort the previous search. Create a new abort controller for this
+    // trigger.
+    this.currentAbortController?.abort();
     const abortController = new AbortController();
     this.currentAbortController = abortController;
+    // Clear the output fields in the form, since we're starting a new search.
     this.form.clear();
+    // Populate query parameters.
+    this.form.populateParams();
+    // Use void to ignore the returned promise.
+    void this.searchAux(abortController);
+  }
+  /**
+   * @param abortController
+   */
+  async searchAux(abortController) {
+    if (abortController.signal.aborted) {
+      return;
+    }
     const expression = this.form.queryExpression();
     if (!expression) {
       return;
