387f06b527dafc421fbcd41d579b80caee3e277f
Pishoy Girgis <pishoybg@gmail.com>
2025-06-03 11:22:21 +0300

    [#452][Macarius/Stats] Account for the New Project in Stats.

--- stats.py
+++ stats.py
@@ -94,6 +94,7 @@ COLUMNS: dict[str | None, list[str]] = {
         "loc_bible",
         "loc_copticsite",
         "loc_crum",
+        "loc_macarius",
         "loc_flashcards",
         "loc_grammar",
         "loc_kellia",
--- stats.sh
+++ stats.sh
@@ -188,6 +188,7 @@ LOC_ARCHIVE=$(loc_archive)
 LOC=$(( $(loc .) + LOC_ARCHIVE))
 
 LOC_CRUM=$(loc "dictionary/marcion_sourceforge_net")
+LOC_MACARIUS=$(loc "dictionary/stmacariusmonastery_org")
 LOC_COPTICSITE=$(loc "dictionary/copticsite_com")
 LOC_KELLIA=$(loc "dictionary/kellia_uni_goettingen_de")
 LOC_DAWOUD="$(loc "dictionary/copticocc_org")"
@@ -202,6 +203,7 @@ LOC_SHARED=$(loc_shared)
 readonly TOTAL="$((
   LOC_ARCHIVE
   + LOC_CRUM
+  + LOC_MACARIUS
   + LOC_COPTICSITE
   + LOC_KELLIA
   + LOC_DAWOUD
@@ -221,6 +223,7 @@ fi
 
 echo -e "${BLUE}Number of lines of code (including archive): ${GREEN}${LOC}${BLUE}."\
 "\n  ${BLUE}Crum: ${GREEN}${LOC_CRUM}"\
+"\n  ${BLUE}Macarius: ${GREEN}${LOC_MACARIUS}"\
 "\n  ${BLUE}copticsite: ${GREEN}${LOC_COPTICSITE}"\
 "\n  ${BLUE}KELLIA: ${GREEN}${LOC_KELLIA}"\
 "\n  ${BLUE}Dawoud: ${GREEN}${LOC_DAWOUD}"\
@@ -546,7 +549,7 @@ if ${COMMIT}; then
   # We have to exclude the first field (`data`) from this though, because it
   # has spaces within it that would be unintentionally replaced with tabs if we
   # were to include it.
-  echo "$(date)$(echo " $(date +%s) ${LOC} ${CRUM_IMG} ${CRUM_DAWOUD} ${LOC_CRUM} ${LOC_COPTICSITE} ${LOC_KELLIA} ${LOC_BIBLE} ${LOC_FLASHCARDS} ${LOC_GRAMMAR} ${LOC_KEYBOARD} ${LOC_MORPHOLOGY} ${LOC_SITE} ${LOC_SHARED} ${LOC_ARCHIVE} ${CRUM_TYPOS} ${CRUM_IMG_SUM} ${CRUM_DAWOUD_SUM} ${NUM_COMMITS} ${NUM_CONTRIBUTORS} ${CRUM_NOTES} ${LOC_PYTHON} ${LOC_MAKE} ${LOC_CSS} ${LOC_SH} ${LOC_JS} ${LOC_MD} ${LOC_YAML} ${LOC_DOT} ${LOC_KEYBOARD_LAYOUT} ${LOC_TXT} ${CRUM_WRD_TYPOS} ${CRUM_DRV_TYPOS} ${CRUM_PAGES_CHANGED} ${CRUM_ROOT_SENSES} ${CRUM_ROOT_SENSES_SUM} ${LOC_TS} ${LOC_JSON} ${DISK_USAGE} ${DISK_USAGE_HUMAN} ${LOC_TOML} ${FOC} ${FOC_PYTHON} ${FOC_MAKE} ${FOC_CSS} ${FOC_SH} ${FOC_JS} ${FOC_MD} ${FOC_YAML} ${FOC_TOML} ${FOC_DOT} ${FOC_KEYBOARD_LAYOUT} ${FOC_TXT} ${FOC_TS} ${FOC_JSON} ${LOC_HTML} ${FOC_HTML} ${CRUM_LAST_PAGES} ${CRUM_OVERRIDE_TYPES} ${CRUM_SISTERS} ${CRUM_SISTERS_SUM} ${CRUM_ANTONYMS} ${CRUM_ANTONYMS_SUM} ${CRUM_HOMONYMS} ${CRUM_HOMONYMS_SUM} ${CRUM_GREEK_SISTERS} ${CRUM_GREEK_SISTERS_SUM} ${NUM_OPEN_ISSUES} ${NUM_CLOSED_ISSUES} ${CRUM_CATEGORIES} ${CRUM_CATEGORIES_SUM} ${LOC_DAWOUD}" | sed 's/ /\t/g')" \
+  echo "$(date)$(echo " $(date +%s) ${LOC} ${CRUM_IMG} ${CRUM_DAWOUD} ${LOC_CRUM} ${LOC_COPTICSITE} ${LOC_KELLIA} ${LOC_BIBLE} ${LOC_FLASHCARDS} ${LOC_GRAMMAR} ${LOC_KEYBOARD} ${LOC_MORPHOLOGY} ${LOC_SITE} ${LOC_SHARED} ${LOC_ARCHIVE} ${CRUM_TYPOS} ${CRUM_IMG_SUM} ${CRUM_DAWOUD_SUM} ${NUM_COMMITS} ${NUM_CONTRIBUTORS} ${CRUM_NOTES} ${LOC_PYTHON} ${LOC_MAKE} ${LOC_CSS} ${LOC_SH} ${LOC_JS} ${LOC_MD} ${LOC_YAML} ${LOC_DOT} ${LOC_KEYBOARD_LAYOUT} ${LOC_TXT} ${CRUM_WRD_TYPOS} ${CRUM_DRV_TYPOS} ${CRUM_PAGES_CHANGED} ${CRUM_ROOT_SENSES} ${CRUM_ROOT_SENSES_SUM} ${LOC_TS} ${LOC_JSON} ${DISK_USAGE} ${DISK_USAGE_HUMAN} ${LOC_TOML} ${FOC} ${FOC_PYTHON} ${FOC_MAKE} ${FOC_CSS} ${FOC_SH} ${FOC_JS} ${FOC_MD} ${FOC_YAML} ${FOC_TOML} ${FOC_DOT} ${FOC_KEYBOARD_LAYOUT} ${FOC_TXT} ${FOC_TS} ${FOC_JSON} ${LOC_HTML} ${FOC_HTML} ${CRUM_LAST_PAGES} ${CRUM_OVERRIDE_TYPES} ${CRUM_SISTERS} ${CRUM_SISTERS_SUM} ${CRUM_ANTONYMS} ${CRUM_ANTONYMS_SUM} ${CRUM_HOMONYMS} ${CRUM_HOMONYMS_SUM} ${CRUM_GREEK_SISTERS} ${CRUM_GREEK_SISTERS_SUM} ${NUM_OPEN_ISSUES} ${NUM_CLOSED_ISSUES} ${CRUM_CATEGORIES} ${CRUM_CATEGORIES_SUM} ${LOC_DAWOUD} ${LOC_MACARIUS}" | sed 's/ /\t/g')" \
     >> "data/stats.tsv"
   git add "data/stats.tsv"
   git commit --message "${COMMIT_MESSAGE}"
