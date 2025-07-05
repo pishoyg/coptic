3ccb5654056991727e896ad04678178f61c90693
Pishoy Girgis <pishoybg@gmail.com>
2025-07-05 10:02:14 +0200

    [#404][Crum] Update the Crum Scan Script.

--- docs/crum/crum/main.ts
+++ docs/crum/crum/main.ts
@@ -1,27 +1,9 @@
 /** Main function for the Crum scan. */
 import * as scan from '../../scan.js';
 const MIN_PAGE_NUM = 1;
-const MAX_PAGE_NUM = 973;
-const OFFSET = 20;
+const MAX_PAGE_NUM = 975;
+const OFFSET = 22;
 const LANDING = -13; // List of Abbreviations is the default for Crum.
-/* eslint-disable no-magic-numbers */
-const PNG_RANGES: [number, number][] = [
-  [1, 6],
-  [11, 20],
-  [865, 973],
-];
-/* eslint-enable no-magic-numbers */
-
-/**
- *
- * @param page
- * @returns
- */
-function ext(page: number): string {
-  return PNG_RANGES.some((range) => page >= range[0] && page <= range[1])
-    ? 'png'
-    : 'jpeg';
-}
 
 /**
  *
@@ -36,7 +18,7 @@ function main(): void {
     // the moment because the field isn't used.
     document.getElementById('search-box') as HTMLInputElement
   );
-  new scan.Scroller(MIN_PAGE_NUM, MAX_PAGE_NUM, OFFSET, ext, form, LANDING);
+  new scan.Scroller(MIN_PAGE_NUM, MAX_PAGE_NUM, OFFSET, 'png', form, LANDING);
   new scan.ZoomerDragger(form);
 }
 
