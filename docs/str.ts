a37ae8b260aa5c1bd6ec646b3cd3ca90897f4d7b
Pishoy Girgis <pishoybg@gmail.com>
2025-07-12 09:11:52 +0200

    [#466][TypeScript/Utils] Define ` str` Helpers Package.

---
+++ docs/str.ts
@@ -0,0 +1,21 @@
+/**
+ * Check whether the given text is upper-case.
+ * NOTE: If the text doesn't contain any "casable" characters, return true.
+ *
+ * @param s - string to check
+ * @returns - Whether the string is in upper-case.
+ */
+export function isUpper(s: string): boolean {
+  return s.toUpperCase() === s;
+}
+
+/**
+ * Check whether the given text is lower-case.
+ * NOTE: If the text doesn't contain any "casable" characters, return true.
+ *
+ * @param s - string to check
+ * @returns - Whether the string is in lower-case.
+ */
+export function isLower(s: string): boolean {
+  return s.toLowerCase() === s;
+}
