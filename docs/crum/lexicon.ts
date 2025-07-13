pishoyg/coptic

commit e82cf121cbe689bcafb0a205c344968dae40a1d9

[#466][Crum/HTML] Prettify Border Dialects in Xooxle Search Results.

docs/crum/lexicon.ts
@@ -147,6 +147,12 @@ interface Xooxle {
   prepublish?: (row: HTMLTableRowElement) => void;
 }
 
+const dialectCheckboxes: HTMLInputElement[] = Array.from(
+  document.querySelectorAll<HTMLInputElement>(`#${DIALECTS_ID} input`)
+);
+
+const highlighter = new highlight.Highlighter(false, dialectCheckboxes);
+
 const XOOXLES: Xooxle[] = [
   {
     indexURL: 'crum.json',
@@ -154,7 +160,10 @@ const XOOXLES: Xooxle[] = [
     collapsibleID: 'crum-collapsible',
     hrefFmt: paths.CRUM_PAGE_KEY_FMT,
     bucketSorter: new CrumDialectSorter(),
-    prepublish: crum.addGreekLookups,
+    prepublish: (row: HTMLTableRowElement): void => {
+      crum.addGreekLookups(row);
+      crum.handleDialect(row, highlighter);
+    },
   },
   {
     indexURL: 'kellia.json',
@@ -162,6 +171,11 @@ const XOOXLES: Xooxle[] = [
     collapsibleID: 'kellia-collapsible',
     hrefFmt: paths.CDO_LOOKUP_KEY_FMT,
     bucketSorter: new KELLIADialectSorter(),
+    prepublish: (row: HTMLTableRowElement): void => {
+      // TODO: (#0) Add Greek lookups after making your linkifier smart enough
+      // to recognize diacritics.
+      crum.handleDialect(row, highlighter);
+    },
   },
   {
     indexURL: 'copticsite.json',
@@ -240,12 +254,6 @@ async function main(): Promise<void> {
   searchBox.addEventListener('keyup', browser.stopPropagation);
   searchBox.addEventListener('keydown', browser.stopPropagation);
   searchBox.addEventListener('keypress', browser.stopPropagation);
-
-  const dialectCheckboxes: HTMLInputElement[] = Array.from(
-    document.querySelectorAll<HTMLInputElement>(`#${DIALECTS_ID} input`)
-  );
-
-  const highlighter = new highlight.Highlighter(false, dialectCheckboxes);
 
   // Initialize searchers.
   // TODO: (#0) You initialize three different Form objects, and it looks like

commit 56d1591211094815b5b64aee135fda5bd0912970

[#466][Lexicon] Fix Bug: Default Dialects before building Highlighter.
The bug was introduced in e82cf121cbe689bcafb0a205c344968dae40a1d9.

docs/crum/lexicon.ts
@@ -147,13 +147,7 @@ interface Xooxle {
   prepublish?: (row: HTMLTableRowElement) => void;
 }
 
-const dialectCheckboxes: HTMLInputElement[] = Array.from(
-  document.querySelectorAll<HTMLInputElement>(`#${DIALECTS_ID} input`)
-);
-
-const highlighter = new highlight.Highlighter(false, dialectCheckboxes);
-
-const XOOXLES: Xooxle[] = [
+const XOOXLES = (highlighter: highlight.Highlighter): Xooxle[] => [
   {
     indexURL: 'crum.json',
     tableID: 'crum',
@@ -255,6 +249,12 @@ async function main(): Promise<void> {
   searchBox.addEventListener('keydown', browser.stopPropagation);
   searchBox.addEventListener('keypress', browser.stopPropagation);
 
+  const dialectCheckboxes: HTMLInputElement[] = Array.from(
+    document.querySelectorAll<HTMLInputElement>(`#${DIALECTS_ID} input`)
+  );
+
+  const highlighter = new highlight.Highlighter(false, dialectCheckboxes);
+
   // Initialize searchers.
   // TODO: (#0) You initialize three different Form objects, and it looks like
   // each one of them will end up populating the query parameters separately!
@@ -262,7 +262,7 @@ async function main(): Promise<void> {
   // While this is not currently a problem, it remains undesirable.
   // Deduplicate these actions, somehow.
   await Promise.all(
-    XOOXLES.map(async (xoox) => {
+    XOOXLES(highlighter).map(async (xoox) => {
       const raw = await fetch(xoox.indexURL);
       const json = (await raw.json()) as xooxle.Index;
       const form = new xooxle.Form({