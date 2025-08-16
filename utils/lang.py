15d60e6644069d12b25576b2f427814332496981
Pimyn Girgis <bemenboshra2001@gmail.com>
2025-08-16 11:54:11 +0300

    [#452][Crum/Andreas] Write initial parser for Andreas with Coptic and Greek encodings.

--- utils/lang.py
+++ utils/lang.py
@@ -7,5 +7,5 @@ def is_arabic_char(c: str) -> bool:
     return "ARABIC" in unicodedata.name(c, "")
 
 
-def is_greek_char(c: str) -> bool:
+def is_greek_char(c: str) -> bool:  # dead: disable
     return "GREEK" in unicodedata.name(c, "")
