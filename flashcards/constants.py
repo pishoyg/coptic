pishoyg/coptic

commit b80f2575f1e76a432b628df9358f1d29e30fc808

[Utils] Deduplicate Domain References.

flashcards/constants.py
@@ -37,8 +37,7 @@ DAWOUD_DIR = "../dawoud/"  # Relative to the HTML write directory.
 SCAN_DIR = "crum/"  # Relative to the HTML write directory.
 EXPLANATORY_DIR = "explanatory/"  # Relative to the HTML write directory.
 
-EMAIL = "remnqymi@gmail.com"
-DESCRIPTION = "https://remnqymi.com"
+DESCRIPTION = f"https://${paths.DOMAIN}"
 KELLIA_PREFIX = "https://coptic-dictionary.org/entry.cgi?tla="
 DAWOUD_SURNAME = "Dawoud"
 
@@ -456,7 +455,7 @@ class Crum(Decker):
         yield '<td><a class="navigate" href="../">home</a></td>'
         # Contact
         yield "<td>"
-        yield f'<a class="contact" href="mailto:{EMAIL}">'
+        yield f'<a class="contact" href="mailto:{paths.EMAIL}">'
         yield "email"
         yield "</a>"
         yield "</td>"

dictionary/marcion_sourceforge_net/appendices_helper.py
@@ -19,13 +19,7 @@ import gspread
 import pandas as pd
 
 from dictionary.marcion_sourceforge_net import tsv
-from utils import gcloud, log, sane, text
-
-CRUM_FMT = "https://remnqymi.com/crum/{key}.html"
-GSPREAD_URL: str = (
-    # pylint: disable-next=line-too-long
-    "https://docs.google.com/spreadsheets/d/1OVbxt09aCxnbNAt4Kqx70ZmzHGzRO1ZVAa2uJT9duVg"
-)
+from utils import gcloud, log, paths, sane, text
 
 KEY_COL: str = "key"
 SISTERS_COL: str = "sisters"
@@ -776,7 +770,7 @@ class Runner:
                 # This type is of little interest at the moment.
                 continue
             cats: list[str] = []
-            _ = subprocess.run(["open", CRUM_FMT.format(key=key)], check=True)
+            _ = subprocess.run(["open", paths.crum(key)], check=True)
             while True:
                 cats = text.ssplit(
                     input(f"Key = {key}. Categories (empty to skip): "),

dictionary/marcion_sourceforge_net/img_helper.py
@@ -17,7 +17,7 @@ import requests
 from PIL import Image
 
 from dictionary.marcion_sourceforge_net import tsv
-from utils import file, log, sane, semver, text
+from utils import file, log, paths, sane, semver, text
 
 # TODO: (#5) Prevent users from updating an image without updating its source.
 # Somehow!
@@ -39,9 +39,6 @@ IMG_300_DIR = "docs/crum/explanatory/"
 FILE_NAME_RE = re.compile(r"(\d+)-(\d+)-(\d+)\.[^\d]+")
 STEM_RE = re.compile("[0-9]+-[0-9]+-[0-9]+")
 NAME_RE = re.compile("[A-Z][a-zA-Z ]*")
-
-DOMAIN = "remnqymi.com"
-CRUM = f"{DOMAIN}/crum"
 
 KEY_COL: str = "key"
 SENSES_COL: str = "senses"
@@ -121,8 +118,8 @@ QUERIERS_FMT: dict[str, list[str]] = {
 }
 
 WIKI_HEADERS = {
-    "Api-User-Agent": "Coptic/1.0 (https://remnqymi.com; remnqymi@gmail.com)",
-    "User-Agent": "Coptic/1.0 (https://remnqymi.com; remnqymi@gmail.com)",
+    "Api-User-Agent": f"Coptic/1.0 ({paths.URL}; {paths.EMAIL})",
+    "User-Agent": f"Coptic/1.0 ({paths.URL}; {paths.EMAIL})",
 }
 
 argparser = argparse.ArgumentParser(
@@ -262,10 +259,6 @@ _ = argparser.add_argument(
     help="If given, plot a YES or NO for whether each included picture has an"
     " image.",
 )
-
-
-def link(key: str) -> str:
-    return f"https://{CRUM}/{key}.html"
 
 
 def get_max_idx(g: list[str], key: str, sense: str) -> int:
@@ -639,7 +632,7 @@ class Prompter:
         print()
         log.info("Data:")
         log.info("- Key:", self.key)
-        log.info("- Link:", link(self.row[KEY_COL]))
+        log.info("- Link:", paths.crum(self.row[KEY_COL]))
         log.info("- Existing:")
         _pretty(existing(self.key))
         log.info("- Downloads:")
@@ -785,7 +778,7 @@ class Prompter:
             print(self.key, message + colorama.Fore.RESET)
             return True
 
-        os_open(*existing(self.key), link(self.row[KEY_COL]))
+        os_open(*existing(self.key), paths.crum(self.row[KEY_COL]))
 
         while True:
             try:
@@ -849,7 +842,7 @@ class Prompter:
             for key in params:
                 self.key = key
                 self.row = self.key_to_row[self.key]
-                os_open(*existing(self.key), link(self.row[KEY_COL]))
+                os_open(*existing(self.key), paths.crum(self.row[KEY_COL]))
             return True
 
         if command == "convert":

env/paths.sh
@@ -3,12 +3,10 @@
 export SITE_DIR="docs/"
 
 export DOMAIN="remnqymi.com"
-export PORT="8000"
-
+export EMAIL="remnqymi@gmail.com"
 export GITHUB="https://github.com/pishoyg/coptic"
 
 CRUM="https://${DOMAIN}/crum"
-LOC_CRUM="http://localhost:${PORT}/crum"
 
 # crum opens a Crum page on ⲣⲉⲙⲛ̀Ⲭⲏⲙⲓ.
 crum () {
@@ -19,6 +17,10 @@ crum () {
     open "${CRUM}/${KEY}.html"
   done
 }
+
+# PORT is used by the local server.
+export PORT="8000"
+LOC_CRUM="http://localhost:${PORT}/crum"
 
 # loc opens a Crum page on the local server.
 loc () {

utils/paths.py
@@ -3,7 +3,16 @@
 import os
 
 SITE_DIR = os.environ["SITE_DIR"]
+DOMAIN = os.environ["DOMAIN"]
+URL = f"https://{DOMAIN}"
+CRUM = f"{URL}/crum"
+EMAIL = os.environ["EMAIL"]
 JSON_KEYFILE_NAME = "google_cloud_keyfile.json"
+
+
+def crum(key: str | int) -> str:
+    return f"{CRUM}/{key}.html"
+
 
 # Define the shared CSS, and verify its existence in the site directory.
 _SHARED_CSS_BASENAME = "style.css"

commit 4ce291ed6953131529d12779fea96821ea8b0609

[Crum/HTML] Fix Typo.
Typo was introduced in b80f2575f1e76a432b628df9358f1d29e30fc808!

flashcards/constants.py
@@ -32,7 +32,7 @@ DAWOUD_DIR = "../dawoud/"  # Relative to the HTML write directory.
 SCAN_DIR = "crum/"  # Relative to the HTML write directory.
 EXPLANATORY_DIR = "explanatory/"  # Relative to the HTML write directory.
 
-DESCRIPTION = f"https://${paths.DOMAIN}"
+DESCRIPTION = f"https://{paths.DOMAIN}"
 KELLIA_PREFIX = "https://coptic-dictionary.org/entry.cgi?tla="
 DAWOUD_SURNAME = "Dawoud"
 