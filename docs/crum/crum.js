86f7088123006f4f59c9cda4f37777f28d880049
Pishoy Girgis <pishoybg@gmail.com>
2025-08-31 18:01:21 +0300

    [#419][Crum/Wiki] `make transpile test`

--- docs/crum/crum.js
+++ docs/crum/crum.js
@@ -16,6 +16,10 @@ import * as cls from './cls.js';
 import * as ccls from '../cls.js';
 import * as header from '../header.js';
 import * as logger from '../logger.js';
+// TODO: (#419) Include numbered books in Biblical references. The following may
+// be comprehensive: Samuel, Kings, Chronicles, Maccabees, Corinthians,
+// Thessalonians, Timothy, and Peter. (There are several epistles of St. John,
+// but we're not sure if they're ever cited in Crum!)
 const REFERENCE_RE = /(\b[a-zA-Z]+)\s+(\d+)\s+(\d+)\b/g;
 const COPTIC_RE = /[\p{Script=Coptic}\p{Mark}]+/gu;
 const GREEK_RE = /[\p{Script=Greek}\p{Mark}]+/gu;
@@ -34,6 +38,13 @@ const bibleBookPath = await (async () => {
     .forEach((book) => {
       if (!book.crum) {
         return;
+      }
+      if (book.crum in mapping) {
+        logger.fatal(
+          'Book abbreviation',
+          book.crum,
+          'appears several times in the index!'
+        );
       }
       mapping[book.crum] = book.title
         .toLowerCase()
@@ -259,9 +270,14 @@ export function handleSisterView(elem) {
  */
 export function handleDialect(elem, highlighter) {
   elem.querySelectorAll(`.${cls.DIALECT}`).forEach((el) => {
-    el.classList.add(ccls.HOVER_LINK);
     const code = el.innerText;
     el.replaceChildren(...d.DIALECTS[code].prettyCode());
+    if (el.closest(`.${cls.WIKI}`)) {
+      // Dialect highlighting doesn't really work under Wiki, so we disable it
+      // here!
+      return;
+    }
+    el.classList.add(ccls.HOVER_LINK);
     el.addEventListener(
       'click',
       highlighter.toggleDialect.bind(highlighter, code)
