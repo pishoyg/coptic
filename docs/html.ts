afead1c49a87590663793a1caabae87eebbf0aa9
Pishoy Girgis <pishoybg@gmail.com>
2025-09-08 08:25:29 +0300

    [#419][Crum/Wiki] Create a Text Match and Replacement Function.

--- docs/html.ts
+++ docs/html.ts
@@ -38,6 +38,87 @@ export function makeSpanLinkToAnchor(el: Element, target: string): void {
 }
 
 /**
+ * Search the text of all nodes under the given root. For each substring
+ * matching the given regex, use the replace method to construct a replacement,
+ * and insert it into the tree.
+ *
+ * NOTE: We search one node at a time. A string that matches the regex, but
+ * lives over two neighboring nodes, won't yield a match!
+ *
+ * @param root - Root of the tree to process.
+ * @param regex - Regex to search for in the text nodes of the tree.
+ * @param replace - A method to construct a fragment from a regex match
+ * obtained with the regex above.
+ * @param excludeClosestQuery - An optional query specifying if any subtrees of
+ * the given root should be excluded.
+ */
+export function replaceText(
+  root: Node,
+  regex: RegExp,
+  replace: (match: RegExpExecArray) => (Node | string)[],
+  excludeClosestQuery?: string
+): void {
+  const walker: TreeWalker = document.createTreeWalker(
+    root,
+    NodeFilter.SHOW_TEXT,
+    (node: Node): number => {
+      if (!node.nodeValue || !regex.test(node.nodeValue)) {
+        // This node doesn't contain a matching text.
+        return NodeFilter.FILTER_REJECT;
+      }
+      if (
+        excludeClosestQuery &&
+        node.parentElement?.closest(excludeClosestQuery)
+      ) {
+        // This node is excluded.
+        return NodeFilter.FILTER_REJECT;
+      }
+      return NodeFilter.FILTER_ACCEPT;
+    }
+  );
+
+  // We can't replace nodes on the fly, as this could corrupt the walker.
+  // Instead, we store all nodes that need replacement, and then process them
+  // afterwards.
+  const nodes: Text[] = [];
+  while (walker.nextNode()) {
+    nodes.push(walker.currentNode as Text);
+  }
+
+  nodes.forEach((node: Text): void => {
+    const text: string = node.nodeValue!;
+    regex.lastIndex = 0;
+
+    let lastIndex = 0;
+    const fragment: DocumentFragment = document.createDocumentFragment();
+
+    for (let match: RegExpExecArray | null; (match = regex.exec(text)); ) {
+      // preceding plain text
+      if (match.index > lastIndex) {
+        fragment.appendChild(
+          document.createTextNode(text.slice(lastIndex, match.index))
+        );
+      }
+
+      fragment.append(...replace(match));
+      lastIndex = match.index + match[0].length;
+    }
+
+    if (lastIndex < text.length) {
+      fragment.append(text.slice(lastIndex));
+    }
+    node.replaceWith(fragment);
+  });
+}
+
+/**
+ * Search for all pieces of text under the given root that match the given
+ * regex. For each match, use the provided method to construct a URL, and insert
+ * a link in that piece of text.
+ * Add the given list of classes to the link.
+ * You can excluded certain subtrees of the given tree by providing a list of
+ * classes which should be excluded. This excludes entire subtrees, not just
+ * individual elements.
  *
  * @param root
  * @param regex
@@ -52,90 +133,25 @@ export function linkifyText(
   classes: string[],
   excludedClasses: string[] = []
 ): void {
-  const walker: TreeWalker = document.createTreeWalker(
+  replaceText(
     root,
-    NodeFilter.SHOW_TEXT,
-    (node: Node): number => {
-      if (!node.nodeValue || !regex.test(node.nodeValue)) {
-        // This node doesn't contain a matching text.
-        return NodeFilter.FILTER_REJECT;
+    regex,
+    (match: RegExpExecArray): (Node | string)[] => {
+      const targetUrl = url(match);
+      if (!targetUrl) {
+        // This text doesn't have a URL. Return the original text.
+        return [match[0]];
       }
-      if (
-        excludedClasses.length &&
-        node.parentElement?.closest(css.classQuery(...excludedClasses))
-      ) {
-        // This node is excluded.
-        return NodeFilter.FILTER_REJECT;
-      }
-      if (node.parentElement?.closest('a')) {
-        // This node is already a link.
-        return NodeFilter.FILTER_REJECT;
-      }
-      return NodeFilter.FILTER_ACCEPT;
-    }
-  );
 
-  // We can't replace nodes on the fly, as this could corrupt the walker.
-  // Instead, we store all nodes that need replacement, and then process them
-  // afterwards.
-  const nodes: Text[] = [];
-  while (walker.nextNode()) {
-    nodes.push(walker.currentNode as Text);
-  }
-  nodes.forEach((node: Text): void => {
-    linkifyTextAux(node, regex, url, classes);
-  });
-}
-
-/**
- *
- * @param node
- * @param regex
- * @param url
- * @param classes
- */
-function linkifyTextAux(
-  node: Text,
-  regex: RegExp,
-  url: (match: RegExpExecArray) => string | null,
-  classes: string[]
-): void {
-  const text: string = node.nodeValue!;
-  regex.lastIndex = 0;
-
-  let lastIndex = 0;
-  const fragment: DocumentFragment = document.createDocumentFragment();
-
-  for (let match: RegExpExecArray | null; (match = regex.exec(text)); ) {
-    const query: string = match[0];
-
-    // preceding plain text
-    if (match.index > lastIndex) {
-      fragment.appendChild(
-        document.createTextNode(text.slice(lastIndex, match.index))
-      );
-    }
-
-    const targetUrl = url(match);
-
-    if (targetUrl) {
+      // Create a link.
       const link = document.createElement('span');
       link.classList.add(...classes);
-      link.textContent = query;
+      link.textContent = match[0];
       link.addEventListener('click', () => {
         browser.open(targetUrl);
       });
-      fragment.appendChild(link);
-    } else {
-      // keep unmatched text as-is
-      fragment.appendChild(document.createTextNode(query));
-    }
-
-    lastIndex = match.index + query.length;
-  }
-
-  if (lastIndex < text.length) {
-    fragment.appendChild(document.createTextNode(text.slice(lastIndex)));
-  }
-  node.replaceWith(fragment);
+      return [link];
+    },
+    css.classQuery(...excludedClasses)
+  );
 }
