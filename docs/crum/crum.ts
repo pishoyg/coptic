a0ac5cfa893bd173037b962ca775ddd08b806c62
Pishoy Girgis <pishoybg@gmail.com>
2025-09-12 07:40:03 +0300

    [#419][Crum/Wiki] Log Warnings for potentially missing References.

--- docs/crum/crum.ts
+++ docs/crum/crum.ts
@@ -77,6 +77,7 @@ export function handleAll(
   handleWikiBible(root);
   handleWikiAnnotations(root);
   handleWikiReferences(root);
+  warnPotentiallyMissingReferences(root);
 }
 
 /**
@@ -600,5 +601,66 @@ export function handleWikiReferences(root: HTMLElement): void {
         )
       );
     });
+  });
+}
+
+/**
+ * Log warnings for all capital letters in the Wiki text that haven't been
+ * marked.
+ * In Crum's text, Capital letters are mainly used for abbreviations of
+ * dialects, and biblical and non-biblical references—all of which we try to
+ * detect. An unmarked text containing a capital letter may therefore be an
+ * abbreviation that we failed to parse.
+ * This method yields a lot of false positives, but we retain it in the meantime
+ * while we sharpen our parser.
+ *
+ * TODO: (#419) Delete this function once your logic is more mature.
+ *
+ * @param root
+ */
+export function warnPotentiallyMissingReferences(root: HTMLElement): void {
+  const query: string = css.classQuery(
+    cls.BULLET,
+    cls.ANNOTATION,
+    cls.DIALECT,
+    cls.REFERENCE,
+    cls.BIBLE
+  );
+
+  root.querySelectorAll(`.${cls.WIKI}`).forEach((elem): void => {
+    const walker: TreeWalker = document.createTreeWalker(
+      elem,
+      NodeFilter.SHOW_TEXT,
+      (node: Node): number => {
+        if (!node.nodeValue) {
+          return NodeFilter.FILTER_REJECT;
+        }
+        if (node.parentElement?.closest(query)) {
+          // This node is excluded.
+          return NodeFilter.FILTER_REJECT;
+        }
+        if (!/[A-Z]/gu.test(node.nodeValue)) {
+          return NodeFilter.FILTER_REJECT;
+        }
+        return NodeFilter.FILTER_ACCEPT;
+      }
+    );
+
+    while (walker.nextNode()) {
+      const text: Text = walker.currentNode as Text;
+      // Find all words containing an upper-case letter.
+      const words: string[] =
+        text.nodeValue?.match(/(?=\p{L}*\p{Lu})[\p{L}\p{M}]+/gu) ?? [];
+      logger.warn(
+        'Possibly unmarked abbreviations:',
+        ...words
+          // Insert a comma after each word.
+          .flatMap((entry: string, index: number): string[] =>
+            index < words.length - 1 ? [entry, ','] : [entry]
+          ),
+        'in',
+        text.nodeValue
+      );
+    }
   });
 }
