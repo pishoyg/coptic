3c35e7d21658a7eb37ba68f5df3081a1bba3f567
Pishoy Girgis <pishoybg@gmail.com>
2025-09-13 09:34:02 +0300

    [#419][Crum/Wiki] Fix Typo. Some Bible Books have `4` parts.
    Samuel I and II and Kings I and II are actually referred to as Kings I,
    II, III, and IV in Crum's text.

--- docs/crum/crum.ts
+++ docs/crum/crum.ts
@@ -25,7 +25,7 @@ import * as ref from './references.js';
 import * as drop from '../dropdown.js';
 import * as orth from '../orth.js';
 
-const BIBLE_RE = /(\b(?:[123]\s)?[a-zA-Z]+)(?:\s+(\d+))(?:\s+(\d+))?\b/gu;
+const BIBLE_RE = /(\b(?:[1-4]\s)?[a-zA-Z]+)(?:\s+(\d+))(?:\s+(\d+))?\b/gu;
 const TWO_WORD_ANNOTATION_RE = /\b[a-zA-Z]+\s+[a-zA-Z]+\b/gu;
 const ONE_WORD_ANNOTATION_RE = /\b[a-zA-Z]+\b/gu;
 
