4945270de6e03d0db104a2ec7cff09783e4b12e4
Pishoy Girgis <pishoybg@gmail.com>
2025-09-14 14:15:02 +0200

    [#524][Bible/Site] Define a `book` Parameter.

--- docs/bible/main.ts
+++ docs/bible/main.ts
@@ -1,7 +1,14 @@
-/** Main function for a Bible chapter. */
+/** Main function for a Bible chapter.
+ *
+ * TODO: (#0) This file is used for both `index.html`, as well as individual
+ * chapter files. Split i!
+ * */
 
 import * as collapse from '../collapse.js';
 import * as browser from '../browser.js';
+import * as logger from '../logger.js';
+
+const BOOK_PARAM = 'book';
 
 /**
  * Add Bible event listeners.
@@ -25,6 +32,25 @@ function addEventListeners(): void {
 }
 
 /**
+ * If the book query parameter is present, click on the title of the given
+ * book to expand its content, and scroll to it.
+ */
+function maybeGoToBook(): void {
+  const url: URL = new URL(window.location.href);
+  const click: string | null = url.searchParams.get(BOOK_PARAM);
+  if (!click) {
+    return;
+  }
+  const elem = document.getElementById(click);
+  if (!elem) {
+    logger.error(click, 'not found!');
+    return;
+  }
+  elem.click();
+  elem.scrollIntoView({ behavior: 'smooth' });
+}
+
+/**
  *
  */
 function main(): void {
@@ -33,6 +59,7 @@ function main(): void {
   document.body.normalize();
   collapse.addEventListenersForSiblings();
   addEventListeners();
+  maybeGoToBook();
 }
 
 main();
