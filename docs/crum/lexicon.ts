2074d2cba4d7503d78b001933f3e710130f387f4
Pishoy Girgis <pishoybg@gmail.com>
2025-09-14 11:54:50 +0200

    [#419][Crum/Wiki] Wiki Hanlders live in a Separate File.
    Side change:
    - Add a TODO.

--- docs/crum/lexicon.ts
+++ docs/crum/lexicon.ts
@@ -8,6 +8,7 @@ import * as help from './help.js';
 import * as header from '../header.js';
 import * as paths from '../paths.js';
 import * as crum from './crum.js';
+import * as wiki from './wiki.js';
 import * as dropdown from '../dropdown.js';
 import * as logger from '../logger.js';
 import * as id from './id.js';
@@ -197,7 +198,8 @@ class WikiSearchResult extends xooxle.SearchResult {
   override row(total: number): HTMLTableRowElement {
     const row: HTMLTableRowElement = super.row(total);
     crum.addGreekLookups(row);
-    crum.handleWikiBible(row);
+    // TODO: (#541) Add other handlers once the post-processing bug is fixed.
+    wiki.handleWikiBible(row);
     return row;
   }
 
