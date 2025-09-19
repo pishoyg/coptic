a567e9c70e1e9cc7914552c505c610d26c9b5c87
Pishoy Girgis <pishoybg@gmail.com>
2025-09-19 08:47:19 +0200

    [#419][Crum/Wiki/References] Rewrite Check as a Unit Test.

--- docs/crum/references.test.ts
+++ docs/crum/references.test.ts
@@ -1,23 +1,18 @@
 import * as ref from './references.js';
-import * as logger from '../logger.js';
 import * as orth from '../orth.js';
 
-// Ensure that all keys are normalized.
-const unnormalized: string[] = Object.keys(ref.MAPPING).filter(
-  (key: string): boolean => orth.normalize(key) !== key
-);
-if (unnormalized.length) {
-  logger.fatal(unnormalized, 'are not normalized!');
-}
-
-// Prevent duplicate hyperlinks in lists.
-Object.values(ref.MAPPING).forEach((source: ref.Source): void => {
-  if (!Array.isArray(source.hyperlink)) {
-    return;
-  }
-  logger.ensure(
-    new Set<string>(source.hyperlink).size === source.hyperlink.length,
-    source.title,
-    'has duplicate hyperlinks'
+test('Ensure all keys are normalized', () => {
+  const unnormalized: string[] = Object.keys(ref.MAPPING).filter(
+    (key: string): boolean => orth.normalize(key) !== key
   );
+  expect(unnormalized).toEqual([]);
+});
+
+describe('Prevent duplicate hyperlinks', () => {
+  Object.values(ref.MAPPING).forEach((source: ref.Source) => {
+    if (!Array.isArray(source.hyperlink)) {
+      return;
+    }
+    expect(new Set(source.hyperlink).size).toBe(source.hyperlink.length);
+  });
 });
