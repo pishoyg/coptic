69c37a8fd8a942d685651ac96f5d59efc066762f
Pishoy Girgis <pishoybg@gmail.com>
2025-09-19 08:15:11 +0200

    [#194][Crum/Wiki/Annotations] Delete Unused Field.

--- docs/crum/annotations.ts
+++ docs/crum/annotations.ts
@@ -1,16 +1,9 @@
 import * as str from '../str.js';
-type Category =
-  | 'Linguistic' // A language abbreviation.
-  | 'Grammatical' // A grammatical abbreviation.
-  | 'Instructional' // A general instructional abbreviation.
-  | 'Coptic'; // A Coptic abbreviation.
 
 export interface Annotation {
   // fullForm defines the full-form of the abbreviation, which is to be
   // presented to the user.
   fullForm: string;
-  // category is, as of the time of writing, unused.
-  category: Category;
   // noCaseVariant indicates that the first letter of this annotation retains
   // the case given below (either lower or upper), and never changes case.
   // If set to true, no case variants will be produced or processed for this
@@ -39,123 +32,89 @@ export interface Annotation {
   noCaseVariant?: boolean;
 }
 
-// NOTE: We exclude parentheses, although Crum had them in his list. They have
-// different meaning based on whether they occur in the headings or elsewhere in
-// the text, which is hard to discern by the parser.
+// NOTE:
+// Crum also had the following entry in his list of abbreviations:
+//     ( ) = Coptic letter inserted by editor, except in headings, where they
+//     indicate variants or hypothetical forms.
+// However, parentheses are not reflect in our list of annotations, due to the
+// fact that they have different meaning based on whether they occur in the
+// headings or elsewhere in the text, which is hard to discern by the parser. It
+// would otherwise be confusing to show users the full definition.
 export const MAPPING: Record<string, Annotation> = {
-  acc: { fullForm: 'accusative', category: 'Grammatical' },
-  adj: { fullForm: 'adjective', category: 'Grammatical' },
-  ar: { fullForm: 'Arabic', category: 'Linguistic' },
-  art: { fullForm: 'article', category: 'Grammatical' },
-  c: {
-    fullForm: 'constructed with (of verbs)',
-    category: 'Grammatical',
-    noCaseVariant: true,
-  },
-  caus: { fullForm: 'causative verb', category: 'Grammatical' },
-  cit: {
-    fullForm: 'cited, quoted in following place',
-    category: 'Instructional',
-  },
-  dem: { fullForm: 'demotic', category: 'Linguistic' },
-  diff: {
-    fullForm: 'different reading, not useful for comparison',
-    category: 'Instructional',
-  },
+  acc: { fullForm: 'accusative' },
+  adj: { fullForm: 'adjective' },
+  ar: { fullForm: 'Arabic' },
+  art: { fullForm: 'article' },
+  c: { fullForm: 'constructed with (of verbs)', noCaseVariant: true },
+  caus: { fullForm: 'causative verb' },
+  cit: { fullForm: 'cited, quoted in following place' },
+  dem: { fullForm: 'demotic' },
+  diff: { fullForm: 'different reading, not useful for comparison' },
   do: {
     fullForm: 'ditto, same as last word cited in this dialect',
-    category: 'Instructional',
     noCaseVariant: true,
   },
-  esp: { fullForm: 'especially', category: 'Instructional' },
-  f: { fullForm: 'feminine', category: 'Grammatical', noCaseVariant: true },
-  gen: { fullForm: 'genitive', category: 'Grammatical' },
-  Gk: { fullForm: 'Greek', category: 'Linguistic', noCaseVariant: true },
-  inf: { fullForm: 'infra', category: 'Instructional' },
-  interj: { fullForm: 'interjection', category: 'Grammatical' },
-  interrog: { fullForm: 'interrogative', category: 'Grammatical' },
+  esp: { fullForm: 'especially' },
+  f: { fullForm: 'feminine', noCaseVariant: true },
+  gen: { fullForm: 'genitive' },
+  Gk: { fullForm: 'Greek', noCaseVariant: true },
+  inf: { fullForm: 'infra' },
+  interj: { fullForm: 'interjection' },
+  interrog: { fullForm: 'interrogative' },
   intr: {
     fullForm:
       'intransitive (i.e. verb without immediate object, or one constructed with prep. ⲉ-)',
-    category: 'Grammatical',
   },
-  l: { fullForm: 'legendum', category: 'Instructional' },
-  lit: { fullForm: 'literally', category: 'Instructional' },
-  m: { fullForm: 'masculine', category: 'Grammatical', noCaseVariant: true },
-  nn: { fullForm: 'noun', category: 'Grammatical', noCaseVariant: true },
-  obj: { fullForm: 'object', category: 'Grammatical' },
-  om: { fullForm: 'omits, omitted', category: 'Instructional' },
-  opp: {
-    fullForm: 'as opposed to, contrasted with',
-    category: 'Instructional',
-  },
-  Ostr: { fullForm: 'ostracon', category: 'Instructional' },
-  paral: { fullForm: 'parallel word or phrase', category: 'Instructional' },
-  pass: { fullForm: 'passim', category: 'Instructional' },
-  'p c': {
-    fullForm: 'conjunctive participle',
-    category: 'Grammatical',
-  },
-  pl: { fullForm: 'plural', category: 'Grammatical' },
-  poss: { fullForm: 'possessive pronoun', category: 'Grammatical' },
-  pref: { fullForm: 'prefix', category: 'Grammatical' },
-  prep: { fullForm: 'preposition', category: 'Grammatical' },
-  pres: {
-    fullForm: 'present tense, thus: 1 pres, 2 pres',
-    category: 'Grammatical',
-  },
-  prob: { fullForm: 'probably', category: 'Instructional' },
-  pron: { fullForm: 'pronoun', category: 'Grammatical' },
-  qual: {
-    fullForm: 'qualitative of verb; also indicated by †',
-    category: 'Grammatical',
-  },
-  refl: { fullForm: 'reflexive use', category: 'Grammatical' },
-  rel: { fullForm: 'relative', category: 'Grammatical' },
-  's f': { fullForm: 'sub fine', category: 'Instructional' },
-  sg: { fullForm: 'singular', category: 'Grammatical' },
-  sim: {
-    fullForm: 'similar in use or in meaning to the last quoted instance',
-    category: 'Instructional',
-  },
-  suff: { fullForm: 'suffix', category: 'Grammatical' },
-  tr: { fullForm: 'transitive', category: 'Grammatical' },
-  V: { fullForm: 'vide', category: 'Instructional' },
-  var: { fullForm: 'variant, in same dialect', category: 'Instructional' },
-  vb: { fullForm: 'verb', category: 'Grammatical' },
-  '†': { fullForm: 'qualitative', category: 'Instructional' },
-  '?': { fullForm: 'perhaps, possibly', category: 'Instructional' },
+  l: { fullForm: 'legendum' },
+  lit: { fullForm: 'literally' },
+  m: { fullForm: 'masculine', noCaseVariant: true },
+  nn: { fullForm: 'noun', noCaseVariant: true },
+  obj: { fullForm: 'object' },
+  om: { fullForm: 'omits, omitted' },
+  opp: { fullForm: 'as opposed to, contrasted with' },
+  Ostr: { fullForm: 'ostracon' },
+  paral: { fullForm: 'parallel word or phrase' },
+  pass: { fullForm: 'passim' },
+  'p c': { fullForm: 'conjunctive participle' },
+  pl: { fullForm: 'plural' },
+  poss: { fullForm: 'possessive pronoun' },
+  pref: { fullForm: 'prefix' },
+  prep: { fullForm: 'preposition' },
+  pres: { fullForm: 'present tense, thus: 1 pres, 2 pres' },
+  prob: { fullForm: 'probably' },
+  pron: { fullForm: 'pronoun' },
+  qual: { fullForm: 'qualitative of verb; also indicated by †' },
+  refl: { fullForm: 'reflexive use' },
+  rel: { fullForm: 'relative' },
+  's f': { fullForm: 'sub fine' },
+  sg: { fullForm: 'singular' },
+  sim: { fullForm: 'similar in use or in meaning to the last quoted instance' },
+  suff: { fullForm: 'suffix' },
+  tr: { fullForm: 'transitive' },
+  V: { fullForm: 'vide' },
+  var: { fullForm: 'variant, in same dialect' },
+  vb: { fullForm: 'verb' },
+  '†': { fullForm: 'qualitative' },
+  '?': { fullForm: 'perhaps, possibly' },
   // The following is somewhat unnecessary, but we include it for completion.
   // Crum had it in his list!
-  ⲛ̅ⲉ̅: { fullForm: 'ⲛⲟⲩⲧⲉ', category: 'Coptic', noCaseVariant: true },
+  ⲛ̅ⲉ̅: { fullForm: 'ⲛⲟⲩⲧⲉ', noCaseVariant: true },
 };
 
 // The following abbreviations are not listed in Crum's List of Abbreviations,
 // but they are nice to have.
 // This list may grow as we discover more abbreviations that are worth tooltips.
-MAPPING['MS'] = {
-  fullForm: 'manuscript',
-  category: 'Instructional',
-  noCaseVariant: true,
-};
-MAPPING['MSS'] = {
-  fullForm: 'manuscripts',
-  category: 'Instructional',
-  noCaseVariant: true,
-};
-MAPPING['cf'] = { fullForm: 'confer', category: 'Instructional' };
-MAPPING['q v'] = { fullForm: 'quod vide', category: 'Instructional' };
-MAPPING['s v'] = { fullForm: 'sub verbo', category: 'Instructional' };
-MAPPING['sc'] = { fullForm: 'scilicet', category: 'Instructional' };
-MAPPING['l c'] = { fullForm: 'loco citato', category: 'Instructional' };
-MAPPING['Heb'] = {
-  fullForm: 'Hebrew',
-  category: 'Linguistic',
-  noCaseVariant: true,
-};
+MAPPING['MS'] = { fullForm: 'manuscript', noCaseVariant: true };
+MAPPING['MSS'] = { fullForm: 'manuscripts', noCaseVariant: true };
+MAPPING['cf'] = { fullForm: 'confer' };
+MAPPING['q v'] = { fullForm: 'quod vide' };
+MAPPING['s v'] = { fullForm: 'sub verbo' };
+MAPPING['sc'] = { fullForm: 'scilicet' };
+MAPPING['l c'] = { fullForm: 'loco citato' };
+MAPPING['Heb'] = { fullForm: 'Hebrew', noCaseVariant: true };
 
 // TODO: (#511) Reconsider whether you want to retain this tip.
-MAPPING['ib'] = { fullForm: 'ibidem', category: 'Instructional' };
+MAPPING['ib'] = { fullForm: 'ibidem' };
 
 Object.entries(MAPPING).forEach(
   ([key, annotation]: [string, Annotation]): void => {
