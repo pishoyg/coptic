8dc84ca24beac408db03779d50e2e9e5cb9645ef
Pishoy Girgis <pishoybg@gmail.com>
2025-10-03 07:59:38 +0200

    [#557,#419][Crum/Wiki] Rewrite the Test to Support Multiple Files.

--- test/wiki.ts
+++ test/wiki.ts
@@ -1,42 +1,58 @@
 /**
- * Ensure website pages load correctly.
+ * Exercise wiki handlers.
  */
-
 import * as play from '@playwright/test';
-import * as log from '../docs/logger.js';
 import * as cls from '../docs/crum/cls.js';
 import * as drop from '../docs/dropdown.js';
 
-// TODO: (#419) Add more test cases. 88 doesn't cover everything!
-// TODO: (#522) The numbers below should increase when references not
-// explicitly mentioned in Crum's list of abbreviations are supported.
-// TODO: (#523) The numbers below should increase when two-part
-// abbreviations are supported.
-play.test(
-  'Inserts hyperlinks for Wiki References',
-  async ({ page }: { page: play.Page }): Promise<void> => {
-    const path = '/crum/88.html';
-    await page.goto(path, { waitUntil: 'networkidle' });
-    for (const testCase of [
-      { query: `.${cls.WIKI} .${cls.REFERENCE}`, want: 139 },
-      { query: `.${cls.WIKI} .${cls.SUFFIX}`, want: 133 },
-      { query: `.${cls.WIKI} .${cls.BIBLE}`, want: 119 },
-      {
-        query: `.${cls.WIKI} .${cls.DIALECT} .${drop.CLS.DROPPABLE}`,
-        want: 383,
-      },
-      { query: `.${cls.WIKI} .${cls.ANNOTATION}`, want: 79 },
-    ]) {
-      const got: number = await page.locator(testCase.query).count();
-      log.ensure(
-        got === testCase.want,
-        'want',
-        testCase.want,
-        'for query',
-        testCase.query,
-        'got',
-        got
-      );
-    }
+// TODO: (#563) Add a test case for 1144.html.
+// TODO: (#419) Add more test cases. This doesn't suffice.
+const TEST_CASES: {
+  path: string;
+  want: {
+    references: number;
+    suffixes: number;
+    bible: number;
+    dialectDropdowns: number;
+    annotations: number;
+  };
+}[] = [
+  {
+    path: '88.html',
+    want: {
+      references: 139,
+      suffixes: 133,
+      bible: 119,
+      dialectDropdowns: 383,
+      annotations: 79,
+    },
+  },
+];
+
+// A map of test keys to their corresponding CSS selectors.
+const QUERIES: Record<string, string> = {
+  references: `.${cls.WIKI} .${cls.REFERENCE}`,
+  suffixes: `.${cls.WIKI} .${cls.SUFFIX}`,
+  bible: `.${cls.WIKI} .${cls.BIBLE}`,
+  dialectDropdowns: `.${cls.WIKI} .${cls.DIALECT} .${drop.CLS.DROPPABLE}`,
+  annotations: `.${cls.WIKI} .${cls.ANNOTATION}`,
+};
+
+play.test.describe('Wiki Reference Handlers', () => {
+  for (const testCase of TEST_CASES) {
+    play.test(
+      `Inserts the correct number of objects on ${testCase.path}`,
+      async ({ page }: { page: play.Page }): Promise<void> => {
+        await page.goto(`/crum/${testCase.path}`, {
+          waitUntil: 'networkidle',
+        });
+
+        await Promise.all(
+          Object.entries(testCase.want).map(([key, want]: [string, number]) =>
+            play.expect(page.locator(QUERIES[key])).toHaveCount(want)
+          )
+        );
+      }
+    );
   }
-);
+});
