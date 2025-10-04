pishoyg/coptic

commit 206e9c73a351a64357e6f9cd518c8c2157a256e9

[#490][TypeScript] Code quality!

docs/crum/crum.ts
@@ -70,7 +70,7 @@ export function handleCategories(root: HTMLElement): void {
         a.classList.add(ccls.HOVER_LINK);
         a.target = '_blank';
         a.textContent = cat;
-        a.href = `${paths.LEXICON}/${cat}.html`;
+        a.href = paths.crum(cat);
         return index === cats.length - 1 ? [a] : [a, ', '];
       })
     );
@@ -86,7 +86,7 @@ export function handleRootType(root: HTMLElement): void {
     const type: string = el.textContent.trim();
     const link = document.createElement('a');
     link.classList.add(ccls.HOVER_LINK);
-    link.href = `${paths.LEXICON}/${type.replaceAll('/', '_')}.html`;
+    link.href = paths.crum(type.replaceAll('/', '_'));
     link.target = '_blank';
     link.textContent = type;
     el.replaceChildren(link);
@@ -118,7 +118,7 @@ export function handleDawoudPageImg(root: HTMLElement): void {
       const img = el.children[0] as HTMLElement;
       img.classList.add(ccls.LINK);
       img.addEventListener('click', (): void => {
-        browser.open(`${paths.DAWOUD}?page=${img.getAttribute('alt')!}`);
+        browser.open(paths.dawoudScan(img.getAttribute('alt')!));
       });
     });
 }
@@ -132,7 +132,7 @@ export function handleCrumPageImg(root: HTMLElement): void {
     const img = el.children[0] as HTMLElement;
     img.classList.add(ccls.LINK);
     img.addEventListener('click', (): void => {
-      browser.open(`${paths.CRUM_SCAN_PREFIX}${img.getAttribute('alt')!}`);
+      browser.open(paths.crumScan(img.getAttribute('alt')!));
     });
   });
 }
@@ -323,21 +323,23 @@ export function insertCrumAbbreviationsLink(): void {
 }
 
 /**
- *
+ * Prefix all navigation URLs with the Lexicon path.
+ * This is unnecessary on web because a browser is capable of resolving relative
+ * paths. But it is necessary on Anki.
  * @param root
  */
 export function handleAnkiNavigation(root: HTMLElement): void {
   if (!iam.amI('anki')) return;
-  root.querySelectorAll<HTMLElement>(`.${cls.NAVIGATE}`).forEach((e) => {
-    if (e.tagName !== 'A' || !e.hasAttribute('href')) {
-      log.error(
-        'This "navigate" element is not an <a> tag with an "href" property!',
-        e
-      );
-      return;
-    }
-    e.setAttribute('href', `${paths.LEXICON}/${e.getAttribute('href')!}`);
-  });
+
+  root
+    .querySelectorAll<HTMLAnchorElement>(`a.${cls.NAVIGATE}`)
+    .forEach((e: HTMLAnchorElement) => {
+      if (e.href.startsWith('http')) {
+        log.error(cls.NAVIGATE, 'element looks like an absolute URL!');
+        return;
+      }
+      e.setAttribute('href', `${paths.LEXICON}/${e.href}`);
+    });
 }
 
 /**
@@ -348,7 +350,7 @@ export function addCopticLookups(root: HTMLElement): void {
   html.linkifyText(
     root,
     COPTIC_RE,
-    (match: RegExpExecArray) => paths.LOOKUP_URL_PREFIX + match[0],
+    (match: RegExpExecArray) => paths.lexiconLookup(match[0]),
     [ccls.HOVER_LINK],
     [cls.TYPE, cls.WIKI]
   );
@@ -362,7 +364,7 @@ export function addGreekLookups(root: HTMLElement): void {
   html.linkifyText(
     root,
     GREEK_RE,
-    (match: RegExpExecArray) => paths.GREEK_DICT_PREFIX + match[0],
+    (match: RegExpExecArray) => paths.greekLookup(match[0]),
     [ccls.LINK, cls.LIGHT]
   );
 }
@@ -376,7 +378,7 @@ export function addEnglishLookups(root: HTMLElement): void {
     html.linkifyText(
       el,
       ENGLISH_RE,
-      (match: RegExpExecArray) => paths.LOOKUP_URL_PREFIX + match[0],
+      (match: RegExpExecArray) => paths.lexiconLookup(match[0]),
       [ccls.HOVER_LINK]
     );
   });

docs/crum/help.ts
@@ -75,11 +75,9 @@ export function makeHelpPanel(
     ],
     X: [
       new help.Shortcut(
-        `Open the <a href="${paths.LEXICON}" target="_blank">dictionary search page</a>`,
+        `Open the <a href="${browser.getLinkHref('search')!}" target="_blank">search page</a>`,
         ['lexicon', 'note', 'index', 'index_index'],
-        () => {
-          browser.open(paths.LEXICON);
-        }
+        browser.openSearchLink
       ),
     ],
     '?': [
@@ -196,13 +194,9 @@ export function makeHelpPanel(
       ),
     ],
     T: [
-      new help.Shortcut(
-        `<a href="${paths.COPTICSITE}" target="_blank" rel="noopener,noreferrer">copticsi<strong>t</strong>e</a>`,
-        ['lexicon'],
-        () => {
-          browser.scroll('copticsite-title');
-        }
-      ),
+      new help.Shortcut('copticsi<strong>t</strong>e', ['lexicon'], () => {
+        browser.scroll('copticsite-title');
+      }),
     ],
     I: [
       new help.Shortcut('Wiki', ['lexicon'], () => {
@@ -304,13 +298,9 @@ export function makeHelpPanel(
       ),
     ],
     t: [
-      new help.Shortcut(
-        `<a href="${paths.COPTICSITE}" target="_blank" rel="noopener,noreferrer">copticsi<strong>t</strong>e</a>`,
-        ['lexicon'],
-        () => {
-          browser.click('copticsite-title');
-        }
-      ),
+      new help.Shortcut('copticsi<strong>t</strong>e', ['lexicon'], () => {
+        browser.click('copticsite-title');
+      }),
     ],
     i: [
       new help.Shortcut('Wiki', ['lexicon'], () => {

docs/crum/id.ts
@@ -1,2 +1,3 @@
+export const WIKI = 'wiki';
 export const WIKI_TITLE = 'wiki-title';
 export const WIKI_COLLAPSIBLE = 'wiki-collapsible';

docs/crum/index.html
@@ -16,6 +16,7 @@
     gtag("js", new Date());
     gtag("config", "G-VCVZFDFZR3");
   </script>
+  <link href="." rel="search">
   <link href="./" rel="canonical">
   <title>ⲡⲓⲖⲉⲝⲓⲕⲟⲛ | Coptic Dictionary</title>
   <script src="lexicon.js" type="module"></script>

docs/crum/lexicon.ts
@@ -104,7 +104,7 @@ class CrumSearchResult extends SearchResult {
    * @returns
    */
   protected override link(): string {
-    return `${paths.LEXICON}/${this.key}.html`;
+    return paths.crum(this.key);
   }
 
   /**
@@ -162,7 +162,7 @@ class KELLIASearchResult extends SearchResult {
    * @returns
    */
   protected override link(): string {
-    return paths.CDO_LOOKUP_BY_KEY_PREFIX + this.key;
+    return paths.copticDictionaryOnline(this.key);
   }
 
   /**
@@ -211,7 +211,7 @@ class WikiSearchResult extends xoox.SearchResult {
    * @returns
    */
   protected override link(): string {
-    return `${paths.LEXICON}/${this.key}.html#wiki`;
+    return `${paths.crum(this.key)}#${id.WIKI}`;
   }
 }
 

docs/crum/wiki.ts
@@ -259,47 +259,37 @@ const DAN_OVERRIDE: Record<string, { chapter: string; name: string }> = {
 function parseBibleCitation(
   match: RegExpExecArray
 ): { url: string; name: string } | null {
-  let [bookAbbreviation, chapter, verse] = [match[1], match[2], match[3]];
-  if (!bookAbbreviation) {
-    // NOTE: This is not expected, because the book abbreviation is a
-    // non-optional piece of the regex. We have this check just to appease the
-    // linter.
-    return null;
-  }
+  let [bookAbbreviation, chapter, verse] = [match[1]!, match[2], match[3]];
   const danOverride = DAN_OVERRIDE[bookAbbreviation];
   if (danOverride) {
     // Given that this special book contains one chapter, the book
-    // abbreviation is followed by the verse number only (there is no
-    // chapter number).
-    bookAbbreviation = 'Dan';
+    // abbreviation is followed by the verse number only. This number would've
+    // been mistakenly interpreted as the chapter number, but it's actually the
+    // verse number.
     verse = chapter;
     chapter = danOverride.chapter;
+    bookAbbreviation = 'Dan';
   }
+
   const book: { name: string; path: string; numChapters: number } | undefined =
     bible.MAPPING[bookAbbreviation];
   if (!book) {
+    // No book found! This match is not a Biblical reference.
     return null;
   }
-  const name: string = danOverride?.name ?? book.name;
-  if (!chapter) {
-    log.ensure(
-      !verse,
-      'Given the regex, if there is no chapter, there is definitely no verse!'
-    );
-    // This points to the whole book.
-    return { url: paths.bibleBookURL(book.path), name };
-  }
-  if (!verse && book.numChapters === 1) {
-    // This is a one-chapter book. The chapter number is always 1. The given
-    // number is actually the verse number.d
+
+  if (chapter && !verse && book.numChapters === 1) {
+    // This is a one-chapter book. The chapter number is always 1. The number
+    // immediately followed the book, which was interpreted as the chapter
+    // number, is actually the verse number.
     verse = chapter;
     chapter = '1';
   }
-  let url = `${paths.BIBLE}/${book.path}_${chapter}.html`;
-  if (verse) {
-    url = `${url}#v${verse}`;
-  }
-  return { url, name };
+
+  return {
+    url: paths.bible(book.path, chapter, verse),
+    name: danOverride?.name ?? book.name,
+  };
 }
 
 /**

docs/dropdown.ts
@@ -57,10 +57,7 @@ export class Droppable {
     }
     // This is a click-invoked tooltip.
     // Prevent clicks on the content from hiding it.
-    this.droppable.addEventListener(
-      'click',
-      browser.stopPropagation.bind(browser)
-    );
+    this.droppable.addEventListener('click', browser.stopPropagation);
     // A click on the .drop element hides the content.
     parent.addEventListener('click', (e: MouseEvent) => {
       this.toggle();

docs/paths.ts
@@ -8,16 +8,56 @@ export const HOME = `${URL}/`;
 export const LEXICON = `${URL}/crum`;
 export const DAWOUD = `${URL}/dawoud`;
 export const BIBLE = `${URL}/bible`;
-export const LOOKUP_URL_PREFIX = `${LEXICON}?query=`;
 
 /**
- * Construct a Bible book URL.
  *
- * @param bookID
- * @returns Bible book URL.
+ * @param key
+ * @param drvKey
+ * @returns
  */
-export function bibleBookURL(bookID: string): string {
-  return `${BIBLE}?book=${bookID}`;
+export function crum(key: string, drvKey?: string): string {
+  const url = `${LEXICON}/${key}.html`;
+  if (drvKey) {
+    return `${url}#drv${drvKey}`;
+  }
+  return url;
+}
+
+/**
+ *
+ * @param page
+ * @returns
+ */
+export function dawoudScan(page: string): string {
+  return `${DAWOUD}?page=${page}`;
+}
+
+/**
+ *
+ * @param query
+ * @returns
+ */
+export function lexiconLookup(query: string): string {
+  return `${LEXICON}?query=${query}`;
+}
+
+/**
+ *
+ * @param book
+ * @param chapter
+ * @param verse
+ * @returns
+ */
+export function bible(book: string, chapter?: string, verse?: string): string {
+  if (!chapter) {
+    // This is a book URL.
+    return `${BIBLE}?book=${book}`;
+  }
+  const chapterURL = `${BIBLE}/${book}_${chapter}.html`;
+  if (verse) {
+    return `${chapterURL}#v${verse}`;
+  }
+  return chapterURL;
 }
 
 // Other pages that we own:
@@ -27,15 +67,35 @@ export const REPORTS_PAGE_PARAM = 'entry.1382006920';
 
 // Pages that we don't own:
 export const KELLIA = 'https://kellia.uni-goettingen.de/';
-export const CDO_LOOKUP_BY_KEY_PREFIX =
-  'https://coptic-dictionary.org/entry.cgi?tla=';
-// TODO: (#50) Revisit the Greek dictionary used.
-export const GREEK_DICT_PREFIX = 'https://logeion.uchicago.edu/';
+
+/**
+ *
+ * @param key
+ * @returns
+ */
+export function copticDictionaryOnline(key: string): string {
+  return `https://coptic-dictionary.org/entry.cgi?tla=${key}`;
+}
+
+/**
+ *
+ * @param word
+ * @returns
+ * TODO: (#50) Revisit the Greek dictionary used.
+ */
+export function greekLookup(word: string): string {
+  return `https://logeion.uchicago.edu/${word}`;
+}
+
 export const CRUM_ABBREVIATIONS =
   'https://www.coptist.com/2025/07/30/digitised-bibliography-crum/';
-// TODO: (#460): Delete the following constant once no longer used.
-export const CRUM_SCAN_PREFIX =
-  'https://coptot.manuscriptroom.com/crum-coptic-dictionary/?docID=800000&pageID=';
-// TODO: (#308) Delete this constant and stop referencing the site. (It's not
-// working anyway!)
-export const COPTICSITE = 'http://copticsite.com/';
+
+/**
+ *
+ * @param page
+ * @returns
+ */
+export function crumScan(page: string): string {
+  // TODO: (#460): Use the in-house scan.
+  return `https://coptot.manuscriptroom.com/crum-coptic-dictionary/?docID=800000&pageID=${page}`;
+}

commit b73db11255404938ca2b4bd981a6ce187905ccc1

[#490][TypeScript] Fix Bug in Anki Navigation Handling.
Bug was introduced in 206e9c73a351a64357e6f9cd518c8c2157a256e9.

docs/crum/crum.ts
@@ -333,12 +333,18 @@ export function handleAnkiNavigation(root: HTMLElement): void {
 
   root
     .querySelectorAll<HTMLAnchorElement>(`a.${cls.NAVIGATE}`)
-    .forEach((e: HTMLAnchorElement) => {
-      if (e.href.startsWith('http')) {
-        log.error(cls.NAVIGATE, 'element looks like an absolute URL!');
+    .forEach((a: HTMLAnchorElement) => {
+      // Get the raw attribute.
+      const href: string | null = a.getAttribute('href');
+      if (!href) {
+        log.error(cls.NAVIGATE, 'element HREF is unset!');
         return;
       }
-      e.setAttribute('href', `${paths.LEXICON}/${e.href}`);
+      if (href.startsWith('http')) {
+        log.error(cls.NAVIGATE, 'element HREF looks like an absolute URL!');
+        return;
+      }
+      a.setAttribute('href', `${paths.LEXICON}/${href}`);
     });
 }
 