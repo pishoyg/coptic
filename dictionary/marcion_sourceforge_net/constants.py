pishoyg/coptic

commit 9939538010110c843baa0d8bb5f9f8aa827bbbc2

[#573][Crum/Parser] Introduce a Notation for Inserted Spaces.

dictionary/marcion_sourceforge_net/constants.py
@@ -160,16 +160,24 @@ PREPROCESSING: list[tuple[str, str]] = [
     # "ⲁⲑⲟⲩ.", which could be handy (though it doesn't have an apparent usage
     # today).
     ("..", ""),
-    # Crum often wrote derivations with a space, including in cases where the
-    # combination is considered, according to our modern standards, a single
-    # word.
-    # For example, ‘ϯⲟⲩⲱⲓⲛⲓ’ is written as ‘ϯ ⲟⲩ.’. We would write this in our
-    # data source as ‘ϯ_ⲟⲩⲱⲓⲛⲓ’, expanding ‘ⲟⲩ.’ to its full form, and replacing
-    # the space with an underscore.
-    # For now, we opt for simply omitting this underscore from the output.
-    # In cases where the two parts are considered separate words in our
-    # standard, we retain the space as is.
+    # Crum often wrote derivations with a space.
+    # 1. In cases where the combination is considered, according to our own
+    #    spacing standard, a single word, we would replace the space with an
+    #    underscore.
+    #    - For example, ‘ϯⲟⲩⲱⲓⲛⲓ’ is written as ‘ϯ ⲟⲩ.’. We would write this in
+    #      our data source as ‘ϯ_ⲟⲩⲱⲓⲛⲓ’, expanding ‘ⲟⲩ.’ to its full form, and
+    #      replacing the space with an underscore.
+    # 2. In cases where the two parts are considered separate words in our
+    #    spacing standard (i.e. our spacing criteria match Crum's), we do
+    #    nothing – retaining the space as is.
+    # 3. In cases where we would like to insert a space where Crum didn't have
+    #    one, we use a double underscore.
+    #
+    # It is important to use this underscore notation, instead of simply
+    # changing the spacing, in order to be able to construct the original
+    # spacing used by Crum if needed.
     ("_", ""),
+    ("__", " "),
 ]
 
 # NOTE: As of the time of writing, some of these annotations occur only once in

commit d8ff26a6875919cca116d2a705f505e2c77de394

[#573][Crum/Parser] Fix Bug.
Bug introduced in 9939538010110c843baa0d8bb5f9f8aa827bbbc2!

dictionary/marcion_sourceforge_net/constants.py
@@ -176,8 +176,8 @@ PREPROCESSING: list[tuple[str, str]] = [
     # It is important to use this underscore notation, instead of simply
     # changing the spacing, in order to be able to construct the original
     # spacing used by Crum if needed.
-    ("_", ""),
     ("__", " "),
+    ("_", ""),
 ]
 
 # NOTE: As of the time of writing, some of these annotations occur only once in