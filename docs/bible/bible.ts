b1c3c84496655ef87bfbaa6f7c2ef96e4f79dfd6
Pishoy Girgis <pishoybg@gmail.com>
2025-11-26 19:48:47 +0100

    [Bible] Nit: Add Animation Delay.

--- docs/bible/bible.ts
+++ docs/bible/bible.ts
@@ -5,6 +5,7 @@ import * as log from '../logger.js';
 import * as cls from './cls.js';
 
 const BOOK_PARAM = 'book';
+const CLICK_DELAY_MS = 500;
 
 /**
  * If the book query parameter is present, click on the title of the given
@@ -21,8 +22,9 @@ function maybeGoToBook(): void {
     log.error(click, 'not found!');
     return;
   }
-  elem.click();
   elem.scrollIntoView({ behavior: 'smooth' });
+  // Scroll first, wait a bit, then click.
+  setTimeout(elem.click.bind(elem), CLICK_DELAY_MS);
 }
 
 /**
