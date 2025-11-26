baf540be6f0af20ecc6dc7f4a54f0d9e8b8ae03a
Pishoy Girgis <pishoybg@gmail.com>
2025-11-26 11:51:55 +0100

    [#194][Crum/Wiki] Add Annotation.

--- docs/crum/annotations.ts
+++ docs/crum/annotations.ts
@@ -122,6 +122,7 @@ export const MAPPING: Record<string, Annotation> = {
   dat: { fullForm: 'dative' },
   demonstr: { fullForm: 'demonstrative' },
   'e g': { fullForm: 'exempli gratia' },
+  frag: { fullForm: 'fragment' },
   fut: { fullForm: 'future' },
   Heb: { fullForm: 'Hebrew', noCaseVariant: true },
   Hebr: { fullForm: 'Hebrew', noCaseVariant: true },
