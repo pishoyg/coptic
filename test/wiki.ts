23d69d054ba2e1346d4ba7a9dcdc87e58f8948c0
Pishoy Girgis <pishoybg@gmail.com>
2025-12-07 19:46:15 +0100

    [#565][Crum/Wiki/References] Update a Test Case.

--- test/wiki.ts
+++ test/wiki.ts
@@ -26,8 +26,8 @@ const TEST_CASES: {
     // lot of common cases.
     key: '88',
     want: {
-      references: 143,
-      suffixes: 136,
+      references: 144,
+      suffixes: 137,
       bible: 119,
       dialectTooltips: 388,
       annotations: 86,
