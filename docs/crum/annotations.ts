e8fcecce62a26c108b489dc0be4f26c496570a91
Pishoy Girgis <pishoybg@gmail.com>
2025-12-12 10:53:54 +0100

    [#194][Crum/Wiki/Annotations] Add Annotations.

--- docs/crum/annotations.ts
+++ docs/crum/annotations.ts
@@ -154,7 +154,6 @@ export const DATA: Abbreviation[] = [
   // TODO: (#194) Is there a third perfect or third present?
   { fullForm: 'third future', variants: ['3 fut'] },
 
-  { fullForm: 'excluding', variants: ['exc'] },
   { fullForm: 'et cetera', variants: ['&c'] },
   { fullForm: 'absolute', variants: ['absol'] },
   { fullForm: 'Anno Domini', variants: ['AD'], noCaseVariant: true },
@@ -176,7 +175,9 @@ export const DATA: Abbreviation[] = [
   { fullForm: 'determination', variants: ['determ', 'determin'] },
   { fullForm: 'exempli gratia', variants: ['e g'] },
   { fullForm: 'Ethiopic', variants: ['Ethiop'] },
+  { fullForm: 'equivalent', variants: ['equiv'] },
   { fullForm: 'etymology', variants: ['etymol'] },
+  { fullForm: 'excluding', variants: ['exc'] },
   { fullForm: 'fragment', variants: ['frag'] },
   { fullForm: 'future', variants: ['fut'] },
   { fullForm: 'Hebrew', variants: ['Heb', 'Hebr'], noCaseVariant: true },
@@ -190,8 +191,10 @@ export const DATA: Abbreviation[] = [
   { fullForm: 'imperfect', variants: ['impf'] },
   { fullForm: 'improbable', variants: ['improb'] },
   { fullForm: 'loco citato', variants: ['l c'] },
+  { fullForm: 'Latin', variants: ['Lat'], noCaseVariant: true },
   { fullForm: 'manuscript', variants: ['MS'], noCaseVariant: true },
   { fullForm: 'manuscripts', variants: ['MSS'], noCaseVariant: true },
+  { fullForm: 'metaphor', variants: ['metaph'] },
   { fullForm: 'negative', variants: ['neg'] },
   // NOTE: 'partic' encountered once, as of the time of writing!
   { fullForm: 'participle', variants: ['particip', 'partic'] },
