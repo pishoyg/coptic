f4115d453f61369b20d096fd7aa2ee3536cffedb
Pishoy Girgis <pishoybg@gmail.com>
2025-12-26 08:03:57 +0200

    [#457][TypeScript] Add `localStorage` to Unit Test Global Scope.

--- bun.ts
+++ bun.ts
@@ -7,10 +7,11 @@ import * as jsdom from 'jsdom';
 
 // Create a new virtual DOM instance with a complete basic HTML structure.
 // This runs once before any of your test files are even loaded.
-const dom = new jsdom.JSDOM('<html></html>');
+const dom = new jsdom.JSDOM('<html></html>', { url: 'http://localhost' });
 
 // Assign the JSDOM-created window and document objects to the global scope.
 // This makes them available to any module that needs them, right from the
 // start.
 global.window = dom.window;
 global.document = dom.window.document;
+global.localStorage = dom.window.localStorage;
