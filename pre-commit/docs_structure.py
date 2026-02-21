589acfdbb430b0446ce914704ecdadb00b1cf487
ⲣⲉⲙⲛ̀Ⲭⲏⲙⲓ <remnqymi@gmail.com>
2026-02-21 17:59:24 +0100

    [#681][Crum/Images] Add a TODO.

--- pre-commit/docs_structure.py
+++ pre-commit/docs_structure.py
@@ -130,7 +130,8 @@ PATTERNS: list[Pattern] = [
         ],
     ),
     Pattern(["crum/crum/*.png"]),  # Crum scan.
-    Pattern(["crum/explanatory/*-*-*.*"]),  # Explanatory images.
+    # TODO: (#681) Restore the image pipeline.
+    # Pattern(["crum/explanatory/*-*-*.*"]),  # Explanatory images.
     Pattern(["crum/anki/*"], required=False),  # Anki is not tracked in Git.
     # Auto-generated (bible):
     Pattern(["bible/index.html"]),
