6c068fc22cfb0d5a489f3850ec84adbd378d9795
ⲣⲉⲙⲛ̀Ⲭⲏⲙⲓ <remnqymi@gmail.com>
2026-04-07 13:33:14 +0200

    [TypeScript] `make javascript`

--- docs/iam.js
+++ docs/iam.js
@@ -1,2 +1,2 @@
-"use strict";export function amI(e){return e==="anki"?typeof ANKI<"u":document.body.classList.contains(e)}
+"use strict";export function amI(e){return typeof document>"u"?!1:document.body.classList.contains(e)}
 //# sourceMappingURL=iam.js.map
