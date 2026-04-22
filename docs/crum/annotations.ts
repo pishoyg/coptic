db983c28085da4c24cdb9dbc3aa2095b1b5c56be
ⲣⲉⲙⲛ̀Ⲭⲏⲙⲓ <remnqymi@gmail.com>
2026-04-22 17:50:53 +0200

    [#194][Crum/Wiki] Add an Annotation.

--- docs/crum/annotations.ts
+++ docs/crum/annotations.ts
@@ -292,6 +292,7 @@ export const DATA: Abbreviation[] = [
   { fullForm: 'loco citato', variants: ['l c', 'll c'], suffix: true },
   { fullForm: 'loci citati', variants: ['ll cc'], suffix: true },
   { fullForm: 'loquitur', variants: ['loq'] },
+  { fullForm: 'Septuagint', variants: ['LXX'], noCaseVariant: true },
   { fullForm: 'Latin', variants: ['Lat'], noCaseVariant: true },
   {
     fullForm: 'manuscript',
