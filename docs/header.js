a9e3afab0cf7ee27e5993f372df592a218e2be5c
ⲣⲉⲙⲛ̀Ⲭⲏⲙⲓ <remnqymi@gmail.com>
2026-05-20 15:35:31 +0200

    [TypeScript] `make javascript`

--- docs/header.js
+++ docs/header.js
@@ -1,2 +1,2 @@
-"use strict";import*as t from"./paths.js";export var CLS=(e=>(e.RESET="reset",e.DEVELOPER="developer",e))(CLS||{});export function reports(){const r=new URL(t.REPORTS);return r.searchParams.set(t.REPORTS_PAGE_PARAM,window.location.href),r.toString()}
+"use strict";import*as r from"./paths.js";export var CLS=(e=>(e.RESET="reset",e.DEVELOPER="developer",e))(CLS||{}),EVENT=(o=>(o.RESET="reset",o))(EVENT||{});export function dispatch(t){document.dispatchEvent(new CustomEvent(t))}export function reports(){const t=new URL(r.REPORTS);return t.searchParams.set(r.REPORTS_PAGE_PARAM,window.location.href),t.toString()}
 //# sourceMappingURL=header.js.map
