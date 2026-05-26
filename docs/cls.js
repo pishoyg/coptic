24f7e80892ff7d53f9f91878271ff594cf3b2529
ⲣⲉⲙⲛ̀Ⲭⲏⲙⲓ <remnqymi@gmail.com>
2026-05-26 20:00:21 +0200

    [TypeScript] `make javascript`

--- docs/cls.js
+++ docs/cls.js
@@ -1,2 +1,2 @@
-"use strict";export const LINK="link",HOVER_LINK="hover-link",HOVER_ACTION="hover-action";
+"use strict";export const LINK="link",HOVER_LINK="hover-link",HOVER_ACTION="hover-action",SMALL_TEXT="small-text";
 //# sourceMappingURL=cls.js.map
