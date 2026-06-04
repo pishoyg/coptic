a2978d060f5720173eff5f9bb0f4ccb1229c1491
ⲣⲉⲙⲛ̀Ⲭⲏⲙⲓ <remnqymi@gmail.com>
2026-06-04 18:14:38 +0300

    [#452][Andreas] Greek Text is not Marked at the Source.

--- docs/crum/andreas.ts
+++ docs/crum/andreas.ts
@@ -1,9 +1,18 @@
-import * as crum from './crum.js';
+import * as html from '../html.js';
+import * as paths from '../paths.js';
+import * as cls from './cls.js';
+
+const GREEK_WORD_RE = /[\p{Script=Greek}][\p{Script=Greek}\p{Mark}]*/u;
 
 /**
  *
  * @param root
  */
 export function handle(root: HTMLElement): void {
-  crum.addGreekLookups(root);
+  html.linkifyText(
+    root,
+    GREEK_WORD_RE,
+    (match: RegExpExecArray): string => paths.greekLookup(match[0]),
+    [cls.GREEK]
+  );
 }
