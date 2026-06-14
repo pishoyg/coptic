8aa8f29dd6cd6e8393fcb53cd42e9f82d3111b92
ⲣⲉⲙⲛ̀Ⲭⲏⲙⲓ <remnqymi@gmail.com>
2026-06-14 09:06:12 +0300

    [#432][Bible] Store a Bibliography.

--- pre-commit/docs_structure.py
+++ pre-commit/docs_structure.py
@@ -105,6 +105,7 @@ PATTERNS: list[Pattern] = [
     Pattern(["index.html"]),  # Home
     Pattern(["crum/index.html"]),  # Lexicon
     Pattern(["keyboard.html"]),
+    Pattern(["bible/bibliography.json"]),
     Pattern(["**.ts"]),
     Pattern(["**.css"]),
     # Data files:
